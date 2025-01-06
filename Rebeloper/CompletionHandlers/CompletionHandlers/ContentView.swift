fe9d3d186405485cbf9e89c1cfaf5c0cb77a3101
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-06 09:51:52 -0500

    update Rebeloper/CompletionHandlers/ContentView with delayedOptional and delayedWithError functions

--- Rebeloper/CompletionHandlers/CompletionHandlers/ContentView.swift
+++ Rebeloper/CompletionHandlers/CompletionHandlers/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var name: String = "World"
+    @State private var name: String = "World!"
     
     var body: some View {
         VStack {
@@ -28,8 +28,21 @@ struct ContentView: View {
     func buttonTapped() {
 //        name = instant()
 
-        delayed { value in
-            self.name = value
+//        delayed { value in
+//            self.name = value
+//        }
+        
+//        delayedOptional { value in
+//            guard value != nil else { return }
+//            self.name = name
+//        }
+        
+        delayedWithError { result in
+            if result.1 != nil {
+                print("Error: \(result.1!.localizedDescription)")
+                return
+            }
+            self.name = result.0
         }
     }
     
@@ -41,6 +54,20 @@ struct ContentView: View {
 
         delay(2) {
             completion("Burlington!")
+        }
+    }
+    
+    func delayedOptional(completion: @escaping (String?) -> Void) {
+
+        delay(2) {
+            completion(nil)
+        }
+    }
+    
+    func delayedWithError(completion: @escaping ((String, Error?)) -> Void) {
+
+        delay(2) {
+            completion(("Wow", nil))
         }
     }
     
