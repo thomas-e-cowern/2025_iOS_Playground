5f8d5d890999cc2ca4c4f79e51dc6ba763a6d015
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-06 09:35:14 -0500

    update Rebeloper/CompletionHandlers/ContentView with instant and delayed functions

--- Rebeloper/CompletionHandlers/CompletionHandlers/ContentView.swift
+++ Rebeloper/CompletionHandlers/CompletionHandlers/ContentView.swift
@@ -26,15 +26,28 @@ struct ContentView: View {
     }
     
     func buttonTapped() {
-        // MARK: TODO - Add buttonTapped function
+//        name = instant()
+
+        delayed { value in
+            self.name = value
+        }
     }
     
+    func instant() -> String {
+        return "Burl!"
+    }
     
+    func delayed(completion: @escaping (String) -> Void) {
+
+        delay(2) {
+            completion("Burlington!")
+        }
+    }
     
     // MARK: - Helpers to simulate delay
     func delay(_ seconds: Double, closure: @escaping () -> Void) {
         DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
-            
+            closure()
         }
     }
 }
