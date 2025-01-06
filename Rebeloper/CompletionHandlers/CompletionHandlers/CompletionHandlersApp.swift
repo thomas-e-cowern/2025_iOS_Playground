e70cec63a957ead7ef479a8c18dbe5e8fb7dedb3
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-06 08:45:39 -0500

    Rebeloper CompletionHadlers initial commit

---
+++ Rebeloper/CompletionHandlers/CompletionHandlers/CompletionHandlersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompletionHandlersApp.swift
+//  CompletionHandlers
+//
+//  Created by Thomas Cowern on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct CompletionHandlersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
