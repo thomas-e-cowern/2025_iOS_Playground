53fe37cc3502b54fa3e0328d560d4c4380028c89
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-06 09:06:47 -0500

    update ContentView in Rebeloper CompletionHandlers with buttonTapped func framework and delay func, add @State variable and Change Name Button

--- Rebeloper/CompletionHandlers/CompletionHandlers/ContentView.swift
+++ Rebeloper/CompletionHandlers/CompletionHandlers/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name: String = "World"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, \(name)")
         }
         .padding()
+        
+        Button("Change Name") {
+            buttonTapped()
+        }
+    }
+    
+    func buttonTapped() {
+        // MARK: TODO - Add buttonTapped function
+    }
+    
+    
+    
+    // MARK: - Helpers to simulate delay
+    func delay(_ seconds: Double, closure: @escaping () -> Void) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            
+        }
     }
 }
 
