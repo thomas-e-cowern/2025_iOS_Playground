1c5f881d63b167d5a2f4f0923b3318810bd437df
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-07 10:45:19 -0500

    add View extension and SizePreferenceKey struct to PeekingScrollView

---
+++ Rebeloper/PeekingScrollView/PeekingScrollView/View+ReadSize.swift
@@ -0,0 +1,28 @@
+//
+//  View+ReadSize.swift
+//  PeekingScrollView
+//
+//  Created by Thomas Cowern on 1/7/25.
+//
+
+import SwiftUI
+
+extension View {
+    func readSize(onChange: @escaping (CGSize) -> Void) -> some View {
+        background(
+            GeometryReader { geo in
+                Color.clear
+                    .preference(key: SizePreferenceKey.self, value: geo.size)
+            }
+        )
+        .onPreferenceChange(SizePreferenceKey.self, perform: onChange)
+    }
+}
+
+private struct SizePreferenceKey: PreferenceKey {
+    static var defaultValue: CGSize = .zero
+    
+    static func reduce(value: inout CGSize, nextValue: () -> CGSize) {
+        value = nextValue()
+    }
+}
