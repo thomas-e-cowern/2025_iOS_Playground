8971ea4c3c29909ff3fb4a4f2037704b544722be
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-08 09:49:29 -0500

    add HackingWithSwift and Sharpening folder and HWSSharpeningSkills project

---
+++ HackingWithSwift/Sharpening/HWSSharpeningSkills/HWSSharpeningSkills/HWSSharpeningSkillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWSSharpeningSkillsApp.swift
+//  HWSSharpeningSkills
+//
+//  Created by Thomas Cowern on 1/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HWSSharpeningSkillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
