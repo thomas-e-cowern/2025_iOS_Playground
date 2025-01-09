f73b8a1b156df19f947e2b14df5065689e2e6ecd
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-09 08:04:39 -0500

    add SwiftUITextFields to Rebeloper project folder

---
+++ Rebeloper/SwiftUITextFields/SwiftUITextFields/SwiftUITextFieldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITextFieldsApp.swift
+//  SwiftUITextFields
+//
+//  Created by Thomas Cowern on 1/9/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITextFieldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
