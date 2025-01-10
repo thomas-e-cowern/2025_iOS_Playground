2b4683f83cf48f5794c37898844042cedbec5e45
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-01-10 09:03:47 -0500

    add ExploringOptionals playground to HackingWithSwift/Playground

---
+++ HackingWithSwift/Playground/ExploringOptionals.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+struct User {
+    var name: String
+    var password: Optional<String>
+}
+
+let marisol = User(name: "Marisol", password: "dshiel12")
+let amelia = User(name: "Amelia", password: nil)
+
+
--- HackingWithSwift/Sharpening/HWSSharpeningSkills/HWSSharpeningSkills/UnderstandingAssertions/Main.swift
+++ HackingWithSwift/Sharpening/HWSSharpeningSkills/HWSSharpeningSkills/UnderstandingAssertions/Main.swift
@@ -14,13 +14,13 @@ func sum(of numbers: [Int]) -> Int {
 
 let number = Int.random(in: 1...3)
 
-switch number {
-case 1:
-    print("You rolled a one...")
-case 2:
-    print("You rolled a one...")
-case 3:
-    print("You rolled a one...")
-default:
-    assertionFailure("Rolling a 3 sided dice is never a good idea...")
-}
+//switch number {
+//case 1:
+//    print("You rolled a one...")
+//case 2:
+//    print("You rolled a one...")
+//case 3:
+//    print("You rolled a one...")
+//default:
+//    assertionFailure("Rolling a 3 sided dice is never a good idea...")
+//}
