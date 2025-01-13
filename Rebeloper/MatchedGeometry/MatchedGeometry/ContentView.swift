42a0ae8e67b392c0b2b1d082bab299d37abc0ee5
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-13 09:07:04 -0500

    Set up ContentView in Rebeloper/MatchedGeometry/MatchedGeometry

--- Rebeloper/MatchedGeometry/MatchedGeometry/ContentView.swift
+++ Rebeloper/MatchedGeometry/MatchedGeometry/ContentView.swift
@@ -10,12 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Circle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                Circle()
+                    .fill(.green)
+                    .frame(width: 100, height: 100)
+            }
+            .padding()
+            Button("Click Me 👆") {
+                withAnimation {
+                    // do something
+                } completion: {
+                    // more here
+                }
+
+            }
         }
-        .padding()
     }
 }
 
