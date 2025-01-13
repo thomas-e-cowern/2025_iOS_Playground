03c260264c9bca8b24da7cd19eb60d2b7c343383
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-13 09:01:11 -0500

    MatchedGeometry added to Rebeloper directory in 2025_iOS_Playground

---
+++ Rebeloper/MatchedGeometry/MatchedGeometry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchedGeometry
+//
+//  Created by Thomas Cowern on 1/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rebeloper/MatchedGeometry/MatchedGeometry/MatchedGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryApp.swift
+//  MatchedGeometry
+//
+//  Created by Thomas Cowern on 1/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
