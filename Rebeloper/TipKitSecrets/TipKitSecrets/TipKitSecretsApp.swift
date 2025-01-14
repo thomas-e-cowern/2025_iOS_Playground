14a3f462882d2af9bed8dd46e4151da4ef503aba
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-14 08:10:16 -0500

    add TipKit import and configuration to App file in Rebeloper/TipKitSecrets

--- Rebeloper/TipKitSecrets/TipKitSecrets/TipKitSecretsApp.swift
+++ Rebeloper/TipKitSecrets/TipKitSecrets/TipKitSecretsApp.swift
@@ -6,9 +6,21 @@
 //
 
 import SwiftUI
+import TipKit
 
 @main
 struct TipKitSecretsApp: App {
+    
+    init () {
+        // resetDatastore must be removed before shipping
+        try? Tips.resetDatastore()
+        
+        // Tips configuration goes here
+        try? Tips.configure([
+            .displayFrequency(.monthly)
+        ])
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
