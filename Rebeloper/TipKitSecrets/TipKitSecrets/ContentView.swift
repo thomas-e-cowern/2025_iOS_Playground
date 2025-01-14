e7657b29745237785e5b3f1d81fdaefff38e8164
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-14 08:02:19 -0500

    Rebeloper TipKitSecrets initial commit

---
+++ Rebeloper/TipKitSecrets/TipKitSecrets/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TipKitSecrets
+//
+//  Created by Thomas Cowern on 1/14/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rebeloper/TipKitSecrets/TipKitSecrets/TipKitSecretsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitSecretsApp.swift
+//  TipKitSecrets
+//
+//  Created by Thomas Cowern on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TipKitSecretsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
