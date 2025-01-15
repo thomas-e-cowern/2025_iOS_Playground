bde594e19a893a8b74e7ad1e629c8468f51f2f0a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-15 11:07:34 -0500

    add Models and DevData folder, add messages.json for DevData folder

---
+++ Skills/DummyDataTest/DummyDataTest/Models/Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  DummyDataTest
+//
+//  Created by Thomas Cowern on 1/15/25.
+//
+
+import Foundation
+
+struct Message {
+    var userId: Int
+    var id: Int
+    var title: String
+    var body: String
+}
