31a06049b0aa8847c9a20b2cb15b27c46a17016e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-15 10:48:04 -0500

    add test Fetch funtions to HWSResult in Random

--- HackingWithSwift/Random/HWSResult/HWSResult/main.swift
+++ HackingWithSwift/Random/HWSResult/HWSResult/main.swift
@@ -22,11 +22,27 @@ func fetchUnreadCount1(from urlString: String, completionHandler: @escaping (Res
     completionHandler(.success(5))
 }
 
+// with switch statement
 fetchUnreadCount1(from: "https://www.hackingwithswift.com") { result in
     switch result {
         case .success(let count):
-            print("\(count) unread messages.")
+            print("switch: \(count) unread messages.")
         case .failure(let error):
             print(error.localizedDescription)
         }
 }
+
+// with if let statement
+fetchUnreadCount1(from: "https://www.hackingwithswift.com") { result in
+    if let count = try? result.get() {
+        print("if let: \(count) unread messages.")
+    }
+}
+
+// with if statement
+fetchUnreadCount1(from: "https://www.hackingwithswift.com") { result in
+    if case .success(let count) = result {
+        print("if: \(count) unread messages.")
+    }
+}
+
