a758e6da1b5003d85afbcb6275a49e719e08f470
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-21 09:22:39 -0500

    add SwiftUIError to Skills in 2025_iOS_Playground

---
+++ Skills/SwiftUIError/SwiftUIError/SwiftUIErrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIErrorApp.swift
+//  SwiftUIError
+//
+//  Created by Thomas Cowern on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIErrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
