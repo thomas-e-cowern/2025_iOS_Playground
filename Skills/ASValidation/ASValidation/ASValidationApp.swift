4fcaef16e5b00baa465f096539a4d3dbb9460e4e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-22 08:58:07 -0500

    add ASValidation to Skills folder

---
+++ Skills/ASValidation/ASValidation/ASValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASValidationApp.swift
+//  ASValidation
+//
+//  Created by Thomas Cowern on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct ASValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
