8f5454812551acb2fc2f6018a79b0942bff79e6b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-22 10:44:08 -0500

    add SwiftfulThinking directory and CustomBindingPlayground project

---
+++ SwiftfulThinking/CustomBindingPlayground/CustomBindingPlayground/CustomBindingPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBindingPlaygroundApp.swift
+//  CustomBindingPlayground
+//
+//  Created by Thomas Cowern on 1/22/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomBindingPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
