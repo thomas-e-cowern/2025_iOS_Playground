a028df9a8078d24e4256cb09b140b9628c4b4649
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-23 10:10:12 -0500

    update CustomErrors with CustomAlerts

--- SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/CustomErrors.swift
+++ SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/CustomErrors.swift
@@ -23,3 +23,44 @@ enum CustomErrors: Error, LocalizedError {
         }
     }
 }
+
+enum CustomAlertss: Error, LocalizedError {
+    case noInternetConnection
+    case dataNotAvailable
+    case urlError(error: Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .noInternetConnection:
+            return "Please check your internet connection and try again."
+        case .dataNotAvailable:
+            return "Data is currently not available. Please try again later."
+        case .urlError(error: let error):
+            return "URL Error: \(error.localizedDescription)"
+        }
+    }
+    
+    var title: String {
+        switch self {
+        case .noInternetConnection:
+            return "Internet unavailable"
+        case .dataNotAvailable:
+            return "Data unavailable"
+        case .urlError:
+            return "Url unavailable"
+        }
+    }
+    
+    var subtitle: String? {
+        switch self {
+        case .noInternetConnection:
+            return "No internet connection is available"
+        case .dataNotAvailable:
+            return nil
+        case .urlError(let error):
+            return "The url you gave us is shit...."
+        }
+    }
+}
+
+
