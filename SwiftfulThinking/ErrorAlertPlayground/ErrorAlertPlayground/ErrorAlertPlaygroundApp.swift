effde3508f9184dd4726c832c3048c3e61beccaa
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-23 08:41:13 -0500

    add ErrorAlertPlayground to SwiftfulThinking

---
+++ SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/ErrorAlertPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorAlertPlaygroundApp.swift
+//  ErrorAlertPlayground
+//
+//  Created by Thomas Cowern on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ErrorAlertPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
