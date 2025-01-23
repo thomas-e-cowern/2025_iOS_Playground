434ff43575dea498dea18045cbd2effa4fd2a6d6
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-23 10:01:46 -0500

    update ContentView and CustomErrors to enable error alerts

--- SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/ContentView.swift
+++ SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/ContentView.swift
@@ -8,17 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    // MARK: - Properties
+    @State private var error: Error? = nil
+    
+    // MARK: - Body
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Click to Show Error") {
+                saveData()
+            }
+//            .alert(error?.localizedDescription ?? "Unknown Error", isPresented: Binding(value: $error)) {
+//                Button("OK") {
+//                    
+//                }
+//            }
+            .alert(error?.localizedDescription ?? "Unknow Error", isPresented: Binding(value: $error)) {
+                Button("Ok") {
+                        
+                }
+            } message: {
+                Text("Error message goes here...")
+            }
+
         }
         .padding()
     }
+    
+    // MARK: - Methods and functions
+    private func saveData() {
+        let isSuccessful: Bool = false
+        
+        if isSuccessful {
+            // do something
+        } else {
+            // error
+            let myError: Error = CustomErrors.dataNotAvailable
+            error = myError
+        }
+    }
 }
 
+// MARK: - Preview
 #Preview {
     ContentView()
 }
--- SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/CustomErrors.swift
+++ SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/CustomErrors.swift
@@ -7,6 +7,19 @@
 
 import Foundation
 
-enum CustomErrors: Error {
+enum CustomErrors: Error, LocalizedError {
     case noInternetConnection
+    case dataNotAvailable
+    case urlError(error: Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .noInternetConnection:
+            return "Please check your internet connection and try again."
+        case .dataNotAvailable:
+            return "Data is currently not available. Please try again later."
+        case .urlError(error: let error):
+            return "URL Error: \(error.localizedDescription)"
+        }
+    }
 }
