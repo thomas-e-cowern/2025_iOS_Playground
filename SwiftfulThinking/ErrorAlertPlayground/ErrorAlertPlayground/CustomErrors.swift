cfa1e60b9e9d6306543f76f2d8e9c6ce19d67877
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-23 09:23:52 -0500

    add CustomErrors to ErrorAlertPlayground

---
+++ SwiftfulThinking/ErrorAlertPlayground/ErrorAlertPlayground/CustomErrors.swift
@@ -0,0 +1,12 @@
+//
+//  CustomErrors.swift
+//  ErrorAlertPlayground
+//
+//  Created by Thomas Cowern on 1/23/25.
+//
+
+import Foundation
+
+enum CustomErrors: Error {
+    case noInternetConnection
+}
