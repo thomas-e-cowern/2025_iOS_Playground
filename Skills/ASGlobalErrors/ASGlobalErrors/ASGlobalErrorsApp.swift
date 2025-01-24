ac9b37e30dd2b9844da82c9a11927d1e7795b00e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-24 07:37:46 -0500

    ASGlobalErrors initial commit in Skills folder

---
+++ Skills/ASGlobalErrors/ASGlobalErrors/ASGlobalErrorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASGlobalErrorsApp.swift
+//  ASGlobalErrors
+//
+//  Created by Thomas Cowern on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ASGlobalErrorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Skills/ASGlobalErrors/ASGlobalErrors/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASGlobalErrors
+//
+//  Created by Thomas Cowern on 1/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
