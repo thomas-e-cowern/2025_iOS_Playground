0fea807c89d64cc2c953838a9e513c8fd5f0e5ed
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-24 09:39:35 -0500

    update Skills folder structure

---
+++ Skills/ErrorHandling/AppleErrorHandling/AppleErrorHandling/AppleErrorHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleErrorHandlingApp.swift
+//  AppleErrorHandling
+//
+//  Created by Thomas Cowern on 1/24/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleErrorHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
