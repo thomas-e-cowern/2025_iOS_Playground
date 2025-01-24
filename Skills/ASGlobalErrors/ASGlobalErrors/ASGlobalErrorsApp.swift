b4e3e55ea08c0e9be72749ec4f6b8a1ca50f16f9
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-24 08:41:14 -0500

    update ASGlobalErrorsApp file to inject error wrapper environment key

--- Skills/ASGlobalErrors/ASGlobalErrors/ASGlobalErrorsApp.swift
+++ Skills/ASGlobalErrors/ASGlobalErrors/ASGlobalErrorsApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct ASGlobalErrorsApp: App {
+    
+    @State private var errorWrapper: ErrorWrapper?
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.showError) { error, guidance in
+                    errorWrapper = ErrorWrapper(error: error, guidance: guidance)
+                }
+                .sheet(item: $errorWrapper) { errorWrapper in
+                    ErrorView(errorWrapper: errorWrapper)
+                }
         }
     }
 }
