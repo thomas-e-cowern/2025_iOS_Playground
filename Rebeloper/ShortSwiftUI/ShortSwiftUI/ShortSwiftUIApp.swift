9f65564953c1002e970ec9fa28cd667374950c75
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-25 11:20:09 -0500

    add ShortSwiftUI project to Rebeloper folder

---
+++ Rebeloper/ShortSwiftUI/ShortSwiftUI/ShortSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortSwiftUIApp.swift
+//  ShortSwiftUI
+//
+//  Created by Thomas Cowern on 1/25/25.
+//
+
+import SwiftUI
+
+@main
+struct ShortSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
