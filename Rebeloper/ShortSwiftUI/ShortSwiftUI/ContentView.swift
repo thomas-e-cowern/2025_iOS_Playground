062e0c389059197dd60ec17e0671b9d525e2c54a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-25 12:13:10 -0500

    update ContentView with various changes...

--- Rebeloper/ShortSwiftUI/ShortSwiftUI/ContentView.swift
+++ Rebeloper/ShortSwiftUI/ShortSwiftUI/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var name = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "sun.max.circle")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            Text(name)
+                .foregroundStyle(.red)
+                .background(.orange)
+//            Divider()
+//            Color.green
+                .font(.largeTitle)
+            Button("Tap Me") {
+                print("Tapped")
+            }
+//            .overlay(alignment: .bottomLeading) {
+//                Text("Hey There!")
+//            }
+            TextField("Name", text: $name)
+            
         }
         .padding()
+        .onAppear {
+            print("948 jklahsflhs")
+        }
     }
 }
 
