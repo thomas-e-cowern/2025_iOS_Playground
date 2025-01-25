764bac1b49b75276e7f8a12e402cdd3d5172b199
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-25 14:16:44 -0500

    add AppService to project

---
+++ Rebeloper/ShortSwiftUI/ShortSwiftUI/AppService.swift
@@ -0,0 +1,24 @@
+//
+//  AppService.swift
+//  ShortSwiftUI
+//
+//  Created by Thomas Cowern on 1/25/25.
+//
+
+import SwiftUI
+
+@Observable
+class AppService {
+    var posts = [Post]()
+    
+    func fetchPosts() async throws {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {
+            return
+        }
+        
+        let (data, _) = try await URLSession.shared.data(from: url)
+        
+        posts = try JSONDecoder().decode([Post].self, from: data)
+        
+    }
+}
--- Rebeloper/ShortSwiftUI/ShortSwiftUI/Post.swift
+++ Rebeloper/ShortSwiftUI/ShortSwiftUI/Post.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Profile: Codable, Identifiable, Hashable {
+struct Post: Codable, Identifiable, Hashable {
     let id: Int
     let userId: Int
     let title: String
