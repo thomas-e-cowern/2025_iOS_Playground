38adbdd656c710c3ceefa3a53307d688de723b5c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-26 09:54:50 -0500

    SwiftUIBootcamp initial commit

---
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/RebeloperCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebeloperCampApp.swift
+//  RebeloperCamp
+//
+//  Created by Thomas Cowern on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct RebeloperCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
