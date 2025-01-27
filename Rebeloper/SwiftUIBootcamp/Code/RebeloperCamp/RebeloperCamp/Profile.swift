956480c40777cd176f6cf0b2b53f2fc3bc434a9f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-27 10:37:57 -0500

    add Profile model to project

---
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  RebeloperCamp
+//
+//  Created by Thomas Cowern on 1/27/25.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    
+    init(name: String? = nil) {
+        self.name = name ?? ""
+    }
+}
