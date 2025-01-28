932713e71b7dde06e94c509839b92f2815a6af95
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-28 11:22:17 -0500

    update Profile to add score

--- Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/Profile.swift
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/Profile.swift
@@ -11,8 +11,27 @@ struct Profile: Codable, Identifiable {
     var id = UUID().uuidString
     
     var name: String
+    var score: Int
     
-    init(name: String? = nil) {
+    enum CodingKeys: String, CodingKey {
+        case name
+        case score
+    }
+    
+    func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.name, forKey: .name)
+    }
+    
+    init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        let score = try container.decode(Int.self, forKey: .score)
+        self.name = try container.decodeIfPresent(String.self, forKey: .name) ?? "Empty"
+        self.score = try container.decode(Int.self, forKey: .score)
+    }
+    
+    init(name: String? = nil, score: Int? = nil) {
         self.name = name ?? ""
+        self.score = score ?? 0
     }
 }
