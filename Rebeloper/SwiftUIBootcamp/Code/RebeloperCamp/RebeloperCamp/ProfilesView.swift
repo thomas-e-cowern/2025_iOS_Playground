0f1a8cbbf731f5d86e78c45a50e38ebc74c9217d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-28 10:31:18 -0500

    add ProfilesView to prject

---
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/ProfilesView.swift
@@ -0,0 +1,65 @@
+//
+//  ProfilesView.swift
+//  RebeloperCamp
+//
+//  Created by Thomas Cowern on 1/28/25.
+//
+
+import SwiftUI
+
+struct ProfilesView: View {
+    
+    @Environment(AppController.self) private var appController
+    
+    var body: some View {
+        VStack {
+            if appController.profiles.isEmpty {
+                Text("No Profiles Available")
+            } else {
+                List {
+                    ForEach(appController.profiles) { profile in
+                        Text(profile.name)
+                    }
+                }
+            }
+        }
+        .toolbar {
+            ToolbarItem(placement: .primaryAction) {
+                Button {
+                    createProfile()
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+        }
+        .onAppear {
+            Task {
+                do {
+                    try await appController.fetchProfiles()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    // MARK: - Methods and functions
+    func createProfile() {
+        Task {
+            do {
+                let profile = Profile(name: "New Profile")
+                try appController.createProfile(profile)
+                try await appController.fetchProfiles()
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        ProfilesView()
+            .environment(AppController())
+    }
+}
