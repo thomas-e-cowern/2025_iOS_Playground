ac9d43125d7f49e75a195162c5323337a3d3259d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-28 08:51:10 -0500

    add AppDelegate and GoogleService-Info.plist to projec to enable Firebase connection

---
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  RebeloperCamp
+//
+//  Created by Thomas Cowern on 1/28/25.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
