84174a9bdb5cd41576df157d78bdfc5c7c02be5a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-29 08:51:59 -0500

    add ColorItem model to project

---
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/ColorItem.swift
@@ -0,0 +1,13 @@
+//
+//  ColorItem.swift
+//  RebeloperCamp
+//
+//  Created by Thomas Cowern on 1/29/25.
+//
+
+import SwiftUI
+
+struct ColorItem: Identifiable {
+    let id = UUID().uuidString
+    let color: Color
+}
