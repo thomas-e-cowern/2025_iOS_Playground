89605918354083c366a9e5c7f0cb75b9cd8be081
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-30 09:16:10 -0500

    add List to MainView, add modifiers to List for testing

--- Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/MainView.swift
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/MainView.swift
@@ -29,48 +29,59 @@ struct MainView: View {
     
     var body: some View {
         
-        List {
-            ForEach(colors) { colorItem in
-                Text(colorItem.color.description)
-                    .foregroundStyle(colorItem.color)
+        VStack(spacing: nil) {
+            List() {
+                ForEach(colors) { colorItem in
+//                    colorCell(color: colorItem.color, text: colorItem.color.description)
+                    Text(colorItem.color.description)
+                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                            Button("Delete") {
+                                print(colorItem.color.hashValue)
+                            }
+                        }
+                }
+//                .onDelete { indexSet in
+//                    colors.remove(at: indexSet.first!)
+//                }
             }
+            .listStyle(.grouped)
         }
         
-//        VStack {
-//            VStack {
-//                Button("Scroll To") {
-//                    scrollPostition.scrollTo(id: Color.orange, anchor: .center)
-//                }
-//            }
-//            GeometryReader { proxy in
-//                //                ScrollViewReader { readerProxy in
-//                ScrollView(.horizontal) {
-//                    LazyHGrid(rows: gridItems, alignment: .center, spacing: 20, pinnedViews: [.sectionFooters, .sectionHeaders]) {
-//                        //                Section {
-//                        ForEach(colors) { colorItem in
-//                            cell(colorItem, proxy: proxy)
-//                                .id(colorItem.color)
-//                                .onScrollVisibilityChange(threshold: 0.1) { visible in
-//                                    print("Color \(colorItem.color.description) is \(visible)")
-//                                }
-//                        }
-//                        //                } header: {
-//                        //                    Text("This is the header")
-//                        //                } footer: {
-//                        //                    Text("This is the footer")
-//                        //                }
-//                    }
-//                }
-//                .scrollIndicators(.hidden)
-//                .scrollPosition($scrollPostition)
-//                .onScrollPhaseChange { oldPhase, newPhase, context in
-//                    print("OP: \(oldPhase)")
-//                    print("NP: \(newPhase)")
-//                    print("CT: \(context.geometry.contentOffset.x)")
-//                }
-//                //                }
-//            }
-//        }
+        //        VStack {
+        //            VStack {
+        //                Button("Scroll To") {
+        //                    scrollPostition.scrollTo(id: Color.orange, anchor: .center)
+        //                }
+        //            }
+        //            GeometryReader { proxy in
+        //                //                ScrollViewReader { readerProxy in
+        //                ScrollView(.horizontal) {
+        //                    LazyHGrid(rows: gridItems, alignment: .center, spacing: 20, pinnedViews: [.sectionFooters, .sectionHeaders]) {
+        //                        //                Section {
+        //                        ForEach(colors) { colorItem in
+        //                            cell(colorItem, proxy: proxy)
+        //                                .id(colorItem.color)
+        //                                .onScrollVisibilityChange(threshold: 0.1) { visible in
+        //                                    print("Color \(colorItem.color.description) is \(visible)")
+        //                                }
+        //                        }
+        //                        //                } header: {
+        //                        //                    Text("This is the header")
+        //                        //                } footer: {
+        //                        //                    Text("This is the footer")
+        //                        //                }
+        //                    }
+        //                }
+        //                .scrollIndicators(.hidden)
+        //                .scrollPosition($scrollPostition)
+        //                .onScrollPhaseChange { oldPhase, newPhase, context in
+        //                    print("OP: \(oldPhase)")
+        //                    print("NP: \(newPhase)")
+        //                    print("CT: \(context.geometry.contentOffset.x)")
+        //                }
+        //                //                }
+        //            }
+        //        }
     }
     
     func cell(_ colorItem: ColorItem, proxy: GeometryProxy) -> some View {
@@ -82,6 +93,26 @@ struct MainView: View {
                 .foregroundStyle(Color.white)
         }
         
+    }
+}
+
+struct colorCell: View {
+    
+    var color: Color
+    var text: String
+    
+    var body: some View {
+        VStack(spacing: nil) {
+            ZStack {
+                color
+                if color.description == "black" {
+                    Text(text)
+                        .foregroundStyle(.white)
+                } else {
+                    Text(text)
+                }
+            }
+        }
     }
 }
 
