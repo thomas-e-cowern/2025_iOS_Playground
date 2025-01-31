2939aa9aa41ff4032dfc8cdb16828bdaf05c5d7b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-31 08:26:11 -0500

    add NavRoot to project

---
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/NavRoot.swift
@@ -0,0 +1,22 @@
+//
+//  NavRoot.swift
+//  RebeloperCamp
+//
+//  Created by Thomas Cowern on 1/31/25.
+//
+
+import SwiftUI
+
+struct NavRoot: View {
+    var body: some View {
+        NavigationStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+    }
+}
+
+#Preview {
+    NavigationStack {
+        NavRoot()
+    }
+}
