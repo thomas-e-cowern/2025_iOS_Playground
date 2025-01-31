8dd50879fa5360e9a652dfc0fc4d6b4e17cbe145
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-01-31 09:56:53 -0500

    udpate Destination enum with View conformance and switch statement

--- Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/Destination.swift
+++ Rebeloper/SwiftUIBootcamp/Code/RebeloperCamp/RebeloperCamp/Destination.swift
@@ -7,8 +7,19 @@
 
 import SwiftUI
 
-enum Destination: String, CaseIterable {
+enum Destination: Hashable, View {
     case view1
     case view2
     case view3
+    
+    var body: some View {
+        switch self {
+        case .view1:
+            MyView1()
+        case .view2:
+            MyView2()
+        case .view3:
+            MyView3()
+        }
+    }
 }
