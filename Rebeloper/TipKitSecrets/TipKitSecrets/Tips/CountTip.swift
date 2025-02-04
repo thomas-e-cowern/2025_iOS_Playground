afc9ecef5b0193a9de3ad201bf63a592a4aae242
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-04 13:56:06 -0500

    add CountTip to project, update CountTip with tip functions

---
+++ Rebeloper/TipKitSecrets/TipKitSecrets/Tips/CountTip.swift
@@ -0,0 +1,15 @@
+//
+//  CountTip.swift
+//  TipKitSecrets
+//
+//  Created by Thomas Cowern on 2/4/25.
+//
+
+import SwiftUI
+import TipKit
+
+struct CountTip: Tip {
+    var title: Text { Text("Tap the button to count up") }
+    var message: Text? { Text("The text will change when you tap it.") }
+    var image: Image? { Image(systemName: "hand.tap.fill") }
+}
