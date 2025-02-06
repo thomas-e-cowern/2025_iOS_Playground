d2d95ee6a188a21f1e1db1d5f70e20c56194cb1a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-06 09:14:06 -0500

    update ContentView and WishModel with SwiftData configuration

--- SwiftUIMasterClass/WishList/WishList/WishListApp.swift
+++ SwiftUIMasterClass/WishList/WishList/WishListApp.swift
@@ -6,12 +6,14 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct WishListApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(for: Wish.self)
         }
     }
 }
