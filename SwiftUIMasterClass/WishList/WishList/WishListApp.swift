fd9de4ea1f28a69935cbd92654417d090c30a14a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-06 08:37:55 -0500

    WishList initial commit in SwiftUIMasterClass

---
+++ SwiftUIMasterClass/WishList/WishList/WishListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListApp.swift
+//  WishList
+//
+//  Created by Thomas Cowern on 2/6/25.
+//
+
+import SwiftUI
+
+@main
+struct WishListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
