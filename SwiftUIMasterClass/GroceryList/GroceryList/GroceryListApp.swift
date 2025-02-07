a4a0871ab476d78f91f1e4d230608fd59b29155d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-07 07:59:00 -0500

    GroceryList initial commit in 2025_iOS_Playground

---
+++ SwiftUIMasterClass/GroceryList/GroceryList/GroceryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListApp.swift
+//  GroceryList
+//
+//  Created by Thomas Cowern on 2/7/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
