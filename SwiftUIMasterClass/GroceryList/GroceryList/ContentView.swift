4ee3cae2489ae22841a3b2e782199b51998f953c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-07 09:35:57 -0500

    add function to populate Items in memory to ContentView

--- SwiftUIMasterClass/GroceryList/GroceryList/ContentView.swift
+++ SwiftUIMasterClass/GroceryList/GroceryList/ContentView.swift
@@ -14,16 +14,55 @@ struct ContentView: View {
     @Query private var items: [Item]
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(items) { item in
+                    Text(item.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
+                        .foregroundStyle(item.isCompleted == false ? Color.primary : Color.accentColor)
+                        .strikethrough(item.isCompleted)
+                        .italic(item.isCompleted)
+                }
+            }
+            .navigationTitle("Grocery List")
+            .overlay {
+                if items.isEmpty {
+                    ContentUnavailableView("Enpty Cart", image: "cart.circle", description: Text("Add some items to the shopping list!"))
+                }
+            }
         }
-        .padding()
+    }
+    
+    func addEssentialFoods() {
+        modelContext.insert(Item(title: "Bakery & Bread", isCompleted: false))
+        modelContext.insert(Item(title: "Meat & Seafood", isCompleted: true))
+        modelContext.insert(Item(title: "Cereals", isCompleted: .random()))
+        modelContext.insert(Item(title: "Pasta & Rice", isCompleted: .random()))
+        modelContext.insert(Item(title: "Cheese & Eggs", isCompleted: .random()))
     }
 }
 
-#Preview {
+#Preview("Empty List") {
     ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
+}
+
+#Preview("With Data") {
+    let sampleData: [Item] = [
+        Item(title: "Bakery & Bread", isCompleted: false),
+        Item(title: "Meat & Seafood", isCompleted: true),
+        Item(title: "Cereals", isCompleted: .random()),
+        Item(title: "Pasta & Rice", isCompleted: .random()),
+        Item(title: "Cheese & Eggs", isCompleted: .random())
+    ]
+    
+    let container = try! ModelContainer(for: Item.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    for item in sampleData {
+        container.mainContext.insert(item)
+    }
+    
+    return ContentView()
+        .modelContainer(container)
 }
