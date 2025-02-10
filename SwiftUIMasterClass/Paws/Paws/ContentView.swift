053715b8c8c5d7a12cf2d30c9d33f6496960dc81
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-10 10:14:51 -0500

    add Grid to ContentView

--- SwiftUIMasterClass/Paws/Paws/ContentView.swift
+++ SwiftUIMasterClass/Paws/Paws/ContentView.swift
@@ -13,11 +13,50 @@ struct ContentView: View {
     @Environment(\.modelContext) var modelContext
     @Query private var pets: [Pet]
     
+    let layout = [
+        GridItem(.flexible(minimum: 120)),
+        GridItem(.flexible(minimum: 120))
+    ]
+    
     var body: some View {
         NavigationStack {
             ScrollView {
-                EmptyView()
+                LazyVGrid(columns: layout) {
+                    GridRow {
+                        ForEach(pets) { pet in
+                            NavigationLink(destination: EmptyView()) {
+                                VStack {
+                                    // MARK: - Image retrieval
+                                    if let imageData = pet.photo {
+                                        if let image = UIImage(data: imageData) {
+                                            Image(uiImage: image)
+                                        }
+                                    } else {
+                                        Image(systemName: "pawprint.circle")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .padding(40)
+                                            .foregroundStyle(.quaternary)
+                                    }
+                                    
+                                    Spacer()
+                                    Text(pet.name)
+                                        .font(.title.weight(.light))
+                                        .padding(.vertical)
+                                    
+                                    Spacer()
+                                }
+                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
+                                .background(.ultraThinMaterial)
+                                .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                            }
+                            .foregroundStyle(.primary)
+                        }
+                    }
+                }
+                .padding(.horizontal)
             }
+            .navigationTitle(pets.isEmpty ? "" : "Paws")
             .overlay {
                 if pets.isEmpty {
                     CustomContentUnavailableView(icon: "dog.circle", title: "No Pets", description: "Add a new pet to get started!")
@@ -25,6 +64,11 @@ struct ContentView: View {
             }
         }
     }
+}
+
+#Preview("With Data") {
+    ContentView()
+        .modelContainer(Pet.preview)
 }
 
 #Preview("No Data") {
