447ff0916451ac854743d28c176e8393a13b6e84
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-10 07:54:25 -0500

    add Photos and Icons to Paws project

---
+++ SwiftUIMasterClass/Paws/Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Thomas Cowern on 2/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
