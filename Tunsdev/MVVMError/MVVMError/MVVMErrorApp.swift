3b923b893eea062755ee4431c8c7267e460992bd
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-11 07:24:44 -0500

    add MVVMError project in Tunsdev folder

---
+++ Tunsdev/MVVMError/MVVMError/MVVMErrorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMErrorApp.swift
+//  MVVMError
+//
+//  Created by Thomas Cowern on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMErrorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
