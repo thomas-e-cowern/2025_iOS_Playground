5ab7b39e4d04748d4ca3b1f64d54c96710bc1813
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-12 08:59:28 -0500

    add NetworkPro project to Skills folder

---
+++ Skills/NetworkPro/NetworkPro/NetworkProApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkProApp.swift
+//  NetworkPro
+//
+//  Created by Thomas Cowern on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
