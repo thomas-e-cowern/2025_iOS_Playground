88e9e5985ec5b604d7ff9a0d602b582401f9478f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-13 12:50:55 -0500

    add AzamSharp folder to folder, GardenApp initial commit

---
+++ AzamSharp/GardenApp/GardenApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GardenApp
+//
+//  Created by Thomas Cowern on 2/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ AzamSharp/GardenApp/GardenApp/GardenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GardenAppApp.swift
+//  GardenApp
+//
+//  Created by Thomas Cowern on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct GardenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
