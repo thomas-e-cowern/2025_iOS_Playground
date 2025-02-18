b442a81e440b02a197f891da456e79bfca359382
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-18 09:07:17 -0500

    add Models folder and Budget model to project

---
+++ Skip/skip-budget/Sources/SkipBudget/Models/Budget.swift
@@ -0,0 +1,13 @@
+//
+//  Budget.swift
+//  skip-budget
+//
+//  Created by Thomas Cowern on 2/18/25.
+//
+import Foundation
+
+struct Budget {
+    var id: Int
+    var name: String
+    var amount: Double
+}
