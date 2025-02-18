88eed66957af7b17ff08c1c7e33dc4157a9638d4
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-18 09:21:56 -0500

    add Stores folder, add DataStore file

---
+++ Skip/skip-budget/Sources/SkipBudget/Stores/DataStore.swift
@@ -0,0 +1,45 @@
+//
+//  DataStore.swift
+//  skip-budget
+//
+//  Created by Thomas Cowern on 2/18/25.
+//
+
+import Foundation
+import SkipSQL
+import Observation
+
+@Observable
+class DataStore {
+    private let context: SQLContext
+    
+    var budgets: [Budget] = []
+    
+    init(context: SQLContext) {
+        self.context = context
+    }
+    
+    func saveBudget(budget: Budget) throws {
+        let statement = try context.prepare(sql: "INSERT INTO Budgets(name, amount) VALUES (?, ?); ")
+        defer {
+            do {
+                try statement.close()
+            } catch {
+                print("Error in saveBudget: \(error.localizedDescription)")
+            }
+        } // MARK: - End of defer
+        
+        try context.transaction {
+            let params: [SQLValue] = [
+                SQLValue.text(budget.name),
+                SQLValue.float(budget.amount)
+            ]
+            
+            try statement.update(parameters: params)
+            
+            var insertBudget = budget
+            insertBudget.id = context.lastInsertRowID
+            budgets.append(insertBudget)
+        }
+    }
+}
