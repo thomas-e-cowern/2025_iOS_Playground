becf8983d7d11c54fdb5c3efae97f131207e5167
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-19 10:43:59 -0500

    add OptionView to project

---
+++ AzamSharp/GardenApp/GardenApp/Views/OptionView.swift
@@ -0,0 +1,21 @@
+//
+//  OptionView.swift
+//  GardenApp
+//
+//  Created by Thomas Cowern on 2/19/25.
+//
+
+import SwiftUI
+
+struct OptionView: View {
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OptionView()
+}
