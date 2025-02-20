a2cc5aad34567a8bdaf91997165f262d6e947236
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-20 10:29:05 -0500

    SACustomList initial commit in Skills/Lists forlder

---
+++ Skills/Lists/SACustomList/SACustomList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SACustomList
+//
+//  Created by Thomas Cowern on 2/20/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skills/Lists/SACustomList/SACustomList/SACustomListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SACustomListApp.swift
+//  SACustomList
+//
+//  Created by Thomas Cowern on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SACustomListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
