2e887884b2bbcd8b4497e155275be2c8f9891abf
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-21 09:20:09 -0500

    add MyGardenView to project

---
+++ AzamSharp/GardenApp/GardenApp/Views/MyGardenView.swift
@@ -0,0 +1,36 @@
+//
+//  MyGardenView.swift
+//  GardenApp
+//
+//  Created by Thomas Cowern on 2/21/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MyGardenView: View {
+    
+    @Query private var myGardenVegetabes: [MyGardenVegetable]
+    var body: some View {
+        List(myGardenVegetabes) { myVeggy in
+            Text(myVeggy.vegetable.name)
+        }
+    }
+}
+
+#Preview("Light Mode") {
+    NavigationStack {
+        MyGardenView()
+    }
+    .modelContainer(previewContainer)
+    .preferredColorScheme(.light)
+}
+
+#Preview("Dark Mode") {
+    NavigationStack {
+        MyGardenView()
+    }
+    .modelContainer(previewContainer)
+    .preferredColorScheme(.dark)
+}
+
