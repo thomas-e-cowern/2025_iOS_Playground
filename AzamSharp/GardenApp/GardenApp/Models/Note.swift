bdb2d3979f1c6dbff5a2f9019cfe1ad2f3523b21
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-24 10:03:57 -0500

    add Note model to project

---
+++ AzamSharp/GardenApp/GardenApp/Models/Note.swift
@@ -0,0 +1,24 @@
+//
+//  Note.swift
+//  GardenApp
+//
+//  Created by Thomas Cowern on 2/24/25.
+//
+import Foundation
+import SwiftData
+
+@Model
+class Note {
+    var title: String
+    var body: String
+    var dateCreated: Date
+    @Attribute(.externalStorage) var photo: Date? = nil
+    
+    var myGardenVegatable: MyGardenVegetable?
+    
+    init(title: String, body: String, dateCreated: Date = Date()) {
+        self.title = title
+        self.body = body
+        self.dateCreated = dateCreated
+    }
+}
