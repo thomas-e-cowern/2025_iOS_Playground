60a2034f364cd522f8792023362dce799950a43f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-24 11:09:12 -0500

    add NoteListView to project

---
+++ AzamSharp/GardenApp/GardenApp/Views/NoteListView.swift
@@ -0,0 +1,50 @@
+//
+//  NoteListView.swift
+//  GardenApp
+//
+//  Created by Thomas Cowern on 2/24/25.
+//
+
+import SwiftUI
+
+struct NoteListView: View {
+    
+    let myGardenVegetable: MyGardenVegetable
+    @State private var addNotePresented: Bool = false
+    
+    var body: some View {
+        VStack {
+            if myGardenVegetable.notes != nil {
+                List(myGardenVegetable.notes ?? []) { note in
+                    Text(note.title)
+                }
+            } else {
+                Text("You have not notes yet.  Let's add some...")
+            }
+        } //: End of VStack
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button("Add Note") {
+                    addNotePresented = true
+                }
+            }
+        }
+        .sheet(isPresented: $addNotePresented) {
+            //
+        }
+    }
+}
+
+#Preview("Light Mode") {
+    NavigationStack {
+        NoteListView(myGardenVegetable: MyGardenVegetable.sampleVegetables[1])
+            .preferredColorScheme(.light)
+    }
+}
+
+#Preview("Dark Mode") {
+    NavigationStack {
+        NoteListView(myGardenVegetable: MyGardenVegetable.sampleVegetables[1])
+            .preferredColorScheme(.dark)
+    }
+}
