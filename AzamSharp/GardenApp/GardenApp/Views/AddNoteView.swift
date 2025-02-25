71a4bf5f8ea984734a9858c6ef19074c898af4d2
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-25 09:16:25 -0500

    update AddNoteView with PhotosPicker and associated formatting

--- AzamSharp/GardenApp/GardenApp/Views/AddNoteView.swift
+++ AzamSharp/GardenApp/GardenApp/Views/AddNoteView.swift
@@ -6,6 +6,7 @@
 //
 import SwiftUI
 import SwiftData
+import PhotosUI
 
 struct AddNoteView: View {
     
@@ -16,12 +17,50 @@ struct AddNoteView: View {
     @State private var noteTitle: String = ""
     @State private var noteBody: String = ""
     
+    @State private var selectedPhoto: PhotosPickerItem? = nil
+    @State private var uiImage: UIImage?
+    @State private var imageData: Data?
+    
     var body: some View {
         Form {
             TextField("Title", text: $noteTitle)
             TextEditor(text: $noteBody)
                 .frame(minHeight: 200)
                 .multilineTextAlignment(.leading)
+            
+            HStack(spacing: 20) {
+                PhotosPicker(selection: $selectedPhoto, matching: .images, photoLibrary: .shared()) {
+                    ZStack {
+                        Circle()
+                            .fill(Color.blue.opacity(0.2))
+                            .frame(width: 60, height: 60)
+                        Image(systemName: "photo.on.rectangle.angled.fill")
+                            .font(.title)
+                            .foregroundStyle(Color.blue)
+                    }
+                }
+            }
+            .buttonStyle(.borderless)
+            
+            if let uiImage {
+                Image(uiImage: uiImage)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+            }
+        }
+        .task(id: selectedPhoto) {
+            if let selectedPhoto {
+                do {
+                    if let data = try await selectedPhoto.loadTransferable(type: Data.self) {
+                        uiImage = UIImage(data: data)
+                        imageData = data
+                    }
+                } catch {
+                    print("There was a problem with selectedPhoto: \(error.localizedDescription)")
+                    // MARK: TODO - Add error throwing here... photoErrors
+                }
+            }
         }
         .toolbar {
             ToolbarItem(placement: .topBarLeading) {
