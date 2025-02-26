084ca2a658c80704cb28977d9750c201740c9858
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-26 10:32:21 -0500

    update EmptyListView with Group, Tabs and formatting

--- SwiftUIMasterClass/MovieList/MovieList/Views/EmptyListView.swift
+++ SwiftUIMasterClass/MovieList/MovieList/Views/EmptyListView.swift
@@ -8,20 +8,35 @@ import SwiftUI
 
 struct EmptyListView: View {
     var body: some View {
-        Circle()
-            .fill(.blue.gradient)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
-            .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
-            .frame(width: 180)
-            .overlay {
-                Image(systemName: "movieclapper")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100)
-                    .foregroundStyle(Color(UIColor.secondarySystemBackground))
-                    .padding(.top, -6)
+        ContentUnavailableView {
+            Circle()
+                .fill(.blue.gradient)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 30)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 60)
+                .stroke(.blue.gradient.opacity(0.15), lineWidth: 90)
+                .frame(width: 180)
+                .overlay {
+                    Image(systemName: "movieclapper")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100)
+                        .foregroundStyle(Color(UIColor.secondarySystemBackground))
+                        .padding(.top, -6)
+                }
+                .padding(.bottom, 90)
+        } description: {
+            GroupBox {
+                TabView {
+                    PageTabView(icon: "1.circle", description: "Add some movies to your list to watch at a later date!")
+                        .padding(.bottom, 36)
+                    
+                    PageTabView(icon: "2.circle", description: "Later you can also choose a random movie to watch")
+                        .padding(.bottom, 36)
+                }
+                .tabViewStyle(PageTabViewStyle())
+                .frame(minWidth: 0, maxWidth: 560, minHeight: 120, maxHeight: 180)
             }
+        }
     }
 }
 
