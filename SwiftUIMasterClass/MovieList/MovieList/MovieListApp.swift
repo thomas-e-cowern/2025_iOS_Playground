be154d2310fbece44e600aa9a6e1525271b33798
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-26 08:41:35 -0500

    add MovieList project to the SwiftUIMasterClass folder

---
+++ SwiftUIMasterClass/MovieList/MovieList/MovieListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListApp.swift
+//  MovieList
+//
+//  Created by Thomas Cowern on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
