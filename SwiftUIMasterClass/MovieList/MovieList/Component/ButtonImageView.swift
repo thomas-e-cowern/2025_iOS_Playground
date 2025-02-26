7498c0682ca8f3fc743f600569af05b1f7981bb8
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-26 15:08:40 -0500

    add ButtonImageView andNewMovieView to project

---
+++ SwiftUIMasterClass/MovieList/MovieList/Component/ButtonImageView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonImageView.swift
+//  MovieList
+//
+//  Created by Thomas Cowern on 2/26/25.
+//
+
+import SwiftUI
+
+struct ButtonImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonImageView()
+}
---
+++ SwiftUIMasterClass/MovieList/MovieList/Views/NewMovieView.swift
@@ -0,0 +1,18 @@
+//
+//  NewMovieView.swift
+//  MovieList
+//
+//  Created by Thomas Cowern on 2/26/25.
+//
+
+import SwiftUI
+
+struct NewMovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewMovieView()
+}
