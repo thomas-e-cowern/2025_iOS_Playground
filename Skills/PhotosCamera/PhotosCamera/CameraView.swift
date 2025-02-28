2575cdf911a2de159290400c4ac5fbeaeb0895cf
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-28 16:17:14 -0500

    add CameraView to project

--- Skills/PhotosCamera/PhotosCamera/CameraView.swift
+++ Skills/PhotosCamera/PhotosCamera/CameraView.swift
@@ -6,13 +6,47 @@
 //
 
 import SwiftUI
+import UIKit
 
-struct CameraView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CameraView: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController() // Create the Camera picker
+        picker.delegate = context.coordinator // Set the cooordinator as delegate
+        picker.sourceType = .camera // set the source to the camera
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {
+        // Left empty on purpose...no updates needed
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: CameraView
+        
+        init(_ parent: CameraView) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.image = image
+            }
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+        
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            parent.presentationMode.wrappedValue.dismiss()
+        }
     }
 }
 
 #Preview {
-    CameraView()
+    CameraView(image: .constant(UIImage(named: "camera")!))
 }
