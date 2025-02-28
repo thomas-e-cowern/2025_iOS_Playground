169f22c000bd1c43d41d22c7301f247a64135dc1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-28 17:47:31 -0500

    update ContentView to use CameraView, add default image for development

--- Skills/PhotosCamera/PhotosCamera/ContentView.swift
+++ Skills/PhotosCamera/PhotosCamera/ContentView.swift
@@ -6,14 +6,67 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct ContentView: View {
+    
+    @State private var photoPickerItem: PhotosPickerItem?
+    @State private var selectedImage: UIImage?
+    @State private var isShowingCamera: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let selectedImage = selectedImage {
+                Image(uiImage: selectedImage)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+            } else {
+#if DEBUG
+                Image("camera")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 300)
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+#endif
+            }
+            
+            PhotosPicker(selection: $photoPickerItem, matching: .images, photoLibrary: .shared())
+            {
+                Text("Select an image")
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue.opacity(0.2))
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+            }
+            .onChange(of: photoPickerItem) { _, newItem in
+                if let newItem = newItem{
+                    Task{
+                        if let data = try? await newItem.loadTransferable(type: Data.self),
+                           let image = UIImage(data: data){
+                            selectedImage = image
+                        }
+                    }
+                }
+            }
+            
+            Button {
+                isShowingCamera.toggle()
+            } label: {
+                Image(systemName: "camera.fill")
+                    .font(.headline)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue.opacity(0.2))
+                    .clipShape(RoundedRectangle(cornerRadius: 25))
+            }
+            .sheet(isPresented: $isShowingCamera) {
+                CameraView(image: $selectedImage)
+            }
+            
+            
         }
         .padding()
     }
