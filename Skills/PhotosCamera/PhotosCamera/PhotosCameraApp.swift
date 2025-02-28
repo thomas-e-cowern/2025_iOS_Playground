2b263d47e415dc55d29f13a0bd9d00a9c13b346a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-02-28 14:43:27 -0500

    PhotosCamera initial commit in Skills folder

---
+++ Skills/PhotosCamera/PhotosCamera/PhotosCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosCameraApp.swift
+//  PhotosCamera
+//
+//  Created by Thomas Cowern on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotosCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
