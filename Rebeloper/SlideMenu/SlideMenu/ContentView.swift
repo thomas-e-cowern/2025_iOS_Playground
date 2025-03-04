94f0cd3d2b87473908191218c7f42604f338c7b8
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-04 15:42:24 -0500

    add SlideMenu to Rebeloper folder

---
+++ Rebeloper/SlideMenu/SlideMenu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideMenu
+//
+//  Created by Thomas Cowern on 3/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rebeloper/SlideMenu/SlideMenu/SlideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideMenuApp.swift
+//  SlideMenu
+//
+//  Created by Thomas Cowern on 3/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SlideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
