26ac9014acd29bc3798a94c620b0dccc7b12bad5
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-05 07:30:46 -0500

    add MenuView and SlideInView to project

---
+++ Rebeloper/SlideMenu/SlideMenu/MenuView.swift
@@ -0,0 +1,18 @@
+//
+//  MenuView.swift
+//  SlideMenu
+//
+//  Created by Thomas Cowern on 3/5/25.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Menu View")
+    }
+}
+
+#Preview {
+    MenuView()
+}
