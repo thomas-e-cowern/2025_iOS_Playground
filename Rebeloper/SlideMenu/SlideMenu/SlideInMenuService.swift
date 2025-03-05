4b9cf9605f0e732c8adad16ea165d22951cb2f11
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-05 07:35:50 -0500

    add SlideInMenuService to project

---
+++ Rebeloper/SlideMenu/SlideMenu/SlideInMenuService.swift
@@ -0,0 +1,12 @@
+//
+//  SlideInMenuService.swift
+//  SlideMenu
+//
+//  Created by Thomas Cowern on 3/5/25.
+//
+
+import SwiftUI
+
+class SlideInMenuService: ObservableObject {
+    @Published var isPresented: Bool = false
+}
