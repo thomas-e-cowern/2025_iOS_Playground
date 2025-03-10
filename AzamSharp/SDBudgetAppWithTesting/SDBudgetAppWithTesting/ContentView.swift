73c75c2079e28f3cd0c9a37c2dff498b0d7bf697
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-10 15:34:57 -0400

    general update

---
+++ AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SDBudgetAppWithTesting
+//
+//  Created by Thomas Cowern on 3/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
