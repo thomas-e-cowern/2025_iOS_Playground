d7d0832e8606c4b6248bfd6ba430c328b78fe507
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-10 15:58:48 -0400

    delete ContentView from project

--- AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/SDBudgetAppWithTestingApp.swift
+++ AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/SDBudgetAppWithTestingApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SDBudgetAppWithTestingApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            BudgetListScreen()
         }
     }
 }
