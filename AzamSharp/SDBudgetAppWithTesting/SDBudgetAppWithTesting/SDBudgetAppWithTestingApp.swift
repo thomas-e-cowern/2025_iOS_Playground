d57ada24c4fbaffb67a1b93883aae21aa749f4ae
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-10 15:55:29 -0400

    add SDBudgetAppWithTesting to AzamSharp folder

---
+++ AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/SDBudgetAppWithTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDBudgetAppWithTestingApp.swift
+//  SDBudgetAppWithTesting
+//
+//  Created by Thomas Cowern on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SDBudgetAppWithTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
