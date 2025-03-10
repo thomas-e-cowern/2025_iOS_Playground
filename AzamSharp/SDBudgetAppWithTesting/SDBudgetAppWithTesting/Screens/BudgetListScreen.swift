ec13ab0cc1c9e3d7778e9108cada68c497236270
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-10 16:20:47 -0400

    update BudgetListScreen with Form, TextFields, Button and formatting

--- AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/Screens/BudgetListScreen.swift
+++ AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/Screens/BudgetListScreen.swift
@@ -6,13 +6,46 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct BudgetListScreen: View {
+    
+    @State private var name: String = ""
+    @State private var limit: Double?
+    
+    private var isFormValid: Bool {
+        !name.trimmingCharacters(in: .whitespaces).isEmpty && limit != nil
+    }
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing: 12) {
+                Form {
+                    Section("New Budget") {
+                        TextField("Budget Name", text: $name)
+                        TextField("Budget Amount", value: $limit, format: .number)
+                            .keyboardType(.numberPad)
+                    }
+                }
+            }
+            
+            VStack {
+                Button("Add Budget") {
+                    // MARK: TODO - Code to add the budget
+                }
+                .disabled(!isFormValid)
+            }
+            .navigationTitle("Budgets")
+        }
+    }
+    
+    // MARK: - Methods and functions
+    private func saveBudget() {
+        let budget = Budget(name: name, limit: limit!)
     }
 }
 
 #Preview {
     BudgetListScreen()
+        .modelContainer(for: Budget.self)
 }
