5ea09acb69927e7f36f91837f5acb7bb74daa814
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-10 15:58:28 -0400

    add BudgetListScreen to project

---
+++ AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/Screens/BudgetListScreen.swift
@@ -0,0 +1,18 @@
+//
+//  BudgetListScreen.swift
+//  SDBudgetAppWithTesting
+//
+//  Created by Thomas Cowern on 3/10/25.
+//
+
+import SwiftUI
+
+struct BudgetListScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BudgetListScreen()
+}
