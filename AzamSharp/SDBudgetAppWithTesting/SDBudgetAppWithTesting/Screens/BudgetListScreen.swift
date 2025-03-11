e8776d7f3257fbe2397a07fec3d46a74fcc52e49
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-11 09:14:49 -0400

    update BudgetListScreen with Budgets section and update saveBudget function to save data and clear TextFields

--- AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/Screens/BudgetListScreen.swift
+++ AzamSharp/SDBudgetAppWithTesting/SDBudgetAppWithTesting/Screens/BudgetListScreen.swift
@@ -10,38 +10,55 @@ import SwiftData
 
 struct BudgetListScreen: View {
     
+    // MARK: - Properties
+    @Environment(\.modelContext) private var context
+    @Query private var budgets: [Budget]
+    
     @State private var name: String = ""
     @State private var limit: Double?
     
     private var isFormValid: Bool {
         !name.trimmingCharacters(in: .whitespaces).isEmpty && limit != nil
     }
-
+    
     var body: some View {
-        NavigationStack {
-            VStack(spacing: 12) {
-                Form {
-                    Section("New Budget") {
-                        TextField("Budget Name", text: $name)
-                        TextField("Budget Amount", value: $limit, format: .number)
-                            .keyboardType(.numberPad)
+        
+        VStack(spacing: 12) {
+            Form {
+                Section("New Budget") {
+                    TextField("Budget Name", text: $name)
+                    TextField("Budget Amount", value: $limit, format: .number)
+                        .keyboardType(.numberPad)
+                }
+                
+                Section("Budgets") {
+                    List(budgets) { budget in
+                        HStack {
+                            Text(budget.name)
+                            Spacer()
+                            Text(budget.limit, format: .currency(code: Locale.current.identifier))
+                        }
                     }
                 }
             }
-            
-            VStack {
-                Button("Add Budget") {
-                    // MARK: TODO - Code to add the budget
-                }
-                .disabled(!isFormValid)
-            }
-            .navigationTitle("Budgets")
         }
+        
+        VStack {
+            Button("Add Budget") {
+                saveBudget()
+            }
+            .disabled(!isFormValid)
+        }
+        .navigationTitle("Budgets")
+        
     }
     
     // MARK: - Methods and functions
     private func saveBudget() {
         let budget = Budget(name: name, limit: limit!)
+        context.insert(budget)
+        name = ""
+        limit = nil
     }
 }
 
