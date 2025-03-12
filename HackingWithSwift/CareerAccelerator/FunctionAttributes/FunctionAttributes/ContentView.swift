7784fb9920f84238198c8af8c8442ba08f938d34
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-12 15:32:50 -0400

    update ContentView with @discardableResult to see difference in usage

--- HackingWithSwift/CareerAccelerator/FunctionAttributes/FunctionAttributes/ContentView.swift
+++ HackingWithSwift/CareerAccelerator/FunctionAttributes/FunctionAttributes/ContentView.swift
@@ -26,13 +26,48 @@ struct ContentView: View {
                 .background(Color.blue)
                 .clipShape(.capsule)
                 .titleStyle()
+                .task {
+                    await test_namesAreLoaded()
+                }
         }
         .padding()
         
         
     }
+    
+    func example() {
+        var albums = Set<String>()
+        albums.insert("Bob Marley")
+        albums.insert("Jimmy Buffett")
+        
+        let result = albums.insert("Jimmy Buffett")
+        print(result.inserted)
+        print(result.memberAfterInsert)
+    }
+    
+    func test_namesAreLoaded() async {
+        let sut = Band()
+        
+        let result = sut.loadData()
+        await result.value
+        await print(result.value)
+        
+        assert(sut.names.count == 4, "Calling loadData() should load four names.")
+    }
 }
 
 #Preview {
     ContentView()
+}
+
+class Band {
+    var names = [String]()
+    
+    @discardableResult
+    func loadData() -> Task<Void, Never>  {
+        Task {
+            try? await Task.sleep(for: .seconds(3))
+            names = ["Bill", "Mike", "Steven", "Reginald"]
+        }
+    }
 }
