0584579411ebbb79a927ac7f21cc0e483275f134
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-12 14:38:45 -0400

    add FunctionAttributes project to the CareerAccelerator folder in HackingWithSwift

---
+++ HackingWithSwift/CareerAccelerator/FunctionAttributes/FunctionAttributes/FunctionAttributesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunctionAttributesApp.swift
+//  FunctionAttributes
+//
+//  Created by Thomas Cowern on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FunctionAttributesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
