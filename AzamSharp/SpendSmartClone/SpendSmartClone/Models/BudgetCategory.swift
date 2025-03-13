0344edff5c15c9ffbf8155511021e64344b99fad
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-13 09:50:21 -0400

    update BudgetCategory model, add Models folder and move models into Models folder

---
+++ AzamSharp/SpendSmartClone/SpendSmartClone/Models/BudgetCategory.swift
@@ -0,0 +1,81 @@
+//
+//  BudgetCategory.swift
+//  SpendSmartClone
+//
+//  Created by Thomas Cowern on 3/13/25.
+//
+
+import Foundation
+import SwiftData
+
+enum BudgetCategoryError: LocalizedError {
+    case titleAlreadyExists
+    
+    var errorDescription: String? {
+        switch self {
+        case .titleAlreadyExists:
+            return "A budget category with this title already exists."
+        }
+    }
+}
+
+@Model
+class BudgetCategory {
+    @Attribute(.unique) var title: String = ""
+    var amount: Decimal = 0.0
+    @Relationship(deleteRule: .cascade) var transactions: [Transaction] = []
+    
+    init(title: String, amount: Decimal) {
+        self.title = title
+        self.amount = amount
+    }
+    
+    var total: Decimal {
+        transactions.reduce(0) { result, transaction in
+            result + transaction.total
+        }
+    }
+    
+    var remaining: Decimal {
+        amount - total
+    }
+    
+    // exists function to check if title already exist or not
+    private func exists(context: ModelContext, title: String) -> Bool {
+        
+        let predicate = #Predicate<BudgetCategory> { $0.title == title }
+        let descriptor = FetchDescriptor(predicate: predicate)
+        
+        do {
+            let result = try context.fetch(descriptor)
+            return !result.isEmpty ? true: false
+        } catch {
+            return false
+        }
+    }
+    
+    func addTransaction(context: ModelContext, transaction: Transaction) {
+        transaction.budgetCategory = self
+    }
+    
+    func update(context: ModelContext, title: String, amount: Decimal) throws {
+        if !exists(context: context, title: title) {
+            self.title = title
+            self.amount = amount
+        } else {
+            throw BudgetCategoryError.titleAlreadyExists
+        }
+    }
+    
+    func save(context: ModelContext) throws {
+        
+        // find if the budget category with the same name already exists
+        if !exists(context: context, title: self.title) {
+            // save it
+            context.insert(self)
+        } else {
+            // do something else
+            throw BudgetCategoryError.titleAlreadyExists
+        }
+    }
+}
