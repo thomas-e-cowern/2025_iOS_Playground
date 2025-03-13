22c44e38489c6820f9fc91f5fd3d627f36e5c6a7
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-13 08:41:26 -0400

    initial commit of SpendSmartClone to AzamSharp folder

---
+++ AzamSharp/SpendSmartClone/SpendSmartClone/SpendSmartCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSmartCloneApp.swift
+//  SpendSmartClone
+//
+//  Created by Thomas Cowern on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SpendSmartCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
