d501697fec2c3a28fee81c611271e9f6db4457eb
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-14 12:31:31 -0400

    bug hunt

--- AzamSharp/SpendSmartClone/SpendSmartCloneTests/SpendSmartCloneTests.swift
+++ AzamSharp/SpendSmartClone/SpendSmartCloneTests/SpendSmartCloneTests.swift
@@ -11,14 +11,23 @@ import SwiftData
 
 final class SpendSmartCloneTests: XCTestCase {
     
+    private var budgetCategory: BudgetCategory!
     private var context: ModelContext!
     
+    @MainActor
     override func setUp() {
-        <#code#>
+        context = mockContainer.mainContext
+        self.budgetCategory = BudgetCategory(title: "Travel", amount: 300)
+        try! self.budgetCategory.save(context: context)
     }
 
     func testThrowTitleExceptionInsertingDuplicateBudgetCategory() throws {
         let newBudgetCategory = BudgetCategory(title: "Travel", amount: 300)
+        XCTAssertThrowsError(try newBudgetCategory.save(context: context), "No exception was thrown") { error in
+            let throwError = error as? BudgetCategoryError
+            XCTAssertNil(throwError)
+            XCTAssertEqual(BudgetCategoryError.titleAlreadyExists, throwError)
+        }
     }
 
 }
