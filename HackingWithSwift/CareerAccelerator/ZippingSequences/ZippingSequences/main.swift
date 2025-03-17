608ae6d05fb277342510760d004c77c62a90ad01
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-17 13:50:49 -0400

    update ZippingSequences with examples

--- HackingWithSwift/CareerAccelerator/ZippingSequences/ZippingSequences/main.swift
+++ HackingWithSwift/CareerAccelerator/ZippingSequences/ZippingSequences/main.swift
@@ -7,5 +7,36 @@
 
 import Foundation
 
-print("Hello, World!")
+let friends = ["Daphne", "Frasier", "Martin", "Niles"]
 
+let numbers = [1, 5, 6]
+
+// Returns Fatal error: Index out of range
+//for (index, friend) in friends.enumerated() {
+//    let number = numbers[index]
+//    print("\(friend) has number \(number)")
+//}
+
+// Using zip to add numbers from 2 arrays
+for (friend, number) in zip(friends, numbers) {
+    print("\(friend) has number \(number)")
+}
+
+
+let firstResults = [85, 95, 90]
+let secondResults = [85, 100, 100]
+
+let combinedResults = zip(firstResults, secondResults).map { $0 + $1 }
+
+//print(combinedResults)
+
+// Using zip to combine multiple data sources
+let characters = ["Aragorn", "Gimli", "Legolas"]
+let races = ["human", "dwarf", "elf"]
+let weapons = ["sword", "axe", "bow"]
+
+let team = zip(characters, zip(races, weapons))
+
+for (character, (race, weapon)) in team {
+    print("\(character) the \(race) uses his \(weapon)")
+}
