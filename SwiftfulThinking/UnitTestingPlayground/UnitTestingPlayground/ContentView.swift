2f8b629f7b047ca7a3a0bf3fbe6f5f4acab631e1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-18 15:04:12 -0400

    refactor ContentView to fix Preview bug

--- SwiftfulThinking/UnitTestingPlayground/UnitTestingPlayground/ContentView.swift
+++ SwiftfulThinking/UnitTestingPlayground/UnitTestingPlayground/ContentView.swift
@@ -4,21 +4,30 @@
 //
 //  Created by Thomas Cowern on 3/18/25.
 //
+// 1. Unit Tests - test the business logic of an app
+
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @StateObject private var vm: UnitTestingPlaygroundViewModel
+    
+    init(isPremium: Bool) {
+        _vm = StateObject(wrappedValue: UnitTestingPlaygroundViewModel(isPremium: isPremium))
+    }
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(vm.isPremium.description)
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(isPremium: true)
 }
