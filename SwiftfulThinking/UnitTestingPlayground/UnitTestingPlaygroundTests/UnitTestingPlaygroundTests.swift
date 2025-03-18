c0c681adf62e87b2662ff3cac6ba676a8e3fbd02
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-18 15:04:54 -0400

    update UnitTestingPlaygroundTests to add isPremium tests

--- SwiftfulThinking/UnitTestingPlayground/UnitTestingPlaygroundTests/UnitTestingPlaygroundTests.swift
+++ SwiftfulThinking/UnitTestingPlayground/UnitTestingPlaygroundTests/UnitTestingPlaygroundTests.swift
@@ -5,13 +5,68 @@
 //  Created by Thomas Cowern on 3/18/25.
 //
 
-import Testing
+// Naming structure: test_UnitOfWork_StateUnderTest_ExpectedBehavior
+// Naming structure: text_[struct or class]_[variable or function]_[expected result]
+
+// Testing Structure:  Given, When, Then
+
+import XCTest
 @testable import UnitTestingPlayground
 
-struct UnitTestingPlaygroundTests {
+class UnitTestingPlaygroundTests: XCTestCase {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    // testing true
+    func test_UnitTestingPlaygroundViewModel_isPremium_shouldBeTrue() {
+        
+        // Given
+        let userIsPremium: Bool = true
+        
+        // When
+        let vm = UnitTestingPlaygroundViewModel(isPremium: userIsPremium)
+        
+        // Then
+        XCTAssertTrue(vm.isPremium)
+    }
+    
+    // testing false
+    func test_UnitTestingPlaygroundViewModel_isPremium_shouldBeFalse() {
+        
+        // Given
+        let userIsPremium: Bool = false
+        
+        // When
+        let vm = UnitTestingPlaygroundViewModel(isPremium: userIsPremium)
+        
+        // Then
+        XCTAssertFalse(vm.isPremium)
+    }
+    
+    // testing random bool
+    func test_UnitTestingPlaygroundViewModel_isPremium_shouldBeInjectedValue() {
+        
+        // Given
+        let userIsPremium: Bool = Bool.random()
+        
+        // When
+        let vm = UnitTestingPlaygroundViewModel(isPremium: userIsPremium)
+        
+        // Then
+        XCTAssertEqual(vm.isPremium, userIsPremium)
+    }
+    
+    // testing random multiple times to ensure coverage
+    func test_UnitTestingPlaygroundViewModel_isPremium_shouldBeInjectedValue_stress() {
+        
+        for _ in 0..<100 {
+            // Given
+            let userIsPremium: Bool = Bool.random()
+            
+            // When
+            let vm = UnitTestingPlaygroundViewModel(isPremium: userIsPremium)
+            
+            // Then
+            XCTAssertEqual(vm.isPremium, userIsPremium)
+        }
     }
 
 }
