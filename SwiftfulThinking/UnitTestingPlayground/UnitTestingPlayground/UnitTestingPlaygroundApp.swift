7d5b8136f39fef254a0cd5cbec338aadd8eb2a8d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-18 15:03:33 -0400

    update App file to fix errors

--- SwiftfulThinking/UnitTestingPlayground/UnitTestingPlayground/UnitTestingPlaygroundApp.swift
+++ SwiftfulThinking/UnitTestingPlayground/UnitTestingPlayground/UnitTestingPlaygroundApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct UnitTestingPlaygroundApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(isPremium: true)
         }
     }
 }
