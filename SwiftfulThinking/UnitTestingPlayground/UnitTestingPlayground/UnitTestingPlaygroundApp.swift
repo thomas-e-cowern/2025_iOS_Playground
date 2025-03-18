44cc36216d55d97c0ab9909e49ac7c0ee1d75e89
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-18 10:08:41 -0400

    add UnitTestingPlayground to the SwiftfulThinking folder

---
+++ SwiftfulThinking/UnitTestingPlayground/UnitTestingPlayground/UnitTestingPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingPlaygroundApp.swift
+//  UnitTestingPlayground
+//
+//  Created by Thomas Cowern on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulThinking/UnitTestingPlayground/UnitTestingPlaygroundTests/UnitTestingPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingPlaygroundTests.swift
+//  UnitTestingPlaygroundTests
+//
+//  Created by Thomas Cowern on 3/18/25.
+//
+
+import Testing
+@testable import UnitTestingPlayground
+
+struct UnitTestingPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
