bcf69133aa729c100509487e29ef6923d336b84a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-19 10:47:24 -0400

    update Converter to refactor functions to make testing easier

--- HackingWithSwift/FirstTesting/FirstTesting/Converter.swift
+++ HackingWithSwift/FirstTesting/FirstTesting/Converter.swift
@@ -9,6 +9,8 @@ import Foundation
 
 struct Converter {
     func convertToCelsius(fahrenheit: Double) -> Double {
-        return (fahrenheit - 32) * 5 / 9
+        let fahrenheit = Measurement(value: fahrenheit, unit: UnitTemperature.fahrenheit)
+        let celsius = fahrenheit.converted(to: .celsius)
+        return celsius.value
     }
 }
