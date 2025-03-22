7f767285840c59ef5e4995d4519a3d7675cc8959
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-22 13:02:39 -0400

    ScrumDinger completion of third section

---
+++ Skills/ScrumDinger/ScrumDinger/Views/ScrumsView.swift
@@ -0,0 +1,24 @@
+//
+//  ScrumsView.swift
+//  ScrumDinger
+//
+//  Created by Thomas Cowern on 3/22/25.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}
