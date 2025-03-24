27e9cab7890e71a111518342ba0b29b8e5cb11d5
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-24 08:23:32 -0400

    update MeetingView with ZStack and scrum binding

--- Skills/ScrumDinger/ScrumDinger/MeetingView.swift
+++ Skills/ScrumDinger/ScrumDinger/MeetingView.swift
@@ -8,45 +8,53 @@
 import SwiftUI
 
 struct MeetingView: View {
+    
+    @Binding var scrum: DailyScrum
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 17, total: 50)
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300", systemImage: "hourglass.tophalf.fill")
+        ZStack {
+            RoundedRectangle(cornerRadius: 16)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                ProgressView(value: 17, total: 50)
+                HStack {
+                    VStack(alignment: .leading) {
+                        Text("Seconds Elapsed")
+                            .font(.caption)
+                        Label("300", systemImage: "hourglass.tophalf.fill")
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("Seconds Remaining")
+                            .font(.caption)
+                        Label("600", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                } // MARK: - End of HStack
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel("Time remaining")
+                .accessibilityValue("10 minutes")
+                
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button {
+                        // More to come
+                    } label: {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
+                    
                 }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600", systemImage: "hourglass.bottomhalf.fill")
-                }
-            } // MARK: - End of HStack
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time remaining")
-            .accessibilityValue("10 minutes")
+            } //: End of VStack
             
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button {
-                    // More to come
-                } label: {
-                    Image(systemName: "forward.fill")
-                }
-                .accessibilityLabel("Next speaker")
-
-            }
-        } //: End of VStack
+        } //: End of ZStack
         .padding()
     }
 }
 
 #Preview {
-    MeetingView()
+    MeetingView(scrum: .constant(DailyScrum.sampleData[1]))
 }
