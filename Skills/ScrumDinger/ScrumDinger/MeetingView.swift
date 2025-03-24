b9da5fa695594b0aed06fa7b2b8e14d3be8605a8
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-24 08:40:34 -0400

    delete header componenets form MeetingView and transfer to MeetingHeaderView

--- Skills/ScrumDinger/ScrumDinger/MeetingView.swift
+++ Skills/ScrumDinger/ScrumDinger/MeetingView.swift
@@ -16,23 +16,7 @@ struct MeetingView: View {
             RoundedRectangle(cornerRadius: 16)
                 .fill(scrum.theme.mainColor)
             VStack {
-                ProgressView(value: 17, total: 50)
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Seconds Elapsed")
-                            .font(.caption)
-                        Label("300", systemImage: "hourglass.tophalf.fill")
-                    }
-                    Spacer()
-                    VStack(alignment: .trailing) {
-                        Text("Seconds Remaining")
-                            .font(.caption)
-                        Label("600", systemImage: "hourglass.bottomhalf.fill")
-                    }
-                } // MARK: - End of HStack
-                .accessibilityElement(children: .ignore)
-                .accessibilityLabel("Time remaining")
-                .accessibilityValue("10 minutes")
+                
                 
                 Circle()
                     .strokeBorder(lineWidth: 24)
