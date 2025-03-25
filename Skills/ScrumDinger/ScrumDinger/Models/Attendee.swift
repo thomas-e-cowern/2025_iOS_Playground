e10c4671c87b7f0f6eb8f93cba771506e1bb2d0a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-25 13:31:48 -0400

    add Attendee to Models folder.

---
+++ Skills/ScrumDinger/ScrumDinger/Models/Attendee.swift
@@ -0,0 +1,40 @@
+//
+//  Attendee.swift
+//  ScrumDinger
+//
+//  Created by Thomas Cowern on 3/25/25.
+//
+import Foundation
+import SwiftData
+
+@Model
+class Attendee: Identifiable {
+    var id: UUID
+    var name: String
+    var dailyScrum: DailyScrum?
+    
+    init(id: UUID = UUID(), name: String) {
+        self.id = id
+        self.name = name
+    }
+}
+
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design",
+                   attendees: ["Cathy", "Daisy", "Simon", "Jonathan"],
+                   lengthInMinutes: 10,
+                   theme: .yellow),
+        DailyScrum(title: "App Dev",
+                   attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"],
+                   lengthInMinutes: 5,
+                   theme: .orange),
+        DailyScrum(title: "Web Dev",
+                   attendees: ["Chella", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"],
+                   lengthInMinutes: 5,
+                   theme: .poppy)
+    ]
+}
+
