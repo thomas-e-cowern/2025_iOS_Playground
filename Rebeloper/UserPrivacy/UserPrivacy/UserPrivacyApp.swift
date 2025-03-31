4aa4a9c34704b0465c868c5469f20a053f86a6d4
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-31 08:57:23 -0400

    UserPrivacy initial commit in Rebeloper folder

---
+++ Rebeloper/UserPrivacy/UserPrivacy/UserPrivacyApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserPrivacyApp.swift
+//  UserPrivacy
+//
+//  Created by Thomas Cowern on 3/31/25.
+//
+
+import SwiftUI
+
+@main
+struct UserPrivacyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
