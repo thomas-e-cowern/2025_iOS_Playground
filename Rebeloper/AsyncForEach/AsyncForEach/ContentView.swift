1e61434f0856d036abab0deded0d655882a59f74
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-03-31 08:40:57 -0400

    update ContentView with countUp function and Sequence extension to permit aynch fetch operations

--- Rebeloper/AsyncForEach/AsyncForEach/ContentView.swift
+++ Rebeloper/AsyncForEach/AsyncForEach/ContentView.swift
@@ -11,19 +11,30 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Button("Tap me") {
-                (1...10).forEach { index in
-                    countUp(index)
+                Task {
+                    try await (1...10).asyncForEach { index in
+                        try await countUp(index)
+                    }
                 }
             }
         }
         .padding()
     }
     
-    func countUp(_ index: Int) {
+    func countUp(_ index: Int) async throws {
+        try await Task.sleep(for: .seconds(1))
         print(index)
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension Sequence {
+    func asyncForEach(_ operations: (Element) async throws -> Void) async rethrows {
+        for element in self {
+            try await operations(element)
+        }
+    }
 }
