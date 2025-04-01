cdd571e997a39b63398ef39f937595dbbce3ee48
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-01 10:23:58 -0400

    add Models folder and Blog model to project

---
+++ Skills/GlitchBlogTest/GlitchBlogTest/Models/Blog.swift
@@ -0,0 +1,14 @@
+//
+//  Blog.swift
+//  GlitchBlogTest
+//
+//  Created by Thomas Cowern on 4/1/25.
+//
+
+import Foundation
+
+struct Blog {
+    let title: String
+    let snippet: String
+    let body: String
+}
