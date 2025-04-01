ac9f91fb759996d17baf8b42893e55816797ddd8
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-01 09:42:10 -0400

    GlitchBlogTest initial commit

---
+++ Skills/GlitchBlogTest/GlitchBlogTest/GlitchBlogTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlitchBlogTestApp.swift
+//  GlitchBlogTest
+//
+//  Created by Thomas Cowern on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct GlitchBlogTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
