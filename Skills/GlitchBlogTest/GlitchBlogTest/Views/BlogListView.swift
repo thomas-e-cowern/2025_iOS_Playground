ab6a25841438793573e0ea4d374810195fcecf8c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-01 22:34:16 -0400

    updated

---
+++ Skills/GlitchBlogTest/GlitchBlogTest/Views/BlogListView.swift
@@ -0,0 +1,21 @@
+//
+//  BlogListView.swift
+//  GlitchBlogTest
+//
+//  Created by Thomas Cowern on 4/1/25.
+//
+
+import SwiftUI
+
+struct BlogListView: View {
+    
+    @State private var blogs: [Blog]
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlogListView(blogs: <#[Blog]#>)
+}
