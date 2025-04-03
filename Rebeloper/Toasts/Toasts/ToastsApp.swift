f6897e9a43a14df286baa36a2c3c4c6f93ed2b0b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-03 07:57:23 -0400

    Toasts initial commit in Rebeloper folde

---
+++ Rebeloper/Toasts/Toasts/ToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastsApp.swift
+//  Toasts
+//
+//  Created by Thomas Cowern on 4/3/25.
+//
+
+import SwiftUI
+
+@main
+struct ToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
