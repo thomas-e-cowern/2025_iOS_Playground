2d8f63660bd8ccac9bc4c4583ed565f234bdcf65
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-07 08:42:00 -0400

    add new AccentColor and BorderProminentLabelColor to assets, update ContentView to use new colors

--- Rebeloper/ButtonBug/ButtonBug/ContentView.swift
+++ Rebeloper/ButtonBug/ButtonBug/ContentView.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button {
+            //
+        } label: {
+            Text("Tap Me")
+                .foregroundStyle(.borderProminentLabel)
         }
-        .padding()
+        .buttonStyle(.borderedProminent)
     }
 }
 
-#Preview {
+#Preview("Light Mode") {
     ContentView()
+}
+
+#Preview("Dark Mode") {
+    ContentView()
+        .preferredColorScheme(.dark)
 }
