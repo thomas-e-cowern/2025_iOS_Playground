0581db424f1538d127ab740ff9ee3169e53dfaaa
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-07 08:18:29 -0400

    add ButtonBug project to Rebeloper folder

---
+++ Rebeloper/ButtonBug/ButtonBug/ButtonBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonBugApp.swift
+//  ButtonBug
+//
+//  Created by Thomas Cowern on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct ButtonBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
