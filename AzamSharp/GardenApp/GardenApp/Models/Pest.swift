8515580d6760731c7a5ad01c57f31f1f867227d2
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-08 09:19:49 -0400

    add Pest model to project

--- AzamSharp/GardenApp/GardenApp/Models/Pest.swift
+++ AzamSharp/GardenApp/GardenApp/Models/Pest.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class Pest: Decodable {
+    var name: String
+    var body: String
+    var symptoms: String
+    var treatment: String
+    var photo: String
+    var vegetable: Vegetable?
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.body = try container.decode(String.self, forKey: .body)
+        self.symptoms = try container.decode(String.self, forKey: .symptoms)
+        self.treatment = try container.decode(String.self, forKey: .treatment)
+        self.photo = try container.decode(String.self, forKey: .photo)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case name, symptoms, treatment, photo
+        case body = "description"
+    }
+    
+}
