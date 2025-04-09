4158a34cb83f7ce943402e37b1fb2f5ca0478708
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-09 09:56:48 -0400

    update MyGardenView with delete function

--- AzamSharp/GardenApp/GardenApp/Views/MyGardenView.swift
+++ AzamSharp/GardenApp/GardenApp/Views/MyGardenView.swift
@@ -10,17 +10,36 @@ import SwiftData
 
 struct MyGardenView: View {
     
+    @Environment(\.modelContext) private var context
+    
     @Query private var myGardenVegetabes: [MyGardenVegetable]
+    
     var body: some View {
-        List(myGardenVegetabes) { myVeggy in
-            NavigationLink {
-                NoteListView(myGardenVegetable: myVeggy)
-            } label: {
-                MyGardenCellView(myGardenVegetable: myVeggy)
+        List {
+            ForEach(myGardenVegetabes) { myVeggy in
+                NavigationLink {
+                    NoteListView(myGardenVegetable: myVeggy)
+                } label: {
+                    MyGardenCellView(myGardenVegetable: myVeggy)
+                }
             }
+            .onDelete(perform: deleteMyGardenVegetable)
         }
         .listStyle(.plain)
         .navigationTitle("My Garden")
+    }
+    
+    // MARK: - Methods and functions
+    private func deleteMyGardenVegetable(at offsets: IndexSet) {
+        offsets.forEach { index in
+            let myGardenVegetable = myGardenVegetabes[index]
+            context.delete(myGardenVegetable)
+            do {
+                try context.save()
+            } catch {
+                print("Failed to delete item from myGardenVegetables: \(error)")
+            }
+        }
     }
 }
 
