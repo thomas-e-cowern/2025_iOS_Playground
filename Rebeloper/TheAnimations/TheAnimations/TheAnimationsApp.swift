81021770e40781c80f151a38db3d6548d23af233
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-10 13:02:12 -0400

    add TheAnimations to Rebeloper folder in Playgrounds

---
+++ Rebeloper/TheAnimations/TheAnimations/TheAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheAnimationsApp.swift
+//  TheAnimations
+//
+//  Created by Thomas Cowern on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TheAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
