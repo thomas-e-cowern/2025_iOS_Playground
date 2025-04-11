b338201eb31cb2b927eae2f9dcfd39fec7b86945
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-11 09:00:44 -0400

    add Button to ContentView

--- Rebeloper/PushNotifications/PushNotifications/ContentView.swift
+++ Rebeloper/PushNotifications/PushNotifications/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                // 
+            } label: {
+                Text("Request Push Notification Access")
+            }
+            .buttonStyle(.borderedProminent)
+
         }
         .padding()
     }
