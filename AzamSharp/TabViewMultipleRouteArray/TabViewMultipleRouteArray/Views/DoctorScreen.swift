cad27e5db6effdd2173ace708f02036f8b4859d4
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-14 09:08:28 -0400

    add DoctorScreen to Views folder

---
+++ AzamSharp/TabViewMultipleRouteArray/TabViewMultipleRouteArray/Views/DoctorScreen.swift
@@ -0,0 +1,18 @@
+//
+//  DoctorScreen.swift
+//  TabViewMultipleRouteArray
+//
+//  Created by Thomas Cowern on 4/14/25.
+//
+import SwiftUI
+
+struct DoctorScreen: View {
+    var body: some View {
+        VStack {
+            Text("Doctor Screen")
+            Button("Go to List") {
+                // more to come...
+            }
+        }
+    }
+}
