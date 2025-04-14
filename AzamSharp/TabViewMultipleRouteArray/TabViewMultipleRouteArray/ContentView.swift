c331c71f016dad490b69273b5f9e5bd301f03ff5
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-14 09:04:15 -0400

    add TabView and tabs to ContentView

--- AzamSharp/TabViewMultipleRouteArray/TabViewMultipleRouteArray/ContentView.swift
+++ AzamSharp/TabViewMultipleRouteArray/TabViewMultipleRouteArray/ContentView.swift
@@ -10,10 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TabView {
+                Tab {
+                    Text("Doctor Screen")
+                }
+                
+                Tab {
+                    Text("Patient Screen")
+                }
+            }
         }
         .padding()
     }
