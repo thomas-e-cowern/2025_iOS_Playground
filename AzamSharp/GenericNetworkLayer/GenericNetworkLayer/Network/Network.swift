cf9196a4442e0cd71f9e1e70ed82adadb50c0c3c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-14 14:31:52 -0400

    add Network file to Network folder

---
+++ AzamSharp/GenericNetworkLayer/GenericNetworkLayer/Network/Network.swift
@@ -0,0 +1,18 @@
+//
+//  Network.swift
+//  GenericNetworkLayer
+//
+//  Created by Thomas Cowern on 4/14/25.
+//
+
+import Foundation
+
+
+
+
+
+enum NewtorkingError: Error {
+    case invalidResponse
+    case badUrl
+    case decodingError
+}
