983814483c8048a28af2384f78c3ffd2452d98ca
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-14 09:06:49 -0400

    update PatientScreen with Text view

---
+++ AzamSharp/TabViewMultipleRouteArray/TabViewMultipleRouteArray/Views/PatientScreen.swift
@@ -0,0 +1,14 @@
+//
+//  PatientScreen.swift
+//  TabViewMultipleRouteArray
+//
+//  Created by Thomas Cowern on 4/14/25.
+//
+import SwiftUI
+
+struct PatientScreen: View {
+    var body: some View {
+        Text("Patient Screen")
+    }
+}
+
