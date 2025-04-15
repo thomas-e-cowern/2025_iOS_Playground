535a5a0c3422052d0ff76d9f9ee504bf463c8d70
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-15 09:08:46 -0400

    add NavigationStack to App file

--- Rebeloper/DebouncingSwiftUI/DebouncingSwiftUI/DebouncingSwiftUIApp.swift
+++ Rebeloper/DebouncingSwiftUI/DebouncingSwiftUI/DebouncingSwiftUIApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct DebouncingSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
