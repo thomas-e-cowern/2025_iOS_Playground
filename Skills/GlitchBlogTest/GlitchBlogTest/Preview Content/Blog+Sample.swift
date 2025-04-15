58c1f30c7a13b16b0e70c8eb987c4fbb0032f414
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-15 13:31:19 -0400

    updated

---
+++ Skills/GlitchBlogTest/GlitchBlogTest/Preview Content/Blog+Sample.swift
@@ -0,0 +1,17 @@
+//
+//  Blog+Sample.swift
+//  
+//
+//  Created by Thomas Cowern on 4/15/25.
+//
+
+import SwiftUI
+
+extension Blog {
+    static let sampleData: [Blog] = [
+        Blog(title: "Hello, World!", snippet: "This is an example blog post.", body: "This is an example blog post."),
+        Blog(title: "Hello, John", snippet: "This is an example blog post.", body: "This is an example blog post."),
+        Blog(title: "Hello, Maria!", snippet: "This is an example blog post.", body: "This is an example blog post."),
+        Blog(title: "What's Up?", snippet: "This is an example blog post.", body: "This is an example blog post.")
+    ]
+}
