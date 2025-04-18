116b0613e5b507d912375fc48d0b9d953d21ccda
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-18 08:21:09 -0400

    ClockRedesign initial commit in HackingWithSwift

---
+++ HackingWithSwift/ClockRedesign/ClockRedesign/ClockAngles.swift
@@ -0,0 +1,8 @@
+//
+//  ClockAngles.swift
+//  ClockRedesign
+//
+//  Created by Thomas Cowern on 4/18/25.
+//
+
+import Foundation
---
+++ HackingWithSwift/ClockRedesign/ClockRedesign/ClockRedesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockRedesignApp.swift
+//  ClockRedesign
+//
+//  Created by Thomas Cowern on 4/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ClockRedesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
