fcb81079ae0ad1d98b540b6bed39d7379bed3ea8
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-18 09:29:34 -0400

    add the clock components to ClockAngles

--- HackingWithSwift/ClockRedesign/ClockRedesign/ClockAngles.swift
+++ HackingWithSwift/ClockRedesign/ClockRedesign/ClockAngles.swift
@@ -12,5 +12,18 @@ struct ClockAngles {
     var minute: Angle
     var second: Angle
     
-    
+    init(for date: Date) {
+        let components = Calendar.current.dateComponents([.hour, .minute, .second, .nanosecond], from: date)
+        
+        let hourComponent = components.hour ?? 0
+        let minuteComponent = Double(components.minute ?? 0)
+        let secondComponent = Double(components.second ?? 0)
+        let nanosecondComponent = Double(components.nanosecond ?? 0)
+        
+        hour = .degrees(30 * (Double(hourComponent % 12) + minuteComponent / 60) + 180)
+        
+        minute = .degrees(6 * (minuteComponent + secondComponent / 60) + 180)
+        
+        second = .degrees(6 * (secondComponent + nanosecondComponent / 1_000_000_000) + 180)
+    }
 }
