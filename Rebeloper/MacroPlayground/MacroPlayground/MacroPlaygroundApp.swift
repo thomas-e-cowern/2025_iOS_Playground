db46672bda63b44ee2b1ecf1bf2f42912465f3b9
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-19 08:59:36 -0400

    add MacroPlayground to Rebeloper forlder

---
+++ Rebeloper/MacroPlayground/MacroPlayground/MacroPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacroPlaygroundApp.swift
+//  MacroPlayground
+//
+//  Created by Thomas Cowern on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MacroPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
