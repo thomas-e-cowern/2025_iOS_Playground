ab5b40a26bd238c4282856766bb268d91ceb8216
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-19 09:03:03 -0400

    update App file with AppController and inject in environment

--- Rebeloper/MacroPlayground/MacroPlayground/MacroPlaygroundApp.swift
+++ Rebeloper/MacroPlayground/MacroPlayground/MacroPlaygroundApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct MacroPlaygroundApp: App {
+    
+    @State private var appController = AppController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(appController)
         }
     }
 }
