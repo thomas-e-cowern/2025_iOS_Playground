304e75c3cda25348c3da6f565d22e3fa6f9f0982
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-19 09:02:18 -0400

    add AppController file to project

---
+++ Rebeloper/MacroPlayground/MacroPlayground/AppController.swift
@@ -0,0 +1,13 @@
+//
+//  AppController.swift
+//  MacroPlayground
+//
+//  Created by Thomas Cowern on 4/19/25.
+//
+
+import SwiftUI
+
+@Observable
+class AppController {
+    var text: String = "Hello World"
+}
