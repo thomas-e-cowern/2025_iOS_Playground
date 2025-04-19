e01270578ff300ec4b1a730f425954471a387482
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-19 11:16:27 -0400

    add BindableEnvironment project to Rebeloper folder

---
+++ Rebeloper/BindableEnvironment/BindableEnvironment/BindableEnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindableEnvironmentApp.swift
+//  BindableEnvironment
+//
+//  Created by Thomas Cowern on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct BindableEnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
