d148168c8c27b9df287ce81d66fe0409f2087c93
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-19 11:23:43 -0400

    add AppControler file to BindableEnvironment project

---
+++ Rebeloper/BindableEnvironment/BindableEnvironment/AppController.swift
@@ -0,0 +1,13 @@
+//
+//  AppController.swift
+//  BindableEnvironment
+//
+//  Created by Thomas Cowern on 4/19/25.
+//
+
+import SwiftUI
+
+@Observable
+class AppController {
+    var text: String = "It magically works..."
+}
