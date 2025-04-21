afbece9d6c61b133da68203804a709f391f08e38
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-21 09:26:54 -0400

    add FocusedField file with FocusedField enum

---
+++ Rebeloper/AppIntentDemo/AppIntentDemo/AppIntentController.swift
@@ -0,0 +1,8 @@
+//
+//  AppIntentController.swift
+//  AppIntentDemo
+//
+//  Created by Thomas Cowern on 4/21/25.
+//
+
+import Foundation
---
+++ Rebeloper/AppIntentDemo/AppIntentDemo/FocusedField.swift
@@ -0,0 +1,14 @@
+//
+//  FocusedField.swift
+//  AppIntentDemo
+//
+//  Created by Thomas Cowern on 4/21/25.
+//
+
+import Foundation
+
+enum FocusedField {
+    case none
+    case email
+    case password
+}
