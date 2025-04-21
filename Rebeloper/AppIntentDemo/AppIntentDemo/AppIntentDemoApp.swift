8ba7afc95ccaa3b310a3f51b3cedf37dd4c4ac7d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-21 09:19:33 -0400

    add AppIntentDemo project to Rebeloper folder

---
+++ Rebeloper/AppIntentDemo/AppIntentDemo/AppIntentDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentDemoApp.swift
+//  AppIntentDemo
+//
+//  Created by Thomas Cowern on 4/21/25.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
