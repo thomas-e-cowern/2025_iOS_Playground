ac63628db839beea0d7fcfbbc560c16fb6534d2b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-21 11:24:37 -0400

    update AppIntentController and FocusedTextFieldIntent to test project.  Tutorial complete

--- Rebeloper/AppIntentDemo/AppIntentDemo/AppIntentController.swift
+++ Rebeloper/AppIntentDemo/AppIntentDemo/AppIntentController.swift
@@ -10,9 +10,13 @@ import SwiftUI
 @Observable
 class AppIntentController {
     
-    static let shared = AppIntentController()
+    static let shared = AppIntentController(focusedField: .none)
     
     var focusedField: FocusedField = .none
+    
+    init(focusedField: FocusedField) {
+        self.focusedField = focusedField
+    }
     
 }
 
--- Rebeloper/AppIntentDemo/AppIntentDemo/FocusedTextFieldIntent.swift
+++ Rebeloper/AppIntentDemo/AppIntentDemo/FocusedTextFieldIntent.swift
@@ -5,4 +5,18 @@
 //  Created by Thomas Cowern on 4/21/25.
 //
 
-import Foundation
+import AppIntents
+
+struct FocusedTextFieldIntent: AppIntent {
+    
+    static var title = LocalizedStringResource("Focused Text Field")
+    static var description = IntentDescription("Enter your credentials")
+    static var openAppWhenRun: Bool = true
+    
+    @Parameter(title: "Focused Field") var focusedField: String
+    
+    func perform() async throws -> some IntentResult {
+        AppIntentController.shared.focusedField = FocusedField.password
+        return .result()
+    }
+}
