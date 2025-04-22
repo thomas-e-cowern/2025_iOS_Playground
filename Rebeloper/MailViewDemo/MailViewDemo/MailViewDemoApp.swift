945779d7a9f7d00c0576598cf56132e8e2f64f0d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-22 09:09:27 -0400

    MailViewDemo initial commit in Rebeloper folder

---
+++ Rebeloper/MailViewDemo/MailViewDemo/MailViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MailViewDemoApp.swift
+//  MailViewDemo
+//
+//  Created by Thomas Cowern on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct MailViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
