909eee5c5656acfc511d3ff0ec3ba731e15a7d19
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-23 08:19:03 -0400

    SizeChangeDemo initial commit to Rebeloper folder

---
+++ Rebeloper/SizeChangeDemo/SizeChangeDemo/SizeChangeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SizeChangeDemoApp.swift
+//  SizeChangeDemo
+//
+//  Created by Thomas Cowern on 4/23/25.
+//
+
+import SwiftUI
+
+@main
+struct SizeChangeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
