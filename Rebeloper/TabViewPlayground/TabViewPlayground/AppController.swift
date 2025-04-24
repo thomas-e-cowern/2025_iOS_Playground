c35f500d2a2cc70bc67ce327ac100e58428a8824
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-24 10:17:07 -0400

    add AppContoller.swift to project

---
+++ Rebeloper/TabViewPlayground/TabViewPlayground/AppController.swift
@@ -0,0 +1,13 @@
+//
+//  Untitled.swift
+//  TabViewPlayground
+//
+//  Created by Thomas Cowern on 4/24/25.
+//
+
+import SwiftUI
+
+@Observable
+class AppController {
+    var selectedTabIndex: Tabs = .archery
+}
