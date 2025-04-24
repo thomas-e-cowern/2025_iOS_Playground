809a7130ea98b18a276c3d1d7f16d1e9daf3312f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-24 09:07:21 -0400

    update ContentView, add TabView and Tabs for ArcheryView, BadmintonView and BaseballView.

--- Rebeloper/TabViewPlayground/TabViewPlayground/ContentView.swift
+++ Rebeloper/TabViewPlayground/TabViewPlayground/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selectedTabIndex: Int = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Archery", systemImage: "figure.archery") {
+                ArcheryView()
+            }
+            
+            Tab("Badminton", systemImage: "figure.badminton") {
+                BadmintonView()
+            }
+            
+            Tab("Baseball", systemImage: "figure.baseball") {
+                BaseballView()
+            }
         }
-        .padding()
     }
 }
 
