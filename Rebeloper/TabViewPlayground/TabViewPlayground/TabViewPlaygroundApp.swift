5db21e4864fe44b4b4d7e4ac9436ec9abd3fcb05
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-24 07:54:39 -0400

    initial commit of TabViewPlayground in Rebeloper folder

---
+++ Rebeloper/TabViewPlayground/TabViewPlayground/TabViewPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewPlaygroundApp.swift
+//  TabViewPlayground
+//
+//  Created by Thomas Cowern on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct TabViewPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
