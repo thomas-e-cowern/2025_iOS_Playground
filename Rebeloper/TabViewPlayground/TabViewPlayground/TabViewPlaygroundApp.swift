fcd782333bcd158fda1881e999d5a9967066f6fc
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-24 10:18:34 -0400

    update App file with @State variable and envirnment injection

--- Rebeloper/TabViewPlayground/TabViewPlayground/TabViewPlaygroundApp.swift
+++ Rebeloper/TabViewPlayground/TabViewPlayground/TabViewPlaygroundApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct TabViewPlaygroundApp: App {
+    
+    @State private var appController = AppController()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(appController)
         }
     }
 }
