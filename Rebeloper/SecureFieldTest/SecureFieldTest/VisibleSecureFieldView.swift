1d3d90546f489e93f3f836cb69b1ce6c9f5bd710
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-28 09:16:56 -0400

    add VisibleSecureFieldView.swift to project

---
+++ Rebeloper/SecureFieldTest/SecureFieldTest/VisibleSecureFieldView.swift
@@ -0,0 +1,39 @@
+//
+//  VisibleSecureFieldView.swift
+//  SecureFieldTest
+//
+//  Created by Thomas Cowern on 4/28/25.
+//
+import SwiftUI
+
+struct VisibleSecureFieldView: View {
+    
+    @Binding var text: String
+    @State private var showPassword: Bool = false
+    
+    var body: some View {
+        HStack {
+            ZStack {
+                TextField("Password", text: $text)
+                    .opacity(showPassword ? 1 : 0)
+                SecureField("Password", text: $text)
+                    .opacity(showPassword ? 0 : 1)
+            }
+            
+            Spacer(minLength: 0)
+            
+            Button {
+                showPassword.toggle()
+            } label: {
+                Image(systemName: showPassword ? "eye.slash" :"eye")
+            }
+            .buttonStyle(.plain)
+        }
+        .padding(.horizontal, 12)
+        .padding(.vertical, 9)
+        .overlay {
+            RoundedRectangle(cornerRadius: 7)
+                .stroke(.secondary, lineWidth: 1)
+        }
+    }
+}
