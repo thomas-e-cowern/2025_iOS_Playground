61f0f0ea809eb930e5520eec8849727bd4a22cb7
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-28 08:55:16 -0400

    SecureFieldTest initial commit in the Rebeloper folder

---
+++ Rebeloper/SecureFieldTest/SecureFieldTest/SecureFieldTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecureFieldTestApp.swift
+//  SecureFieldTest
+//
+//  Created by Thomas Cowern on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct SecureFieldTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
