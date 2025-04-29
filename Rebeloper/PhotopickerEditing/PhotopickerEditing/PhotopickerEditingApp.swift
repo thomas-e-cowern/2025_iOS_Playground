ed3e8faa2fa026477afeec4171dc6157b4f38067
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-29 08:25:13 -0400

    PhotopickerEditing initial commit in Rebeloper folder

---
+++ Rebeloper/PhotopickerEditing/PhotopickerEditing/PhotopickerEditingApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotopickerEditingApp.swift
+//  PhotopickerEditing
+//
+//  Created by Thomas Cowern on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotopickerEditingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
