e24980d4a96e3eeac3c9ad23de5f4cdf74f55775
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-29 14:14:47 -0400

    update Router with enum and Router class

--- Skills/SwiftUIRouter/SwiftUIRouter/Router/Router.swift
+++ Skills/SwiftUIRouter/SwiftUIRouter/Router/Router.swift
@@ -9,12 +9,14 @@ import Foundation
 import SwiftUI
 import Observation
 
+// Step one create an enum for each view
 enum Route: Hashable {
     case firstView
     case secondView
     case thirdView
 }
 
+// Step two create an @Observable router with a path, popToRoute function and function for each view using the enums
 @Observable
 class Router {
     var path = NavigationPath()
