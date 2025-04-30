1ee5f9af188a8d9b184b4a9a78d0b69203e0c98f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-30 11:33:11 -0400

    add APIUtility to project

---
+++ Rebeloper/MVSUExample/MVSUExample/APIUtility.swift
@@ -0,0 +1,13 @@
+//
+//  APIUtility.swift
+//  MVSUExample
+//
+//  Created by Thomas Cowern on 4/30/25.
+//
+import SwiftUI
+
+struct APIUtility {
+    static func fetchName() -> String {
+        return "John"
+    }
+}
