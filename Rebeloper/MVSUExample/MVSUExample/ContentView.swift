7c440065711b68ae8abafc7d79e10b3ccd38e449
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-30 10:43:27 -0400

    MVSUExample initial commit in Rebeloper folder

---
+++ Rebeloper/MVSUExample/MVSUExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVSUExample
+//
+//  Created by Thomas Cowern on 4/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rebeloper/MVSUExample/MVSUExample/MVSUExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVSUExampleApp.swift
+//  MVSUExample
+//
+//  Created by Thomas Cowern on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct MVSUExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
