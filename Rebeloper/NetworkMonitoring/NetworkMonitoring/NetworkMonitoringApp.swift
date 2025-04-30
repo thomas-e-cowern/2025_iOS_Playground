dfe83412940b4232dbb68958ea40ce379f17803f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-30 08:21:13 -0400

    NetworkMonitoring initial commit in the Rebeloper folder

---
+++ Rebeloper/NetworkMonitoring/NetworkMonitoring/NetworkMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkMonitoringApp.swift
+//  NetworkMonitoring
+//
+//  Created by Thomas Cowern on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
