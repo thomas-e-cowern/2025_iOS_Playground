107186f89b1a0b4dcad9e1d2a747feaf63d96207
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-04-30 10:45:04 -0400

    add Profile model to project

---
+++ Rebeloper/MVSUExample/MVSUExample/Profile.swift
@@ -0,0 +1,16 @@
+//
+//  Profile.swift
+//  MVSUExample
+//
+//  Created by Thomas Cowern on 4/30/25.
+//
+
+import SwiftUI
+
+struct Profile {
+    var name: String
+    
+    init(name: String = "") {
+        self.name = name
+    }
+}
