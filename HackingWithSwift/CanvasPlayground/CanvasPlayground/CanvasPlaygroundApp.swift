8abddc16d9f0e3a92fbc02d1fb3cb3aeb34256b4
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-01 09:10:24 -0400

    CanvasPlayground initial commit in HackingWithSwift folder

---
+++ HackingWithSwift/CanvasPlayground/CanvasPlayground/CanvasPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasPlaygroundApp.swift
+//  CanvasPlayground
+//
+//  Created by Thomas Cowern on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CanvasPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
