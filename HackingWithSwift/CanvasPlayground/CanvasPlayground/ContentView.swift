0a1a4f2eb5cb3083ab07262f9c4cfb4d918b19e2
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-01 09:41:25 -0400

    update ContentView Circles

--- HackingWithSwift/CanvasPlayground/CanvasPlayground/ContentView.swift
+++ HackingWithSwift/CanvasPlayground/CanvasPlayground/ContentView.swift
@@ -12,9 +12,16 @@ struct ContentView: View {
         VStack {
             Canvas { context, size in
                 let size = 220
+                context.opacity = 0.5
                 
                 let circle1 = Path(ellipseIn: CGRect(x: 100, y: 200, width: size, height: size))
                 context.fill(circle1, with: .color(red: 1, green: 0, blue: 0))
+                
+                let circle2 = Path(ellipseIn: CGRect(x: 100, y: 300, width: size, height: size))
+                context.fill(circle2, with: .color(red: 0, green: 1, blue: 0))
+                
+                let circle3 = Path(ellipseIn: CGRect(x: 100, y: 400, width: size, height: size))
+                context.fill(circle3, with: .color(red: 0, green: 0, blue: 1))
                 
             }
         }
