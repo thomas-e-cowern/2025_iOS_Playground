396b3c76ac500ed0e657fc275f6ad7a55384c9b3
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-02 09:26:17 -0400

    add User model to project

---
+++ iOS-Server-Test/FakeAPIClient/FakeAPIClient/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  FakeAPIClient
+//
+//  Created by Thomas Cowern on 5/2/25.
+//
+import SwiftUI
+
+struct User: Identifiable, Codable {
+    let id: Int
+    let name: String
+}
