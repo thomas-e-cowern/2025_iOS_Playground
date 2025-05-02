1212f47fa5dda1b9c2c32b45283fb906dfa98499
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-02 08:20:37 -0400

    FakeAPIClient initial commit in iOS-Server-Test folder

---
+++ iOS-Server-Test/FakeAPIClient/FakeAPIClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeAPIClient
+//
+//  Created by Thomas Cowern on 5/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Server-Test/FakeAPIClient/FakeAPIClient/FakeAPIClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeAPIClientApp.swift
+//  FakeAPIClient
+//
+//  Created by Thomas Cowern on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FakeAPIClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
