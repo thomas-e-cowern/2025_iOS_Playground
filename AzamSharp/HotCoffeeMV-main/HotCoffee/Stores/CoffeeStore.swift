e1b77459a9ac1ecb732424a9c7bbbbbb43f70b19
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-05 15:49:28 -0400

    update CoffeeStore with variables and functions to take over VM

--- AzamSharp/HotCoffeeMV-main/HotCoffee/Stores/CoffeeStore.swift
+++ AzamSharp/HotCoffeeMV-main/HotCoffee/Stores/CoffeeStore.swift
@@ -9,5 +9,23 @@ import Observation
 
 @Observable
 class CoffeeStore {
+    let httpClient: HTTPClient
     
+    var orders: [CoffeeOrder] = []
+    
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+    
+    func loadOrders() async throws {
+        let resource = Resource(url: APIs.orders.url, modelType: [CoffeeOrder].self)
+        orders = try await httpClient.load(resource)
+    }
+    
+    func placeOrder(coffeeOrder: CoffeeOrder) async throws {
+        let newOrderData = try JSONEncoder().encode(coffeeOrder)
+        let resource = Resource(url: APIs.addOrder.url, method: .post(newOrderData), modelType: CoffeeOrder.self)
+        let savedCoffeeOrder = try await httpClient.load(resource)
+        orders.append(savedCoffeeOrder)
+    }
 }
