5beb0cb9776ea4cd3e4e6b673b7eb68a5d66f859
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-07 10:00:01 -0400

    add Stores folder and PatientManagementStore file

---
+++ AzamSharp/SharedStore/SharedStore/Stores/PatientManagementStore.swift
@@ -0,0 +1,13 @@
+//
+//  PatientManagementStore.swift
+//  SharedStore
+//
+//  Created by Thomas Cowern on 5/7/25.
+//
+import Foundation
+import Observation
+
+@Observable
+class PatientManagementStore {
+    // More to come...
+}
