da8d1dbd1adcc85b4d9c628a1bc70a9bbf2e0c5c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-07 11:04:32 -0400

    add load functions to PatientManagementStore

--- AzamSharp/SharedStore/SharedStore/Stores/PatientManagementStore.swift
+++ AzamSharp/SharedStore/SharedStore/Stores/PatientManagementStore.swift
@@ -9,5 +9,18 @@ import Observation
 
 @Observable
 class PatientManagementStore {
+    let httpClient: HTTPClient
     var patients: [Patient] = []
+    
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+    
+    func loadPatients() async throws {
+        patients = try await httpClient.load(.patients)
+    }
+    
+    func patientByID(_ id: UUID) -> Patient? {
+        return nil
+    }
 }
