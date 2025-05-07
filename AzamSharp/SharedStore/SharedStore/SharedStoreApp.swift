cc8b578a40e1986394d0bc58ef6c529aa6a89f36
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-07 09:57:53 -0400

    SharedStore initial commit

---
+++ AzamSharp/SharedStore/SharedStore/SharedStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  SharedStoreApp.swift
+//  SharedStore
+//
+//  Created by Thomas Cowern on 5/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SharedStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
