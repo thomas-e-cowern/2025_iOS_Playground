e3e3db164f902fdc495f8222c0bb0f849e244c6a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-09 08:59:59 -0400

    add Appointment model to Models folder

---
+++ AzamSharp/SharedStore/SharedStore/Models/Appointment.swift
@@ -0,0 +1,12 @@
+//
+//  Appointment.swift
+//  SharedStore
+//
+//  Created by Thomas Cowern on 5/9/25.
+//
+import Foundation
+
+struct Appointment {
+    let patient: Patient
+    let allergies: [Allergy]
+}
