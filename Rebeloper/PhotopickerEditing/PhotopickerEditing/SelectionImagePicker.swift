319ed92eb3bf53c44059b93a6c774170adc1fe19
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-13 09:31:46 -0400

    update SelectionImagePicker with buttons to enable choice of image source

--- Rebeloper/PhotopickerEditing/PhotopickerEditing/SelectionImagePicker.swift
+++ Rebeloper/PhotopickerEditing/PhotopickerEditing/SelectionImagePicker.swift
@@ -10,13 +10,30 @@ struct SelectionImagePicker<Label: View>: View {
     
     @Binding var selectedImage: UIImage?
     @ViewBuilder var label: () -> Label
+    @State private var isConfirmationDialogIsPresented: Bool = false
+    @State private var selectedImageSourceType: ImageSourceType?
     
     var body: some View {
         Button {
-            
+            isConfirmationDialogIsPresented.toggle()
         } label: {
             label()
         }
         .buttonStyle(.plain)
+        .confirmationDialog("Choose an Image  ", isPresented: $isConfirmationDialogIsPresented) {
+            Button("Camera") {
+                selectedImageSourceType = .camera
+            }
+            Button("Library") {
+                selectedImageSourceType = .photoLibrary
+            }
+            Button("Cancel", role: .cancel) {
+                // Cancels automatically
+            }
+        }
+        .sheet(item: $selectedImageSourceType) { sourceType in
+            UIImagePicker(selectedImage: $selectedImage, imageSourceType: sourceType)
+                .ignoresSafeArea(.all)
+        }
     }
 }
