62d5d713f6d3a8e5a71e7bda1cd3a01c95488835
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-14 07:36:15 -0400

    EnumViews initial commit in Rebeloper folder

---
+++ Rebeloper/EnumViews/EnumViews/EnumViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnumViewsApp.swift
+//  EnumViews
+//
+//  Created by Thomas Cowern on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct EnumViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
