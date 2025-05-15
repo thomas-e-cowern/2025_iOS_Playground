5184d4db67bb4e2f7152d4ba453120e3b69eda51
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-15 06:04:15 -0400

    add NavigationLink to ContentView

--- Rebeloper/OnAppearOnce/OnAppearOnce/ContentView.swift
+++ Rebeloper/OnAppearOnce/OnAppearOnce/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            NavigationLink {
+                DestinationView()
+            } label: {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Go to link...")
+            }
         }
         .padding()
     }
