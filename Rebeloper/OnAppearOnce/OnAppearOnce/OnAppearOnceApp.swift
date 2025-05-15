83befbbf02e1c0bbb07f528941cb109de148dfa1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-15 05:52:38 -0400

    OnAppearOnce initial commit in Rebeloper folder

---
+++ Rebeloper/OnAppearOnce/OnAppearOnce/OnAppearOnceApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnAppearOnceApp.swift
+//  OnAppearOnce
+//
+//  Created by Thomas Cowern on 5/15/25.
+//
+
+import SwiftUI
+
+@main
+struct OnAppearOnceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
