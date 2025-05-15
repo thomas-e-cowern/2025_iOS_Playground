36c35336cc1c18d0f861559613ca47c558ce61fe
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-15 06:03:50 -0400

    add NavigationStack to App file

--- Rebeloper/OnAppearOnce/OnAppearOnce/OnAppearOnceApp.swift
+++ Rebeloper/OnAppearOnce/OnAppearOnce/OnAppearOnceApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct OnAppearOnceApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationStack {
+                ContentView()
+            }
         }
     }
 }
