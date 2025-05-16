803826217fb9f25a3108de5eb5c26467f74c599c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-16 11:47:25 -0400

    add label to GroupBox in ContentView

--- Skills/GroupBoxTest/GroupBoxTest/ContentView.swift
+++ Skills/GroupBoxTest/GroupBoxTest/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            GroupBox {
+            GroupBox(label: Text("Something")) {
                 Image(systemName: "globe")
                     .imageScale(.large)
                     .foregroundStyle(.tint)
