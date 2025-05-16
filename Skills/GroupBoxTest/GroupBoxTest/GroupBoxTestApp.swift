573010b415ab32bc213955dbe7925049b072effa
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-16 10:16:38 -0400

    GroupBoxTest initial commit in Skills folder

---
+++ Skills/GroupBoxTest/GroupBoxTest/GroupBoxTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBoxTestApp.swift
+//  GroupBoxTest
+//
+//  Created by Thomas Cowern on 5/16/25.
+//
+
+import SwiftUI
+
+@main
+struct GroupBoxTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
