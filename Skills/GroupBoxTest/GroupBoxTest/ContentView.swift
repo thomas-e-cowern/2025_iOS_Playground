045e72466297d4e8dea20cb7af6c16bfe774f2ba
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-16 10:18:50 -0400

    add GroupBox to ConentView

--- Skills/GroupBoxTest/GroupBoxTest/ContentView.swift
+++ Skills/GroupBoxTest/GroupBoxTest/ContentView.swift
@@ -10,10 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            GroupBox {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
         }
         .padding()
     }
