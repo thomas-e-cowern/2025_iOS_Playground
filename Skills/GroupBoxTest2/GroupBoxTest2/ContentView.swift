b6a3a2b0e2f1a13888e92cf09310dd36e597d199
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-18 07:28:22 -0400

    GroupBoxTest2 initial commit in Skills folder

---
+++ Skills/GroupBoxTest2/GroupBoxTest2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroupBoxTest2
+//
+//  Created by Thomas Cowern on 5/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skills/GroupBoxTest2/GroupBoxTest2/GroupBoxTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  GroupBoxTest2App.swift
+//  GroupBoxTest2
+//
+//  Created by Thomas Cowern on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct GroupBoxTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
