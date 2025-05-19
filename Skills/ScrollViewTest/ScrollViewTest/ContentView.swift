25885b96c4cf15de11ee7dd992ba807282c2822d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-19 07:02:25 -0400

    add colors variable to ContentView and update ForEach to use colors

--- Skills/ScrollViewTest/ScrollViewTest/ContentView.swift
+++ Skills/ScrollViewTest/ScrollViewTest/ContentView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var colors: [Color] = [.pink, .red, .blue, .gray, .green, .orange, .teal, .yellow, .black, .indigo]
+    
     var body: some View {
         HStack {
             ScrollView(.horizontal) {
-                ForEach(1..<11) { circle in
-                    Circle()
+                HStack {
+                    ForEach(1..<11) { circle in
+                        Circle()
+                            .frame(width: 100, height: 100)
+                            .foregroundStyle(colors[(Int(circle.description) ?? 0) - 1])
+                    }
                 }
             }
         }
