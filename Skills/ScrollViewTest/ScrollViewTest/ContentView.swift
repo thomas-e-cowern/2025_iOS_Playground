8bc8687e4c308d98865b9273dd565c86c486e8c6
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-19 06:46:25 -0400

    ScrollViewTest initial commit in Skills folder

---
+++ Skills/ScrollViewTest/ScrollViewTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScrollViewTest
+//
+//  Created by Thomas Cowern on 5/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skills/ScrollViewTest/ScrollViewTest/ScrollViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewTestApp.swift
+//  ScrollViewTest
+//
+//  Created by Thomas Cowern on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
