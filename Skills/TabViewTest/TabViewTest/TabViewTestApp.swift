7e64a5b77fd4b8c3aa30a424939df2fec2b99848
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-21 14:59:21 -0400

    TabViewTest initial commit

---
+++ Skills/TabViewTest/TabViewTest/TabViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewTestApp.swift
+//  TabViewTest
+//
+//  Created by Thomas Cowern on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct TabViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
