50c5b05d58109dcd413b6cfc0d3fed1883d8c363
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-21 15:05:27 -0400

    add BookInfoButtonsView, BookInfoView, HorizontalButtonsView, TabSelectionView

---
+++ Skills/TabViewTest/TabViewTest/Views/BookInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  BookInfoView.swift
+//  TabViewTest
+//
+//  Created by Thomas Cowern on 5/21/25.
+//
+
+import SwiftUI
+
+struct BookInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookInfoView()
+}
---
+++ Skills/TabViewTest/TabViewTest/Views/TabSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  TabSelectionView.swift
+//  TabViewTest
+//
+//  Created by Thomas Cowern on 5/21/25.
+//
+
+import SwiftUI
+
+struct TabSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabSelectionView()
+}
