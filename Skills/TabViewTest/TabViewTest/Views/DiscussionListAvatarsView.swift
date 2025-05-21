d3363620f98a0cf4719a939578ea73f3d6bbf233
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-21 15:08:21 -0400

    add DiscussionListAvatarsView, DiscussionListNavigationLinkView, DiscussionListRowView, and DiscussionListView

---
+++ Skills/TabViewTest/TabViewTest/Views/DiscussionListAvatarsView.swift
@@ -0,0 +1,18 @@
+//
+//  DiscussionListAvatarsView.swift
+//  TabViewTest
+//
+//  Created by Thomas Cowern on 5/21/25.
+//
+
+import SwiftUI
+
+struct DiscussionListAvatarsView: View {
+    var body: some View {
+        Text("Avatars View")
+    }
+}
+
+#Preview {
+    DiscussionListAvatarsView()
+}
---
+++ Skills/TabViewTest/TabViewTest/Views/DiscussionListView.swift
@@ -0,0 +1,18 @@
+//
+//  DiscussionListView.swift
+//  TabViewTest
+//
+//  Created by Thomas Cowern on 5/21/25.
+//
+
+import SwiftUI
+
+struct DiscussionListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DiscussionListView()
+}
