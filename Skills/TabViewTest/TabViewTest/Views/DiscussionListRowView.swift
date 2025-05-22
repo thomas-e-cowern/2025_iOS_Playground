6723c6a8ae22a10256492a900c55183595a72034
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-22 17:35:22 -0400

    update DiscussionListRowView, add Text and Label and formatting

--- Skills/TabViewTest/TabViewTest/Views/DiscussionListRowView.swift
+++ Skills/TabViewTest/TabViewTest/Views/DiscussionListRowView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct DiscussionListRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(alignment: .top) {
+                Text("Club Lobby")
+                    .font(.headline)
+                
+                Spacer()
+                
+                Label("42", systemImage: "message")
+                    .font(.subheadline)
+                    .foregroundStyle(.primary)
+            }
+            
+            DiscussionListNavigationLinkView()
+        }
+        .padding(.vertical)
+        .listRowSeparator(.hidden)
     }
 }
 
