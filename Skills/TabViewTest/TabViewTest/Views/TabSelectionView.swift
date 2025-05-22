e086df404f08b684ecb7071c7a00fa7ca8f76dc4
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-22 13:24:02 -0400

    update TabSelectionView

--- Skills/TabViewTest/TabViewTest/Views/TabSelectionView.swift
+++ Skills/TabViewTest/TabViewTest/Views/TabSelectionView.swift
@@ -8,11 +8,41 @@
 import SwiftUI
 
 struct TabSelectionView: View {
+    @Binding var tabSelection: Int
+    @Namespace private var buttonId
+    private let selectionButtons = ["Discussion", "Schedule", "Resources"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(alignment: .top) {
+            ForEach(selectionButtons.indices, id: \.self) { index in
+                VStack {
+                    Button(selectionButtons[index]) {
+                        withAnimation {
+                            tabSelection = index
+                        }
+                    }
+                    .foregroundStyle(tabSelection == index ? .primary : .secondary)
+                    .padding(.horizontal)
+                    
+                    if tabSelection == index {
+                        Capsule()
+                            .frame(width: 80, height: 4)
+                            .padding(.horizontal, 4)
+                            .foregroundStyle(.purple)
+                            .matchedGeometryEffect(id: "ID", in: buttonId)
+                    } else {
+                        EmptyView()
+                            .frame(height: 4)
+                            .matchedGeometryEffect(id: "ID", in: buttonId)
+                    }
+                }
+            }
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    TabSelectionView()
+    TabSelectionView(tabSelection: .constant(1))
 }
