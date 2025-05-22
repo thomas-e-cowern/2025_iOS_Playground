1ebe1cc1c8ae896ee4551aef7444171096ec98bb
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-22 17:32:20 -0400

    update DiscussionListView with List and dummy data

--- Skills/TabViewTest/TabViewTest/Views/DiscussionListView.swift
+++ Skills/TabViewTest/TabViewTest/Views/DiscussionListView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct DiscussionListView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(0..<5) { _ in
+                DiscussionListRowView()
+            }
+        }
+        .listStyle(.plain)
     }
 }
 
