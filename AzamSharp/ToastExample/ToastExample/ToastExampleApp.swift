c4e25a2e9a7d307f8342025b8711578827b88b3e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-23 10:31:41 -0400

    ToastExample initial commit in AzamSharp folder

---
+++ AzamSharp/ToastExample/ToastExample/ToastExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToastExampleApp.swift
+//  ToastExample
+//
+//  Created by Thomas Cowern on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ToastExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
