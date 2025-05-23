8a3549fe6f8ee8560ebd0f19616eeca931370e7a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-23 14:07:24 -0400

    add SettingsView to project

---
+++ AzamSharp/ToastExample/ToastExample/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  ToastExample
+//
+//  Created by Thomas Cowern on 5/23/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings View")
+    }
+}
+
+#Preview {
+    SettingsView()
+}
