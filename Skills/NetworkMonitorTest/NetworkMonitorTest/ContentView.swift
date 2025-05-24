f1a56d220451ab906015314dd820c679422b1338
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-24 10:28:33 -0400

    NetworkMonitorTest initial commit in Skills folder

---
+++ Skills/NetworkMonitorTest/NetworkMonitorTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkMonitorTest
+//
+//  Created by Thomas Cowern on 5/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skills/NetworkMonitorTest/NetworkMonitorTest/NetworkMonitorTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkMonitorTestApp.swift
+//  NetworkMonitorTest
+//
+//  Created by Thomas Cowern on 5/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NetworkMonitorTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
