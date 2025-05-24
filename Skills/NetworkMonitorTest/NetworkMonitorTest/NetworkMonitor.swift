cf5417acca7de4ba80c4a883cc1df891655632ff
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-24 10:38:21 -0400

    add NetworkMonitor file to project

---
+++ Skills/NetworkMonitorTest/NetworkMonitorTest/NetworkMonitor.swift
@@ -0,0 +1,33 @@
+//
+//  NetworkMonitor.swift
+//  NetworkMonitorTest
+//
+//  Created by Thomas Cowern on 5/24/25.
+//
+import Foundation
+import Network
+import Observation
+
+@Observable
+final class NetworkMonitor {
+    private var isConnected: Bool = false
+    private var isCellular: Bool = false
+    
+    private let nwMonitor = NWPathMonitor()
+    private let workerQueue = DispatchQueue.global()
+    
+    public func start() {
+        nwMonitor.start(queue: workerQueue)
+        nwMonitor.pathUpdateHandler = {[weak self] path in
+            DispatchQueue.main.async {
+                self?.isConnected = path.status == .satisfied
+                self?.isCellular = path.usesInterfaceType(.cellular)
+            }
+        }
+    }
+    
+    public func stop() {
+        nwMonitor.cancel()
+    }
+}
+
