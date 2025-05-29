930cda7b7764dab3be09b238089e5dbb15ec676b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-29 10:18:56 -0400

    First initial commit in HackingWithSwift folder

---
+++ HackingWithSwift/First/First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Thomas Cowern on 5/29/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackingWithSwift/First/FirstTests/FirstTests.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTests.swift
+//  FirstTests
+//
+//  Created by Thomas Cowern on 5/29/25.
+//
+
+import Testing
+@testable import First
+
+struct FirstTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
