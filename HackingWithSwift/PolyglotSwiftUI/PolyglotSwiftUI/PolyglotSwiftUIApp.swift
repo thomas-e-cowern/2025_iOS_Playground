b3282fda697bedc4780b51cb1639b849b381cc21
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-30 08:18:27 -0400

    PolyglotSwiftUI initial commit to HackingWithSwift folder

---
+++ HackingWithSwift/PolyglotSwiftUI/PolyglotSwiftUI/PolyglotSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolyglotSwiftUIApp.swift
+//  PolyglotSwiftUI
+//
+//  Created by Thomas Cowern on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PolyglotSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
