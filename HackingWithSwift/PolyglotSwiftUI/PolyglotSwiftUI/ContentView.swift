ba5740125725815a4905074b36ffdc860063737d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-30 08:30:56 -0400

    add variable, onAppear and saveInitialValues function to ContentView

--- HackingWithSwift/PolyglotSwiftUI/PolyglotSwiftUI/ContentView.swift
+++ HackingWithSwift/PolyglotSwiftUI/PolyglotSwiftUI/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var words: [String] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+            .onAppear {
+                let defaults = UserDefaults.standard
+                
+                if let savedWords = defaults.object(forKey: "Words") as? [String] {
+                    words = savedWords
+                } else {
+                    saveInitialValues(to: defaults)
+                }
+            }
+            .navigationTitle("POLYGLOT")
         }
-        .padding()
+    }
+    
+    func saveInitialValues(to defaults: UserDefaults) {
+        words.append("bear::l'ours")
+        words.append("camel::le chameau")
+        words.append("cow::la vache")
+        words.append("fox::le renard")
+        words.append("goat::la chèvre")
+        words.append("monkey::le singe")
+        words.append("pig::le cochon")
+        words.append("rabbit::le lapin")
+        words.append("sheep::le mouton")
+
+        defaults.set(words, forKey: "Words")
     }
 }
 
