f6a7c3a7403b5bf7d4f70730657a30756ccff769
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-05-30 13:57:21 -0400

    add AddWordsView to project, update ContentView to include AddWordsView in sheet modifier

---
+++ HackingWithSwift/PolyglotSwiftUI/PolyglotSwiftUI/AddWordsView.swift
@@ -0,0 +1,13 @@
+//
+//  AddWordsView.swift
+//  PolyglotSwiftUI
+//
+//  Created by Thomas Cowern on 5/30/25.
+//
+import SwiftUI
+
+struct AddWordsView: View {
+    var body: some View {
+        Text("Words View")
+    }
+}
