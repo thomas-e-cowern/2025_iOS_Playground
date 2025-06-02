8c91f638b7b7feda05fe38b1b77fbaca5f19d447
Thomas Cowern <thomascowern@mac.attlocal.net>
2025-06-02 09:32:56 -0400

    FloatingTabBar initial commit in Skills folder

---
+++ Skills/FloatingTabBar/FloatingTabBar/FloatingTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatingTabBarApp.swift
+//  FloatingTabBar
+//
+//  Created by Thomas Cowern on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FloatingTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
