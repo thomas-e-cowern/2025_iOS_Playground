1ca773074353d5943dabca4498965127e75b5845
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-04 15:28:07 -0400

    update FlotaingTabView with padding

--- Skills/FloatingTabBar/FloatingTabBar/FloatingTabView.swift
+++ Skills/FloatingTabBar/FloatingTabBar/FloatingTabView.swift
@@ -44,7 +44,29 @@ struct FloatingTabView<Content: View, Value: CaseIterable & Hashable & FloatingT
             }
             
             FloatingTabBar(activeTab: $selection, config: config)
+                .padding(.horizontal, config.hPadding)
+                .padding(.vertical, config.bPadding)
             
         }
+    }
+}
+
+class FloatingTabViewHelper: ObservableObject {
+    @Published var hideTabBar: Bool = false
+}
+
+struct HideFloatingTabBarModifier: ViewModifier {
+    var status: Bool
+    @EnvironmentObject private var helper: FloatingTabViewHelper
+    
+    func body(content: Content) -> some View {
+        content
+            .onChange(of: status, initial: true) { oldValue, newValue in
+                helper.hideTabBar = newValue
+            }
+    }
+    
+    private func concat() {
+        
     }
 }
