fce1fd9ea5610f6bf48129db6657e92601960bf9
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-05 14:28:02 -0400

    initial commit of CustomTabBar in Skills folder

---
+++ Skills/CustomTabBar/CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Thomas Cowern on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
