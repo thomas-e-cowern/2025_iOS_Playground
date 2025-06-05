757a101f96e8f10047dc3a5ec8f145057f6d7491
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-05 14:44:00 -0400

    add Spacer, .foregroundStyle and padding to CustomTabBar

--- Skills/CustomTabBar/CustomTabBar/CustomTabBar.swift
+++ Skills/CustomTabBar/CustomTabBar/CustomTabBar.swift
@@ -7,32 +7,46 @@
 import SwiftUI
 
 struct CustomTabBar: View {
+    
+    @Binding var index: Int
+    
     var body: some View {
         HStack {
             Button {
-                // More to come...
+                self.index = 0
             } label: {
                 Image(systemName: "house")
             }
+            .foregroundStyle(Color.blue.opacity(self.index == 0 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
             
             Button {
-                // More to come...
+                self.index = 1
             } label: {
                 Image(systemName: "magnifyingglass")
             }
+            .foregroundStyle(Color.blue.opacity(self.index == 1 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
             
             Button {
-                // More to come...
+                self.index = 2
             } label: {
                 Image(systemName: "heart")
             }
+            .foregroundStyle(Color.blue.opacity(self.index == 2 ? 1 : 0.2))
+            
+            Spacer(minLength: 0)
             
             Button {
-                // More to come...
+                self.index = 3
             } label: {
                 Image(systemName: "person")
             }
+            .foregroundStyle(Color.blue.opacity(self.index == 3 ? 1 : 0.2))
 
         }
+        .padding(.horizontal, 35)
     }
 }
