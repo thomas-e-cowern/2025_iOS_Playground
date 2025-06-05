93e91e78fa597ab2498d43dffb6c919c00470f8b
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-05 15:31:46 -0400

    update CustomTabBar, add plus button and formatting

--- Skills/CustomTabBar/CustomTabBar/CustomTabBar.swift
+++ Skills/CustomTabBar/CustomTabBar/CustomTabBar.swift
@@ -31,6 +31,24 @@ struct CustomTabBar: View {
             Spacer(minLength: 0)
             
             Button {
+                // More to come...
+            } label: {
+                ZStack {
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 70)
+                    Circle()
+                        .frame(width: 50)
+                    Image(systemName: "plus")
+                        .foregroundStyle(Color.white)
+                        .font(.largeTitle)
+                }
+            }
+            .offset(y: -25)
+
+            Spacer()
+            
+            Button {
                 self.index = 2
             } label: {
                 Image(systemName: "heart")
@@ -48,5 +66,7 @@ struct CustomTabBar: View {
 
         }
         .padding(.horizontal, 35)
+        .background(Color.white)
+        .offset(y: 12)
     }
 }
