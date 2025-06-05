622c5cde9cb27c6e727b5f6bc0a6d3cf02210c63
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-05 14:34:41 -0400

    add CustomTabBar to project

---
+++ Skills/CustomTabBar/CustomTabBar/CustomTabBar.swift
@@ -0,0 +1,38 @@
+//
+//  CustomTabBar.swift
+//  CustomTabBar
+//
+//  Created by Thomas Cowern on 6/5/25.
+//
+import SwiftUI
+
+struct CustomTabBar: View {
+    var body: some View {
+        HStack {
+            Button {
+                // More to come...
+            } label: {
+                Image(systemName: "house")
+            }
+            
+            Button {
+                // More to come...
+            } label: {
+                Image(systemName: "magnifyingglass")
+            }
+            
+            Button {
+                // More to come...
+            } label: {
+                Image(systemName: "heart")
+            }
+            
+            Button {
+                // More to come...
+            } label: {
+                Image(systemName: "person")
+            }
+
+        }
+    }
+}
