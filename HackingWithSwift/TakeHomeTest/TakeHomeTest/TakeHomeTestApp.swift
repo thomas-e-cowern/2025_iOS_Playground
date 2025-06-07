a07681a7e9c41b5bbf62ae5b23009ae065b176b1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-07 09:45:09 -0400

    TakeHomeTest initial commit in HackingWithSwift folder

---
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/TakeHomeTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestApp.swift
+//  TakeHomeTest
+//
+//  Created by Thomas Cowern on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
