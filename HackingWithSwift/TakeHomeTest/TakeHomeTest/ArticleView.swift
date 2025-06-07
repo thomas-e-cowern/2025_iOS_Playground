9d50c605dcab8635f85cd7ec382f4d001b0a573c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-07 13:15:06 -0400

    add ArticleView to project

---
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/ArticleView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleView.swift
+//  TakeHomeTest
+//
+//  Created by Thomas Cowern on 6/7/25.
+//
+import SwiftUI
+
+struct ArticleView: View {
+    
+    var article: Article
+    
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    ArticleView(article: .example)
+}
