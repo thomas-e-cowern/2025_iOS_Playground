09530a70a2581ef4d693d54887d9620b010d3375
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-08 08:45:01 -0400

    update Contentview, remove List code and move to ArticleRow

--- HackingWithSwift/TakeHomeTest/TakeHomeTest/ContentView.swift
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/ContentView.swift
@@ -14,34 +14,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             VStack {
-                List(viewModel.articles, id: \.self) { article in
-                    NavigationLink(value: article) {
-                        HStack {
-                            AsyncImage(url: article.thumbnail) { phase in
-                                switch phase {
-                                case .empty:
-                                    ProgressView()
-                                case .success(let image):
-                                    image
-                                        .resizable()
-                                        .scaledToFill()
-                                default:
-                                    Image(systemName: "newspaper")
-                                }
-                            }
-                                .frame(width: 80, height: 80)
-                                .clipShape(.rect(cornerRadius: 10))
-                                .scaledToFit()
-                            
-                            VStack(alignment: .leading) {
-                                Text(article.section)
-                                    .font(.caption.weight(.heavy))
-                                
-                                Text(article.title)
-                            }
-                        }
-                    }
-                }
+                List(viewModel.articles, rowContent: ArticleRow.init)
                 .navigationDestination(for: Article.self, destination: ArticleView.init)
                 .navigationTitle("Take Home Test")
             }
