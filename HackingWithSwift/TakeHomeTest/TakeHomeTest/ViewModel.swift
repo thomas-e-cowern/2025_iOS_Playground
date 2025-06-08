1451a94968f705f63d2059dd15ca25bf7ab5f582
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-08 13:02:40 -0400

    add filterText variable and filtereArticles function to ViewModel

--- HackingWithSwift/TakeHomeTest/TakeHomeTest/ViewModel.swift
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/ViewModel.swift
@@ -21,6 +21,16 @@ extension ContentView {
         private(set) var loadState = LoadState.loading
         private(set) var loadError: (any Error)?
         
+        var filterText = ""
+        
+        var filteredArticles: [Article] {
+            if filterText.isEmpty {
+                articles
+            } else {
+                articles.filter{ $0.title.localizedStandardContains(filterText) }
+            }
+        }
+        
         func loadArticles() async {
             loadState = .loading
             do {
