76fd25fc9f3e03ec42f9a867d8fa80f96232f82c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-08 08:23:41 -0400

    add fetchArticles function to ViewModel with associated variables

--- HackingWithSwift/TakeHomeTest/TakeHomeTest/ViewModel.swift
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/ViewModel.swift
@@ -10,5 +10,18 @@ extension ContentView {
     @Observable @MainActor
     class ViewModel {
         
+        private(set) var articles: [Article] = []
+        
+        func loadArticles() async {
+            do {
+                let url = URL(string: "https://www.hackingwithswift.com/samples/news")!
+                let (data, _) = try await URLSession.shared.data(from: url)
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                articles = try decoder.decode([Article].self, from: data)
+            } catch {
+                print("Error in loadArticles: \(error.localizedDescription)")
+            }
+        }
     }
 }
