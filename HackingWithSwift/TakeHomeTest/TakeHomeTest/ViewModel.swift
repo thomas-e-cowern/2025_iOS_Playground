830d8e5b10505fd8900ec49fc4122b61ae55c7f0
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-08 10:52:05 -0400

    update ViewModel, add LoadState enum and variables

--- HackingWithSwift/TakeHomeTest/TakeHomeTest/ViewModel.swift
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/ViewModel.swift
@@ -7,20 +7,33 @@
 import SwiftUI
 
 extension ContentView {
+    
+    enum LoadState {
+        case loading
+        case loaded
+        case failed
+    }
+    
     @Observable @MainActor
     class ViewModel {
         
         private(set) var articles: [Article] = []
+        private(set) var loadState = LoadState.loading
+        private(set) var loadError: (any Error)?
         
         func loadArticles() async {
+            loadState = .loading
             do {
                 let url = URL(string: "https://www.hackingwithswift.com/samples/news")!
                 let (data, _) = try await URLSession.shared.data(from: url)
                 let decoder = JSONDecoder()
                 decoder.dateDecodingStrategy = .iso8601
                 articles = try decoder.decode([Article].self, from: data)
+                loadState = .loaded
             } catch {
                 print("Error in loadArticles: \(error.localizedDescription)")
+                loadState = .failed
+                loadError = error
             }
         }
     }
