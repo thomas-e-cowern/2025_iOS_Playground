52491639876766bed701a2bd663ece48148c5538
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-08 08:13:14 -0400

    add ViewModel file to project

---
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/ViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  ViewModel.swift
+//  TakeHomeTest
+//
+//  Created by Thomas Cowern on 6/8/25.
+//
+import SwiftUI
+
+extension ContentView {
+    @Observable @MainActor
+    class ViewModel {
+        
+    }
+}
