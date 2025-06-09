f2a848bc4e2d8ec96f9b2f4674e572e6f1083936
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-09 07:09:49 -0400

    add DataFetching protocol to project

---
+++ HackingWithSwift/TakeHomeTest/TakeHomeTest/DataFetching.swift
@@ -0,0 +1,11 @@
+//
+//  DataFetching.swift
+//  TakeHomeTest
+//
+//  Created by Thomas Cowern on 6/9/25.
+//
+import Foundation
+
+protocol DataFetching: Sendable {
+    func data(from url: URL) async throws -> (Data, URLResponse)
+}
--- HackingWithSwift/TakeHomeTest/TakeHomeTestTests/TakeHomeTestTests.swift
+++ HackingWithSwift/TakeHomeTest/TakeHomeTestTests/TakeHomeTestTests.swift
@@ -6,11 +6,51 @@
 //
 
 import Testing
+import Foundation
+@testable import TakeHomeTest
 
+@MainActor
 struct TakeHomeTestTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test func viewModelStartsEmpty() async throws {
+        let viewModel = ContentView.ViewModel()
+        
+        #expect(viewModel.articles.isEmpty, "There should be not article initially")
+        #expect(viewModel.loadState == .loading, "The viewModel should be loading")
+    }
+    
+    @Test func viewModelLoadsArticle() async throws {
+        let viewModel = ContentView.ViewModel()
+        await viewModel.loadArticles()
+        
+        // Same test different ways
+        #expect(viewModel.articles.count > 0, "There should be articles after loading")
+        #expect(viewModel.articles.isEmpty == false, "There should be articles after loading")
+        
+        // Load state test
+        #expect(viewModel.loadState == .loaded, "The viewModel should be loaded")
+    }
+    
+    @Test func viewModelNoFiltering() async throws {
+        let viewModel = ContentView.ViewModel()
+        await viewModel.loadArticles()
+        
+        #expect(viewModel.filteredArticles == viewModel.articles, "No filter, all articles should be shown")
+    }
+    
+    @Test func viewModelFilterOne() async throws {
+        let viewModel = ContentView.ViewModel()
+        await viewModel.loadArticles()
+        viewModel.filterText = viewModel.articles.first?.title ?? ""
+        
+        #expect(viewModel.filteredArticles.count == 1, "Only one article should remain after filtering")
     }
 
+    @Test func viewModelFilterEmpty() async throws {
+        let viewModel = ContentView.ViewModel()
+        await viewModel.loadArticles()
+        viewModel.filterText = "XXX NOT HAPPENING"
+        
+        #expect(viewModel.filteredArticles.count == 0, "No articles should remain after filtering")
+    }
 }
