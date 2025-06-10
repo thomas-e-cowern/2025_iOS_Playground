456d3b60b387e399bb2b6ea466f0b5c904029e6f
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-10 08:55:44 -0400

    SwiftUIRouting initial commit in Skills folder

---
+++ Skills/SwiftUIRouting/SwiftUIRouting/SwiftUIRoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRoutingApp.swift
+//  SwiftUIRouting
+//
+//  Created by Thomas Cowern on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
