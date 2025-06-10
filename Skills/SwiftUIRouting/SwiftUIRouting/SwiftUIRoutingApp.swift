c0edd5c7ef85a33421823f4f08fe1b979d3c91fb
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-10 11:12:44 -0400

    inject appRoutes into App file

--- Skills/SwiftUIRouting/SwiftUIRouting/SwiftUIRoutingApp.swift
+++ Skills/SwiftUIRouting/SwiftUIRouting/SwiftUIRoutingApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct SwiftUIRoutingApp: App {
+    
+    @StateObject private var appRouts: Router<AppRoutes> = .init()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(appRouts)
         }
     }
 }
