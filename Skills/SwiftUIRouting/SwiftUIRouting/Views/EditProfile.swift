4e71138f687d42733f8b89e01047ccfbf4f24b39
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-10 11:57:48 -0400

    update Profile and EditProfile with buttons and navigation code

--- Skills/SwiftUIRouting/SwiftUIRouting/Views/EditProfile.swift
+++ Skills/SwiftUIRouting/SwiftUIRouting/Views/EditProfile.swift
@@ -8,16 +8,33 @@
 import SwiftUI
 
 struct EditProfile: View {
+    
+    @EnvironmentObject var router: Router<AppRoutes>
+    
     var body: some View {
-        HStack {
-            Image(systemName: "person.crop.circle")
-                .font(.largeTitle)
-            Text("Edit Profile View")
-                .font(.largeTitle)
+        VStack {
+            HStack {
+                Image(systemName: "person.crop.circle")
+                    .font(.largeTitle)
+                Text("Edit Profile View")
+                    .font(.largeTitle)
+            }
+            
+            Button {
+                router.navigateToRoot()
+            } label: {
+                Image(systemName: "house")
+                    .font(.largeTitle)
+                Text("Home")
+                    .font(.largeTitle)
+            }
+            .buttonStyle(.borderedProminent)
+
         }
     }
 }
 
 #Preview {
     EditProfile()
+        .environmentObject(Router<AppRoutes>())
 }
--- Skills/SwiftUIRouting/SwiftUIRouting/Views/Profile.swift
+++ Skills/SwiftUIRouting/SwiftUIRouting/Views/Profile.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct Profile: View {
+    
+    @EnvironmentObject var router: Router<AppRoutes>
+    
     var body: some View {
-        HStack {
-            Image(systemName: "person")
-                .font(.largeTitle)
-            Text("Profile View")
-                .font(.largeTitle)
+        VStack {
+            HStack {
+                Image(systemName: "person")
+                    .font(.largeTitle)
+                Text("Profile View")
+                    .font(.largeTitle)
+            }
+            
+            Button {
+                router.navigate(to: .editProfile)
+            } label: {
+                Image(systemName: "person.crop.circle")
+                Text("Edit Profile")
+            }
+            .buttonStyle(.borderedProminent)
+
+            
+            Button {
+                router.navigateBack(1)
+            } label: {
+                Image(systemName: "arrow.left")
+            }
+            .buttonStyle(.bordered)
         }
     }
 }
 
 #Preview {
     Profile()
+        .environmentObject(Router<AppRoutes>())
 }
