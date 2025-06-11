5061aec67d4bfc33668ff1e9bebd2464db961bd9
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-11 08:29:06 -0400

    CustomBirthdayRouter initial commit in skills folder

---
+++ Skills/CustomBirthdayRouter/CustomBirthdayRouter/CustomBirthdayRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBirthdayRouterApp.swift
+//  CustomBirthdayRouter
+//
+//  Created by Thomas Cowern on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomBirthdayRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
