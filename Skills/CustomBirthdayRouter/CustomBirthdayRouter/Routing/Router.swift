e837c475ee226a5568311ef66fb91afa31b52f68
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-11 09:29:03 -0400

    add Router file to project

---
+++ Skills/CustomBirthdayRouter/CustomBirthdayRouter/Routing/Router.swift
@@ -0,0 +1,59 @@
+//
+//  Router.swift
+//  CustomBirthdayRouter
+//
+//  Created by Thomas Cowern on 6/11/25.
+//
+
+import Foundation
+import SwiftUI
+
+enum Route: Hashable {
+    case home
+    case settings
+    case favorites
+}
+
+@Observable
+class Router {
+    var path = NavigationPath()
+    
+    func navigateTo(route: Route) {
+        path.append(route)
+    }
+}
+
+struct RouterViewModifier: ViewModifier {
+
+    @State private var router = Router()
+    
+    func navigateTo(route: Route) -> some View {
+        Group {
+            switch route {
+            case .home:
+                Home()
+            case .settings:
+                Settings()
+            case .favorites:
+                Favorites()
+            }
+        }
+        .environment(router)
+    }
+    
+    func body(content: Content) -> some View {
+        NavigationStack(path: $router.path) {
+            content
+                .environment(router)
+                .navigationDestination(for: Route.self) { route in
+                    navigateTo(route: route)
+                }
+        }
+    }
+}
+
+extension View {
+    func withRouter() -> some View {
+        modifier(RouterViewModifier())
+    }
+}
