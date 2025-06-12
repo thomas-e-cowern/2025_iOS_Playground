e6e6752e75f5b4ea190b3be463ea855a479060e9
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-12 14:10:30 -0400

    add PriviewsDataTest to AzamSharp folder

---
+++ AzamSharp/PriviewsDataTest/PriviewsDataTest/PriviewsDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PriviewsDataTestApp.swift
+//  PriviewsDataTest
+//
+//  Created by Thomas Cowern on 6/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PriviewsDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
