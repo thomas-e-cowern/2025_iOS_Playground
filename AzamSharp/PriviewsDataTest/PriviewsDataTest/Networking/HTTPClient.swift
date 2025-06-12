05948d7bc0c2d695e0b22136bf318152c0b479f1
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-12 15:09:39 -0400

    add Networking folder with HTTPClient

--- AzamSharp/PriviewsDataTest/PriviewsDataTest/Networking/HTTPClient.swift
+++ AzamSharp/PriviewsDataTest/PriviewsDataTest/Networking/HTTPClient.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+
+protocol HTTPClientProtocol {
+    func load<T: Codable>(_ resource: Resource<T>) async throws -> T
+}
+
+struct Resource<T: Codable> {
+    let url: URL
+    var method: HTTPMethod = .get([])
+    var headers: [String: String]? = nil
+    var modelType: T.Type
+}
+enum HTTPMethod {
+    case get([URLQueryItem])
+    case post(Data?)
+    case delete
+    case put(Data?)
+    
+    var name: String {
+        switch self {
+        case .get:
+            return "GET"
+        case .post:
+            return "POST"
+        case .delete:
+            return "DELETE"
+        case .put:
+            return "PUT"
+        }
+    }
+}
