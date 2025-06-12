e014e3fd4be68f8418423e1b08a38dff723fbba7
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-12 15:20:45 -0400

    add MockHTTPClient to project

---
+++ AzamSharp/PriviewsDataTest/PriviewsDataTest/Networking/MockHTTPClient.swift
@@ -0,0 +1,22 @@
+//
+//  MockHTTPClient.swift
+//  PriviewsDataTest
+//
+//  Created by Thomas Cowern on 6/12/25.
+//
+
+import Foundation
+
+struct MockHTTPClient: HTTPClientProtocol {
+    func load<T>(_ resource: Resource<T>) async throws -> T where T : Decodable, T : Encodable {
+        
+        guard let filename = resource.url.pathComponents.last else {
+            print("Error in MockHTTPClient getting fileName")
+            throw URLError(.badURL)
+        }
+        
+        let jsonFreeFileName = filename.replacingOccurrences(of: "json", with: "")
+        
+        return PreviewData.load(jsonFreeFileName)
+    }
+}
