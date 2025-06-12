e64149aae4ec452a2d508e428b51f04809d3fb1d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-12 14:33:42 -0400

    update PreviewData with load method

--- AzamSharp/PriviewsDataTest/PriviewsDataTest/PreviewContent/PreviewData.swift
+++ AzamSharp/PriviewsDataTest/PriviewsDataTest/PreviewContent/PreviewData.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+struct PreviewData {
+    
+    static func load<T: Decodable>(_ filename: String, as type: T.Type = T.self) -> T {
+        
+        let data: Data
+        
+        // Find the file
+        guard let file = Bundle.main.url(forResource: filename, withExtension: "json") else {
+            fatalError("Couldn't find \(filename).json in the main buncle")
+        }
+        
+        // Load data
+        do {
+            data = try Data(contentsOf: file)
+        } catch {
+            fatalError("Unable to load data from \(filename).json.  Error: \(error.localizedDescription)")
+        }
+        
+        // Decode json
+        do {
+            let decoder = JSONDecoder()
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            fatalError("Unable to decode data from \(filename).json.  Error: \(error.localizedDescription)")
+        }
+    }
+}
