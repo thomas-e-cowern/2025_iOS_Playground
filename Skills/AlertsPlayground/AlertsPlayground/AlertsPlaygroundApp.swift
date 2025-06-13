ccefed2bef624404141579f94bacd4407ba0b951
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-06-13 13:41:00 -0400

    AlertsPlayground initial commit in Skills folder

---
+++ Skills/AlertsPlayground/AlertsPlayground/AlertsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertsPlaygroundApp.swift
+//  AlertsPlayground
+//
+//  Created by Thomas Cowern on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct AlertsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
