daa0e558c0864c244fa7a3695a77bc7ecdb784bd
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-18 13:08:48 -0400

    CustomBindingsPlayground initial commit

---
+++ Skills/CustomBindingsPlayground/CustomBindingsPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomBindingsPlayground
+//
+//  Created by Thomas Cowern on 6/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Skills/CustomBindingsPlayground/CustomBindingsPlayground/CustomBindingsPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomBindingsPlaygroundApp.swift
+//  CustomBindingsPlayground
+//
+//  Created by Thomas Cowern on 6/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomBindingsPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
