330aaeb78bfe7ce758c314d7d373cfddffae4e9e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-18 15:18:18 -0400

    update NonOptionalExample with computedVariable

--- Skills/CustomBindingsPlayground/CustomBindingsPlayground/Tabs/NonOptionalExample.swift
+++ Skills/CustomBindingsPlayground/CustomBindingsPlayground/Tabs/NonOptionalExample.swift
@@ -8,7 +8,15 @@
 import SwiftUI
 
 struct NonOptionalExample: View {
-    @State private var sliderValue: Double = 5.0
+    @State private var sliderValue: Int = 5
+    private var doubleBinding: Binding<Double> {
+        Binding {
+            Double(sliderValue)
+        } set: {
+            sliderValue = Int($0)
+        }
+
+    }
     var body: some View {
         NavigationStack {
             Text("There other cases where creating your own custom bindings can be really useful, particularly if your underlying models use different Types from what the controls you are using require, like we saw with optionals for a TextField to a String or a DatePicker.")
@@ -17,7 +25,7 @@ struct NonOptionalExample: View {
                 .padding()
             Form {
                 Section("Binding a Slider to an Int") {
-                    Slider(value: $sliderValue, in: 1...100, step: 1.0)
+                    Slider(value: doubleBinding, in: 1...100, step: 1.0)
                     Text("\(Int(sliderValue))")
                         .font(.system(size: 100))
                         .frame(maxWidth: .infinity, alignment: .center)
