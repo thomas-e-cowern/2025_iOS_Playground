938b942641441cbb9360a64378e8491a189c859a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-19 10:19:30 -0400

    DuolingoStyleButtons initial commit in Skills folder

---
+++ Skills/DuolingoStyleButtons/DuolingoStyleButtons/DepthButtonStyle.swift
@@ -0,0 +1,8 @@
+//
+//  DepthButtonStyle.swift
+//  DuolingoStyleButtons
+//
+//  Created by Thomas Cowern on 6/19/25.
+//
+
+import Foundation
---
+++ Skills/DuolingoStyleButtons/DuolingoStyleButtons/DuolingoStyleButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuolingoStyleButtonsApp.swift
+//  DuolingoStyleButtons
+//
+//  Created by Thomas Cowern on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct DuolingoStyleButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
