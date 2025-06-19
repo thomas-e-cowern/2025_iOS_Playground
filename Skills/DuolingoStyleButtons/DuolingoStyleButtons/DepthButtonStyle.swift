2d4ba9f4586ceef8a4f4ce423d075484178fc599
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-19 10:29:30 -0400

    update DepthButtonStyle with formatting and configuration

--- Skills/DuolingoStyleButtons/DuolingoStyleButtons/DepthButtonStyle.swift
+++ Skills/DuolingoStyleButtons/DuolingoStyleButtons/DepthButtonStyle.swift
@@ -6,3 +6,63 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct DepthButtonStyle: ButtonStyle {
+    private enum Shape {
+        case rectangle
+        case ellipse
+    }
+    
+    private var foregroundColor: Color
+    private var backgroundColor: Color
+    private var shape: Shape
+    private var cornerRadius: CGFloat = 0
+    private var yOffset: CGFloat {
+        shape == .rectangle ? 4 : 8
+    }
+    
+    // rectangle button
+    init(
+        foregroundColor: Color,
+        backgroundColor: Color,
+        cornerRadius: CGFloat
+    ) {
+        self.foregroundColor = foregroundColor
+        self.backgroundColor = backgroundColor
+        self.shape = .rectangle
+        self.cornerRadius = cornerRadius
+    }
+    
+    // Ellipse button
+    init(
+        foregroundColor: Color,
+        backgroundColor: Color
+    ) {
+        self.foregroundColor = foregroundColor
+        self.backgroundColor = backgroundColor
+        self.shape = .ellipse
+    }
+    
+    func makeBody(configuration: Configuration) -> some View {
+        ZStack {
+            buttonShape(color: backgroundColor)
+            buttonShape(color: foregroundColor)
+                .offset(y: configuration.isPressed ? 0 : -yOffset)
+            configuration.label
+                .foregroundStyle(foregroundColor)
+                .offset(y: -yOffset)
+                .offset(y: configuration.isPressed ? yOffset : 0)
+        }
+    }
+    
+    @ViewBuilder
+    private func buttonShape(color: Color) -> some View {
+        switch shape {
+        case .rectangle:
+            RoundedRectangle(cornerRadius: cornerRadius).fill(color)
+        case .ellipse:
+            Ellipse().fill(color)
+        }
+    }
+}
