cf5cd33abcc2903446443b9ad261c5fc3e1f106c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-20 12:33:28 -0400

    HealthKitPlayground initial commit in Skills folde

---
+++ Skills/HealthKitPlayground/HealthKitPlayground/HealthKitPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitPlaygroundApp.swift
+//  HealthKitPlayground
+//
+//  Created by Thomas Cowern on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
