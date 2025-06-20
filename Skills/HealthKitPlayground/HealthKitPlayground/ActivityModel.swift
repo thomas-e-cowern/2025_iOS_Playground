e32ae397b01300ea36ef48f2dde5406eef1d0677
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-20 13:43:48 -0400

    add ActivityModel to project

---
+++ Skills/HealthKitPlayground/HealthKitPlayground/ActivityModel.swift
@@ -0,0 +1,16 @@
+//
+//  ActivityModel.swift
+//  HealthKitPlayground
+//
+//  Created by Thomas Cowern on 6/20/25.
+//
+
+import Foundation
+
+struct Activity {
+    let id: Int
+    let title: String
+    let subtitle: String
+    let imageName: String
+    let amount: String
+}
