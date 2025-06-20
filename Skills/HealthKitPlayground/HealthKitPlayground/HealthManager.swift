d96ebcb1ad5f3401fca2cd0c6c52e69e5776c9dc
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-20 13:13:04 -0400

    add HealthManager to project

--- Skills/HealthKitPlayground/HealthKitPlayground/HealthManager.swift
+++ Skills/HealthKitPlayground/HealthKitPlayground/HealthManager.swift
@@ -6,3 +6,25 @@
 //
 
 import Foundation
+import HealthKit
+
+@Observable
+class HealthManager {
+    
+    let healthStore = HKHealthStore()
+    
+    init () {
+        let steps = HKQuantityType(.stepCount)
+        
+        let healthTypes: Set<HKQuantityType> = [steps]
+        
+        Task {
+            do {
+                try await healthStore.requestAuthorization(toShare: [], read: healthTypes)
+            } catch {
+                print("Error fetching health data in HealthManager: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
