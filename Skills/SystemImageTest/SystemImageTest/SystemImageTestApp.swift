3b995b37b7adc9975b56a1ce0d081247374022e6
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-24 10:36:26 -0400

    initial commit of SystemImageTest in Skills folder

---
+++ Skills/SystemImageTest/SystemImageTest/SystemImageTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SystemImageTestApp.swift
+//  SystemImageTest
+//
+//  Created by Thomas Cowern on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SystemImageTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
