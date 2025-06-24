2c5ede41123355efab035bb2827dda1c9a8a8bdd
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-06-24 10:42:14 -0400

    update ContentView with symbols array and ForEach displaying symbols

--- Skills/SystemImageTest/SystemImageTest/ContentView.swift
+++ Skills/SystemImageTest/SystemImageTest/ContentView.swift
@@ -8,12 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var symbols: [String] = ["star.fill", "swirl.circle.righthalf.filled.inverse", "circle.dotted.circle.fill", "person.fill", "sun.min.fill"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ForEach(symbols, id: \.self) { symbol in
+                Image(systemName: symbol)
+                    .padding(10)
+            }
         }
         .padding()
     }
