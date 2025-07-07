4e422e037a0dd8d1118e41c0f09ec0e331cbbb88
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-07 16:27:06 -0400

    add SolidPlayground to Rebeloper folder

---
+++ Rebeloper/SolidPlayground/SolidPlayground/SolidPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidPlaygroundApp.swift
+//  SolidPlayground
+//
+//  Created by Thomas Cowern on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct SolidPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
