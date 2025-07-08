889097fe960731f847f73e171d60492e7882239a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-08 07:03:40 -0400

    add CustomTabBar to Rebeloper folder

---
+++ Rebeloper/CustomTabBar/CustomTabBar/CustomTabBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTabBarApp.swift
+//  CustomTabBar
+//
+//  Created by Thomas Cowern on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomTabBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
