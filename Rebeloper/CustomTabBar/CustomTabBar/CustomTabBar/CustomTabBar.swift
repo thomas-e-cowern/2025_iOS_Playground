10053ef467a8e9427d1f0606a82c78af81995e1d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-08 08:30:06 -0400

    add tabs array and tabs from function to CustomTabBar

--- Rebeloper/CustomTabBar/CustomTabBar/CustomTabBar/CustomTabBar.swift
+++ Rebeloper/CustomTabBar/CustomTabBar/CustomTabBar/CustomTabBar.swift
@@ -12,8 +12,23 @@ struct CustomTabBar: View {
     @Binding var selectedTab: String
     var animation: Namespace.ID
     
+    let tabs: [String] = ["Home", "Search", "Account"]
+    
     var body: some View {
         Text("Custom Tab Bar")
+    }
+    
+    private func icon(for tabs: String) -> String {
+        switch tabs {
+        case "Home":
+            return "house"
+        case "Search":
+            return "magnifyingglass"
+        case "Account":
+            return "person.circle"
+        default:
+            return "quationmark"
+        }
     }
 }
 
