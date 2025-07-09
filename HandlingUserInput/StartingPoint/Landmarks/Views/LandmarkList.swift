c1efed94a4455bc3d33f41b8f82188502690f024
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-09 07:56:44 -0400

    update LandmarkList with Toggle to view favorites

--- HandlingUserInput/StartingPoint/Landmarks/Views/LandmarkList.swift
+++ HandlingUserInput/StartingPoint/Landmarks/Views/LandmarkList.swift
@@ -19,11 +19,16 @@ struct LandmarkList: View {
     
     var body: some View {
         NavigationSplitView {
-            List(filteredLandmarks) { landmark in
-                NavigationLink {
-                    LandmarkDetail(landmark: landmark)
-                } label: {
-                    LandmarkRow(landmark: landmark)
+            List {
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Show Favorites Only")
+                }
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
                 }
             }
             .navigationTitle("Landmarks")
