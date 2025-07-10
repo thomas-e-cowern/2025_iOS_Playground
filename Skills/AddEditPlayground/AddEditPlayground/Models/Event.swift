6facae309725e4563283283508fc4073ac7c09f7
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-10 08:05:44 -0400

    update Event and Person models with relationships

--- Skills/AddEditPlayground/AddEditPlayground/Models/Event.swift
+++ Skills/AddEditPlayground/AddEditPlayground/Models/Event.swift
@@ -11,16 +11,11 @@ import SwiftData
 class Event {
     var name: String
     var location: String
-    var details: String
 
-    init(name: String, location: String, details: String, metAt: Event? = nil) {
+    init(name: String, location: String) {
         self.name = name
         self.location = location
-        self.details = details
-        self.metAt = metAt
     }
     
     var people = [Person]()
-    
-    var metAt: Event?
 }
