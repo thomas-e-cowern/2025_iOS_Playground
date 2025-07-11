4ee8afb9d0804e6f2e368256fd3f739fb356f671
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-11 13:07:13 -0400

    add CustomTabBarView to project

---
+++ Skills/DisapearingTabBar/DisapearingTabBar/CustomTabBarView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarView.swift
+//  DisapearingTabBar
+//
+//  Created by Thomas Cowern on 7/11/25.
+//
+
+import SwiftUI
+
+struct CustomTabBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBarView()
+}
