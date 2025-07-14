756616f38026e7827183cfa780ef967fa62b3aab
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-14 09:10:19 -0400

    add Views folder and associated views to AppleNavigationCookbook project

---
+++ Apple/AppleNavigationCookbook/AppleNavigationCookbook/AppleNavigationCookbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNavigationCookbookApp.swift
+//  AppleNavigationCookbook
+//
+//  Created by Thomas Cowern on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct AppleNavigationCookbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
