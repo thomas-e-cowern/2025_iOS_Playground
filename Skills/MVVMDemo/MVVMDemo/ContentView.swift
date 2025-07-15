eb65c6f0c2c4b3f65880b78df674c76f5199c7ca
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-15 15:03:55 -0400

    update ContentView to use VM.  Tutorial complete

--- Skills/MVVMDemo/MVVMDemo/ContentView.swift
+++ Skills/MVVMDemo/MVVMDemo/ContentView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var personVM = PersonViewModel()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                ForEach(personVM.people) { person in
+                    HStack {
+                        Text(person.name)
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        Spacer()
+                        VStack {
+                            Text(person.email)
+                            Text(person.phoneNumber)
+                        }
+                        .font(.footnote)
+                    }
+                }
+            }
+            
+            Menu("Options") {
+                Button {
+                    personVM.reverseOrder()
+                } label: {
+                    Text("Reverse")
+                }
+                
+                Button {
+                    personVM.shuffleOrder()
+                } label: {
+                    Text("Shuffle")
+                }
+                
+                Button {
+                    personVM.removeLasePerson()
+                } label: {
+                    Text("Remove Last")
+                }
+                
+                Button {
+                    personVM.removeFirstPerson()
+                } label: {
+                    Text("Remove First")
+                }
+                
+                Button {
+                    personVM = .init()
+                } label: {
+                    Text("Back to start")
+                }
+            }
         }
-        .padding()
     }
 }
 
