f1b00c4f567a7c8cd8885215d79cc4c48ad8c065
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-15 14:34:50 -0400

    add MVVMDemo project to Skills folder

---
+++ Skills/MVVMDemo/MVVMDemo/MVVMDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMDemoApp.swift
+//  MVVMDemo
+//
+//  Created by Thomas Cowern on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct MVVMDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
