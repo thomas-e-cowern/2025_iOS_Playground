588b08a20a4607c976ac86439f53ef0f4a07c491
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-17 14:41:50 -0400

    add ResuableToobarButton project to Skills folder

---
+++ Skills/ResuableToobarButton/ResuableToobarButton/ResuableToobarButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResuableToobarButtonApp.swift
+//  ResuableToobarButton
+//
+//  Created by Thomas Cowern on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ResuableToobarButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
