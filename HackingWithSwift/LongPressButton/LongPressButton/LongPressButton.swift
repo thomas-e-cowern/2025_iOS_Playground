7959d819b0aa821e7108ae38014c5b3b22766d3d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-21 12:18:21 -0400

    add LongPressButton to project

---
+++ HackingWithSwift/LongPressButton/LongPressButton/LongPressButton.swift
@@ -0,0 +1,35 @@
+//
+//  LongPressButton.swift
+//  LongPressButton
+//
+//  Created by Thomas Cowern on 7/21/25.
+//
+
+import SwiftUI
+
+struct LongPressButton: View {
+    
+    var backgroundColor: Color = .clear
+    var foregroundColor: Color = .primary
+    var strokeColor: Color = .red
+    var strokeWidth: CGFloat = 6
+    
+    var body: some View {
+        Image(systemName: "star")
+            .font(.title)
+            .foregroundColor(foregroundColor)
+            .padding()
+            .background(
+                Circle()
+                    .fill(backgroundColor)
+            )
+            .overlay(
+                Circle()
+                    .stroke(strokeColor, style: StrokeStyle(lineWidth: strokeWidth, lineCap: .round))
+            )
+    }
+}
+
+#Preview {
+    LongPressButton()
+}
