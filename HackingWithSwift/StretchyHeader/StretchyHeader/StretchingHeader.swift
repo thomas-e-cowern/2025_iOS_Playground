19aeb513dd1839079363f368e509da9e7b714cfc
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-22 12:43:09 -0400

    add StretchyHeader project to HackingWithSwift folder

---
+++ HackingWithSwift/StretchyHeader/StretchyHeader/StretchingHeader.swift
@@ -0,0 +1,35 @@
+//
+//  StretchingHeader.swift
+//  StretchyHeader
+//
+//  Created by Thomas Cowern on 7/22/25.
+//
+
+import SwiftUI
+
+struct StretchingHeader<Content: View>: View {
+    
+    let content: () -> Content
+    
+    func height(for proxy: GeometryProxy) -> CGFloat {
+        let y = proxy.frame(in: .global).minY
+        return proxy.size.height + max(0, y)
+    }
+    
+    func offset(for proxy: GeometryProxy) -> CGFloat {
+        let y = proxy.frame(in: .global).minY
+        return min(0, -y)
+    }
+    
+    var body: some View {
+        GeometryReader { geo in
+            content()
+                .frame(width: geo.size.width, height: height(for: geo))
+                .offset(y: offset(for: geo))
+        }
+    }
+}
+
+#Preview {
+    StretchingHeader<<#Content: View#>>()
+}
---
+++ HackingWithSwift/StretchyHeader/StretchyHeader/StretchyHeaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StretchyHeaderApp.swift
+//  StretchyHeader
+//
+//  Created by Thomas Cowern on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct StretchyHeaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
