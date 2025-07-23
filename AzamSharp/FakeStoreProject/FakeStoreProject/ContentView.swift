6fa52faa4cff41c91c91f457b1a6d3e14373d127
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-23 10:23:22 -0400

    add FakeStoreProject to AzamSharp folder

---
+++ AzamSharp/FakeStoreProject/FakeStoreProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FakeStoreProject
+//
+//  Created by Thomas Cowern on 7/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ AzamSharp/FakeStoreProject/FakeStoreProject/FakeStoreProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FakeStoreProjectApp.swift
+//  FakeStoreProject
+//
+//  Created by Thomas Cowern on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct FakeStoreProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
