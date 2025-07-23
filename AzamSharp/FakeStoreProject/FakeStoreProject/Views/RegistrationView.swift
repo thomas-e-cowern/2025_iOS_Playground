c00d0e048f99969c9e8b4586d6f04815d952b853
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-23 10:55:20 -0400

    update RegistrationView with validate function

--- AzamSharp/FakeStoreProject/FakeStoreProject/Views/RegistrationView.swift
+++ AzamSharp/FakeStoreProject/FakeStoreProject/Views/RegistrationView.swift
@@ -13,6 +13,28 @@ struct RegistrationView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     
+    private func validate() -> [String] {
+        var errors: [String] = []
+        
+        if name.isEmptyOrWhitespace {
+            errors.append("Name cannot be emtpy.")
+        }
+        
+        if email.isEmptyOrWhitespace {
+            errors.append("Email cannot be emtpy.")
+        }
+        
+        if password.isEmpty{
+            errors.append("Password must be at least 8 characters long.")
+        }
+        
+        if !email.isValidEmail {
+            errors.append("Please enter a valid email address.")
+        }
+        
+        return errors
+    }
+    
     var body: some View {
         Form {
             TextField("Name", text: $name)
