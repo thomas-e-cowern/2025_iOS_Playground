8220e76ea27bb592968f26e8f28b4d84ef6c9e57
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-23 10:49:07 -0400

    update String+Extensions to include email and password validation

--- AzamSharp/FakeStoreProject/FakeStoreProject/Views/String+Extensions.swift
+++ AzamSharp/FakeStoreProject/FakeStoreProject/Views/String+Extensions.swift
@@ -11,4 +11,13 @@ extension String {
     var isEmptyOrWhitespace: Bool {
         trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
+    
+    var isValidPassword: Bool {
+        self.count > 8
+    }
+    
+    var isValidEmail: Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        return range(of: emailRegex, options: .regularExpression, range: nil, locale: nil) != nil
+    }
 }
