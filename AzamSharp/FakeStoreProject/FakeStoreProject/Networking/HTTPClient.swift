4fdf8e9815707f89d9fb4d771c73e9fe55580fe3
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-23 13:48:41 -0400

    add Services folder, add AuthenticationService to Services folder

---
+++ AzamSharp/FakeStoreProject/FakeStoreProject/Networking/HTTPClient.swift
@@ -0,0 +1,8 @@
+//
+//  HTTPClient.swift
+//  FakeStoreProject
+//
+//  Created by Thomas Cowern on 7/23/25.
+//
+
+import Foundation
---
+++ AzamSharp/FakeStoreProject/FakeStoreProject/Services/AuthenticationService.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationService.swift
+//  FakeStoreProject
+//
+//  Created by Thomas Cowern on 7/23/25.
+//
+
+import Foundation
+
+struct AuthenticationService {
+    
+    let httpClient = HTTPClient()
+    
+    func register(name: String, email: String, password: String) {
+        
+    }
+    
+}
