ff6fc83684d4e2323b74f99698f81b3112dc0521
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-25 12:10:47 -0400

    add NavLivestream to AzamShart folder

---
+++ AzamSharp/NavLivestream/NavLivestream/NavLivestreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavLivestreamApp.swift
+//  NavLivestream
+//
+//  Created by Thomas Cowern on 7/25/25.
+//
+
+import SwiftUI
+
+@main
+struct NavLivestreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
