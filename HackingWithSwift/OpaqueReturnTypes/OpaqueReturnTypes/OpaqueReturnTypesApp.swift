bcdfb681cc3f86e0aa949e88cb924db429781036
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-28 15:53:28 -0400

    add OpaqueReturnTypes project to  HackingWithSwift folder

---
+++ HackingWithSwift/OpaqueReturnTypes/OpaqueReturnTypes/OpaqueReturnTypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpaqueReturnTypesApp.swift
+//  OpaqueReturnTypes
+//
+//  Created by Thomas Cowern on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct OpaqueReturnTypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackingWithSwift/Overloading/Overloading/OverloadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  OverloadingApp.swift
+//  Overloading
+//
+//  Created by Thomas Cowern on 7/28/25.
+//
+
+import SwiftUI
+
+@main
+struct OverloadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
