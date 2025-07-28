040b8f0f2ad8b395621d561595514d2f218b4a86
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-28 18:55:21 -0400

    update ContentView to add formatting and Text

--- HackingWithSwift/OpaqueReturnTypes/OpaqueReturnTypes/ContentView.swift
+++ HackingWithSwift/OpaqueReturnTypes/OpaqueReturnTypes/ContentView.swift
@@ -9,13 +9,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("Hello, World!")
+            .padding()
+            .background(Color.red)
+            .foregroundColor(Color.white)
+            .onTapGesture {
+                print(type(of: self.body))
+            }
     }
 }
 
