22aebed7e67ae9104258913c6a409b67fb8a078d
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-30 09:48:18 -0400

    add Colors and formatting to ContentView

--- Skills/TipKitDemo/TipKitDemo/ContentView.swift
+++ Skills/TipKitDemo/TipKitDemo/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var colors = MockData.colors
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ScrollView {
+                ForEach(colors, id: \.self) { color in
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(color.gradient)
+                        .frame(height: 100)
+                        .contextMenu {
+                            Button("Favorite") {
+                                // More to come...
+                            }
+                        }
+                }
+            }
+            .padding()
+            .navigationTitle("TipKit Demo")
+            .toolbar {
+                Button {
+                    colors.insert(.random, at: 0)
+                } label: {
+                    Image(systemName: "plus")
+                }
+
+            }
         }
-        .padding()
     }
 }
 
@@ -27,4 +47,13 @@ extension Color {
     static var random: Color {
         return Color(.sRGB, red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))
     }
+}
+
+struct MockData {
+    static let colors = [
+        Color.random,
+        Color.random,
+        Color.random,
+        Color.random
+    ]
 }
