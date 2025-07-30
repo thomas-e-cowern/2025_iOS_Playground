7466f72d42d9f581169a3172f11eda03331d298c
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-07-30 09:36:21 -0400

    add TipKitDemo to Skills folder

---
+++ Skills/TipKitDemo/TipKitDemo/TipKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitDemoApp.swift
+//  TipKitDemo
+//
+//  Created by Thomas Cowern on 7/30/25.
+//
+
+import SwiftUI
+
+@main
+struct TipKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
