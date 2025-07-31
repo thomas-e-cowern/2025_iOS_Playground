36bc1604ce73295af695d677b70143d27058e73a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-31 13:29:02 -0400

    add PopoverTip to project

---
+++ Skills/TipKitDemoMap/TipKitDemoMap/PopoverTip.swift
@@ -0,0 +1,19 @@
+//
+//  PopoverTip.swift
+//  TipKitDemoMap
+//
+//  Created by Thomas Cowern on 7/31/25.
+//
+
+import Foundation
+import TipKit
+
+struct PopoverTip: View {
+    var body: some View {
+        Text("PopoverTip")
+    }
+}
+
+#Preview {
+    PopoverTip()
+}
