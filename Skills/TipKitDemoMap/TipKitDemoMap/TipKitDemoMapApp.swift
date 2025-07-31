5534723c0e0e1ea1c55851bcfb38e91d0a0d71c3
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-31 12:36:16 -0400

    add TipKitDemoMap project to Skills folder

---
+++ Skills/TipKitDemoMap/TipKitDemoMap/TipKitDemoMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipKitDemoMapApp.swift
+//  TipKitDemoMap
+//
+//  Created by Thomas Cowern on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct TipKitDemoMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
