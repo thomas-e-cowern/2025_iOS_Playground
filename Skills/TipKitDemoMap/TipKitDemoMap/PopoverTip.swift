3875a2b3107d30e019609ec776b71ef5815c1d4d
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-31 13:41:30 -0400

    add PoperTipView to PopoverTip

--- Skills/TipKitDemoMap/TipKitDemoMap/PopoverTip.swift
+++ Skills/TipKitDemoMap/TipKitDemoMap/PopoverTip.swift
@@ -9,11 +9,38 @@ import Foundation
 import TipKit
 
 struct PopoverTip: View {
+    
+    private let popoverTip = PopoverTipView()
+    
     var body: some View {
         Text("PopoverTip")
+            .popoverTip(popoverTip)
     }
 }
 
+
 #Preview {
     PopoverTip()
+        .task {
+            try? Tips.resetDatastore()
+            
+            try? Tips.configure([
+                .displayFrequency(.immediate),
+                .datastoreLocation(.applicationDefault)
+            ])
+        }
+}
+
+struct PopoverTipView: Tip {
+    var title: Text {
+        Text("Halloween Events")
+    }
+    
+    var message: Text? {
+        Text("All Halloween events nearby")
+    }
+    
+    var image: Image? {
+        Image("Info.tip.image")
+    }
 }
