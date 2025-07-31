c968b037630d4d2a3455366c24b66916a7509195
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-07-31 13:20:59 -0400

    add map functions to ContentView

--- Skills/TipKitDemoMap/TipKitDemoMap/ContentView.swift
+++ Skills/TipKitDemoMap/TipKitDemoMap/ContentView.swift
@@ -22,40 +22,81 @@ struct ContentView: View {
                 ZStack {
                     Map(coordinateRegion: $region)
                         .ignoresSafeArea()
-                    
-                        .padding()
-                        .offset(x: 0, y: 130)
                 }
                 .overlay (
-                  getCarouselImage()
-                , alignment: .bottom)
+                    getCarouselImage()
+                    , alignment: .bottom)
                 
                 .overlay(
                     getInfoButton()
                     ,alignment: .topTrailing )
                 
                 .gesture(
-                DragGesture()
-                    .onEnded({ value in
-                        let threshold:CGFloat = 50
-                        if value.translation.width > threshold {
-                            withAnimation{
-                                currentIndex = max(0, currentIndex - 1)
+                    DragGesture()
+                        .onEnded({ value in
+                            let threshold:CGFloat = 50
+                            if value.translation.width > threshold {
+                                withAnimation{
+                                    currentIndex = max(0, currentIndex - 1)
+                                }
                             }
-                        }
-                        else if value.translation.width < -threshold {
-                            withAnimation {
-                                currentIndex = min(images.count - 1,
-                                currentIndex + 1)
+                            else if value.translation.width < -threshold {
+                                withAnimation {
+                                    currentIndex = min(images.count - 1,
+                                                       currentIndex + 1)
+                                }
                             }
-                        }
-                    })
+                        })
                 )
             }
             .onAppear{
+                
+            }
+        }
+    }
+        
+        func getCarouselImage() -> some View {
+            ForEach(0..<images.count, id: \.self) { index in
+                Image(images[index])
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .cornerRadius(25)
+                    .opacity(currentIndex == index ? 1.0 : 0.5)
+                    .scaleEffect(currentIndex == index ? 1.2 : 0.8)
+                    .offset(x: CGFloat(index - currentIndex)*150 + dragOffset, y:0)
+                    .onTapGesture {
+                        
+                    }
+            }
+        }
+        
+        func getTitleSubtitle() -> some View {
+            VStack(alignment: .center, spacing: 10) {
+                Text("Trick-or-treat 🎃")
+                    .font(.title3)
+                    .foregroundStyle(.primary)
+                
+                Text("We wish you not to face the biggest designer's nightmares in reality and that clients treat you well!")
+                    .font(.body)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.center)
+            }
+        }
+        
+        func getInfoButton() -> some View {
+            Button {
+                print("Button was tapped")
+                
+            } label: {
+                Image(systemName: "info.circle.fill")
+                    .resizable()
+                    .frame(width: 25, height: 25)
 
             }
-    }
+            .padding()
+            
+        }
 }
 
 #Preview {
