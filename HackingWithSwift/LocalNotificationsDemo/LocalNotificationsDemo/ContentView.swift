8b6026fba660780005a918d9fa1580d34174acbe
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-05 12:58:57 -0400

    add UserNotificationPermissions to firts button in ContentView

--- HackingWithSwift/LocalNotificationsDemo/LocalNotificationsDemo/ContentView.swift
+++ HackingWithSwift/LocalNotificationsDemo/LocalNotificationsDemo/ContentView.swift
@@ -12,7 +12,13 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Button("Request Permission") {
-                
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { success, error in
+                    if success {
+                        print("All Set!")
+                    } else if let error {
+                        print(error.localizedDescription)
+                    }
+                }
             }
             
             Button("Schedule Notification") {
