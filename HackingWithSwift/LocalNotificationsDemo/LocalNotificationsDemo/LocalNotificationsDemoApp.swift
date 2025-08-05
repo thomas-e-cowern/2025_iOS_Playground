d83c6f4fd7faaf01aaf4a505b6408d34fea9899c
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-05 12:53:50 -0400

    add LocalNotificationsDemo to HackingWithSwift folder

---
+++ HackingWithSwift/LocalNotificationsDemo/LocalNotificationsDemo/LocalNotificationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationsDemoApp.swift
+//  LocalNotificationsDemo
+//
+//  Created by Thomas Cowern on 8/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
