9e27069768ff5d6e7f4a75144c3db2154a3ef1c0
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-08-12 12:41:43 -0400

    add Solt folder to 2025_iOS_Playground.  Add SetpCounter project to Solt folder.

---
+++ Solt/StepCounter/StepCounter/StepCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepCounterApp.swift
+//  StepCounter
+//
+//  Created by Thomas Cowern on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StepCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
