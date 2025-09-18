c9a16cf732d9451d47956b1256936964493e6fe3
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-18 06:13:40 -0400

    add ErrorAlert to project

---
+++ Skills/CGTPErrorHandling/CGTPErrorHandling/CGTPErrorHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CGTPErrorHandlingApp.swift
+//  CGTPErrorHandling
+//
+//  Created by Thomas Cowern on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct CGTPErrorHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Skills/CGTPErrorHandling/CGTPErrorHandling/ErrorHandling/ErrorAlert.swift
@@ -0,0 +1,27 @@
+//
+//  ErrorHandling.swift
+//  CGTPErrorHandling
+//
+//  Created by Thomas Cowern on 9/18/25.
+//
+
+import Foundation
+import SwiftUI
+
+struct ErrorAlert: Identifiable {
+    let id = UUID()
+    let message: String
+    var dismissAction: (() -> Void)?
+}
+
+@Observable
+class ErrorHandling {
+    
+    private var currentAlert: ErrorAlert?
+    
+    func handle(error: Error) {
+        currentAlert = ErrorAlert(message: error.localizedDescription)
+        // You can add more sophisticated error handling logic here,
+        // like logging the error or presenting different types of alerts based on error type.
+    }
+}
