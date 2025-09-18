7b1732f956a48853a8becacb35a07a122d765570
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-18 06:08:15 -0400

    update ContentView

--- Apple/AppleErrorHandling/AppleErrorHandling/ContentView.swift
+++ Apple/AppleErrorHandling/AppleErrorHandling/ContentView.swift
@@ -10,15 +10,18 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var errorWrapper: ErrorWrapper?
+    @State private var showError: Bool = false
     
     @Environment(\.dismiss) private var dismiss
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                showError.toggle()
+            } label: {
+                Text("Error!")
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
         .sheet(item: $errorWrapper) {
