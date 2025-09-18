b878e52b8a8ad4ed3b259080f6f01d308e5248f7
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-18 05:51:43 -0400

    AppleErrorHandling added to Apple folder

---
+++ Apple/AppleErrorHandling/AppleErrorHandling/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AppleErrorHandling
+//
+//  Created by Thomas Cowern on 9/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
