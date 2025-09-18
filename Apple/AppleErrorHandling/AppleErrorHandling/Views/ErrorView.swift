0d9f284c76047ddda234a2a8395fb2dc1d546221
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-18 05:59:48 -0400

    update ErrorView, add details and formatting, update Preview

--- Apple/AppleErrorHandling/AppleErrorHandling/Views/ErrorView.swift
+++ Apple/AppleErrorHandling/AppleErrorHandling/Views/ErrorView.swift
@@ -12,7 +12,20 @@ struct ErrorView: View {
     let errorWrapper: ErrorWrapper
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
     }
 }
 
