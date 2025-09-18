85b02bf79e60ca68b71b98b54f93c04381195210
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-18 06:03:57 -0400

    update ErrorView, add Tooldbar and dismiss action

--- Apple/AppleErrorHandling/AppleErrorHandling/Views/ErrorView.swift
+++ Apple/AppleErrorHandling/AppleErrorHandling/Views/ErrorView.swift
@@ -11,21 +11,32 @@ struct ErrorView: View {
     
     let errorWrapper: ErrorWrapper
     
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
-        VStack {
-            Text("An error has occurred!")
-                .font(.title)
-                .padding(.bottom)
-            Text(errorWrapper.error.localizedDescription)
-                .font(.headline)
-            Text(errorWrapper.guidance)
-                .font(.caption)
-                .padding(.top)
-            Spacer()
+        NavigationStack {
+            VStack {
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
         }
-        .padding()
-        .background(.ultraThinMaterial)
-        .cornerRadius(16)
     }
 }
 
