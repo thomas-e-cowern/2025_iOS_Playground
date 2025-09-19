5043881665f59f958d74fab727b327436828ef81
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-19 08:15:53 -0400

    wired up Errors in App file

--- Rebeloper/SwiftUIErrorHandling/SwiftUIErrorHandling/MyError.swift
+++ Rebeloper/SwiftUIErrorHandling/SwiftUIErrorHandling/MyError.swift
@@ -9,11 +9,23 @@ import Foundation
 
 enum MyError: LocalizedError {
     case custom
+    case networkError
+    
+    var title: String? {
+        switch self {
+        case .custom:
+            return "Custom Title"
+        case .networkError:
+            return "Network Error"
+        }
+    }
     
     var errorDescription: String? {
         switch self {
         case .custom:
             return "Custom Error"
+        case .networkError:
+            return "Network Error"
         }
     }
 }
--- Rebeloper/SwiftUIErrorHandling/SwiftUIErrorHandling/SwiftUIErrorHandlingApp.swift
+++ Rebeloper/SwiftUIErrorHandling/SwiftUIErrorHandling/SwiftUIErrorHandlingApp.swift
@@ -9,9 +9,15 @@ import SwiftUI
 
 @main
 struct SwiftUIErrorHandlingApp: App {
+    
+    @State private var errorAlerts = ErrorAlerts()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modifier(ErrorAlertsViewModifier())
+                .environment(\.errorAlerts, errorAlerts)
         }
+        
     }
 }
