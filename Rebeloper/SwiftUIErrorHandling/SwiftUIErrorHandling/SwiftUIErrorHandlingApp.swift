072366492a09068b05de69209e9d25f7ebf75cf7
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-19 07:01:14 -0400

    SwiftUIErrorHandling initial commit in Rebeloper folder

---
+++ Rebeloper/SwiftUIErrorHandling/SwiftUIErrorHandling/SwiftUIErrorHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIErrorHandlingApp.swift
+//  SwiftUIErrorHandling
+//
+//  Created by Thomas Cowern on 9/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIErrorHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
