2bb4f864b732d945d60b645be2976eb6b758ef5e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-09-19 08:15:06 -0400

    add Title variable to ErrorAlerts

--- Rebeloper/SwiftUIErrorHandling/SwiftUIErrorHandling/ErrorAlerts.swift
+++ Rebeloper/SwiftUIErrorHandling/SwiftUIErrorHandling/ErrorAlerts.swift
@@ -17,10 +17,59 @@ final class ErrorAlerts {
     var action: (() -> Void)?
     
     func present(_ error: Error, title: String = "Error", buttonTitle: String = "OK", action: (() -> Void)? = nil) {
+        print(title)
         print(error.localizedDescription)
         self.error = error
         self.title = title
         self.buttonTitle = buttonTitle
         self.action = action
     }
+}
+
+struct ErrorAlertKey: EnvironmentKey {
+    @MainActor
+    static let defaultValue = ErrorAlerts()
+}
+
+extension EnvironmentValues {
+    var errorAlerts: ErrorAlerts {
+        get {
+            self[ErrorAlertKey.self]
+        } set {
+            self[ErrorAlertKey.self] = newValue
+        }
+    }
+}
+
+struct ErrorAlertsViewModifier: ViewModifier {
+    @Environment(\.errorAlerts) private var errorAlerts
+    @State private var isPresented: Bool = false
+    
+    func body(content: Content) -> some View {
+        content
+            .alert(errorAlerts.title ?? "Error", isPresented: $isPresented) {
+                Button {
+                    errorAlerts.action?()
+                    errorAlerts.error = nil
+                    errorAlerts.title = nil
+                    errorAlerts.buttonTitle = nil
+                } label: {
+                    Text(errorAlerts.buttonTitle ?? "")
+                }
+
+            } message: {
+                Text(errorAlerts.error?.localizedDescription ?? "")
+            }
+            .onChange(of: errorAlerts.title) { _, newValue in
+                if newValue != nil {
+                    isPresented = true
+                }
+            }
+
+    }
+}
+
+#Preview {
+    Text("Hello, world!")
+        .modifier(ErrorAlertsViewModifier())
 }
