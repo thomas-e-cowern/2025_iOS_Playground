dd1afac0a3c6fbd323f3bc70aa69a3a3828e7740
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-03 10:42:25 -0400

    MockAPITest initial commit

---
+++ MockAPITest/MockAPITest/MockAPITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockAPITestApp.swift
+//  MockAPITest
+//
+//  Created by Thomas Cowern on 10/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MockAPITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
