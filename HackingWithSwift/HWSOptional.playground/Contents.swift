58cf7a166f4c82fcb71ba28aea97dc9a21c98d13
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-08 10:38:06 -0400

    add User structs demonstrating difference between Option and usse of ? shortcut

--- HackingWithSwift/HWSOptional.playground/Contents.swift
+++ HackingWithSwift/HWSOptional.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+struct User {
+    var name: String
+    var password: String?
+}
+
+// user with password
+let john = User(name: "John", password: "qs35fbn89")
+
+// user without password
+let amy = User(name: "Amy")
+
+// safer way to do it
+struct UserNP {
+    var name: String
+    var password: Optional<String>
+}
+
+// requires use of nil for passwrod
+let bob = UserNP(name: "Bob", password: nil)
