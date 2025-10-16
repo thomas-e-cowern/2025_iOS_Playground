93231378ff65fe7ca0365aa263c45e1adde72a7f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-16 10:04:01 -0400

    add Sight model to Models folder

---
+++ HackingWithSwift/iTour/iTour/Models/Sight.swift
@@ -0,0 +1,18 @@
+//
+//  Sight.swift
+//  iTour
+//
+//  Created by Thomas Cowern on 10/16/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Sight {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+}
