2e73c3c925a398b130adea7c675e1f0e4801364a
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-16 09:19:57 -0400

    add DestinationListingView to iTour Views folder

---
+++ HackingWithSwift/iTour/iTour/Views/DestinationListingView.swift
@@ -0,0 +1,43 @@
+//
+//  DestinationListingView.swift
+//  iTour
+//
+//  Created by Thomas Cowern on 10/16/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct DestinationListingView: View {
+    
+    @Environment(\.modelContext) var modelContext
+    
+    @Query var destinations: [Destination]
+    
+    var body: some View {
+        List {
+            ForEach(destinations) { destination in
+                NavigationLink(value: destination) {
+                    VStack(alignment: .leading) {
+                        Text(destination.name)
+                            .font(.headline)
+                        
+                        Text(destination.date.formatted(date: .long, time: .shortened))
+                    }
+                }
+            }
+            .onDelete(perform: deleteDestinations)
+        }
+    }
+    
+    func deleteDestinations(_ indexSet: IndexSet) {
+        for index in indexSet {
+            let destination = destinations[index]
+            modelContext.delete(destination)
+        }
+    }
+}
+
+#Preview {
+    DestinationListingView()
+}
