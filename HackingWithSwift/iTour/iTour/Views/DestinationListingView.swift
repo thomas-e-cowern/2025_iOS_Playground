6feedfc456fac90bd9089674bdcfac79ccd0a48e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-16 09:49:02 -0400

    update DestinationListingView, update init for searchText

--- HackingWithSwift/iTour/iTour/Views/DestinationListingView.swift
+++ HackingWithSwift/iTour/iTour/Views/DestinationListingView.swift
@@ -14,8 +14,18 @@ struct DestinationListingView: View {
     
     @Query var destinations: [Destination]
     
-    init(sort: SortDescriptor<Destination>) {
-        _destinations = Query(sort: [sort])
+//    init(sort: SortDescriptor<Destination>) {
+//        _destinations = Query(sort: [sort])
+//    }
+    
+    init(sort: SortDescriptor<Destination>, searchString: String) {
+        _destinations = Query(filter: #Predicate {
+            if searchString.isEmpty {
+                return true
+            } else {
+                return $0.name.localizedStandardContains(searchString)
+            }
+        }, sort: [sort])
     }
     
     var body: some View {
@@ -43,5 +53,5 @@ struct DestinationListingView: View {
 }
 
 #Preview {
-    DestinationListingView(sort: SortDescriptor(\Destination.name))
+    DestinationListingView(sort: SortDescriptor(\Destination.name), searchString: "")
 }
