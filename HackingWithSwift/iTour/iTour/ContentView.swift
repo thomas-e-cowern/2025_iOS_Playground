e11964d48636ff9173441021a976ce801daa0c6e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-16 09:48:17 -0400

    update ContentView, add search variable and searchable function

--- HackingWithSwift/iTour/iTour/ContentView.swift
+++ HackingWithSwift/iTour/iTour/ContentView.swift
@@ -14,12 +14,14 @@ struct ContentView: View {
     
     @State private var path = [Destination]()
     @State private var sortOrder = SortDescriptor(\Destination.name)
+    @State private var searchText = ""
     
     var body: some View {
         VStack {
             NavigationStack(path: $path) {
-                DestinationListingView(sort: sortOrder)
+                DestinationListingView(sort: sortOrder, searchString: searchText)
                 .navigationTitle("iTour")
+                .searchable(text: $searchText)
                 .toolbar {
 //                    Button("Add Samples", action: addSamples)
                     Button("Add Destination", systemImage: "plus", action: addDestination)
