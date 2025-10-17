bcdd046ce2b9da2b9e6b2412396be44b25e7e23f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-17 08:24:15 -0400

    update ContentView with news and message variables, add List to display data

--- HackingWithSwift/NetworkingSandbox/NetworkingSandbox/ContentView.swift
+++ HackingWithSwift/NetworkingSandbox/NetworkingSandbox/ContentView.swift
@@ -8,14 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var headlines = [News]()
+    @State private var messages = [Message]()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                Section("Headlines") {
+                    ForEach(headlines) { headline in
+                        VStack(alignment: .leading) {
+                            Text(headline.title)
+                                .font(.headline)
+
+                            Text(headline.strap)
+                        }
+                    }
+                }
+
+                Section("Messages") {
+                    ForEach(messages) { message in
+                        VStack(alignment: .leading) {
+                            Text(message.from)
+                                .font(.headline)
+
+                            Text(message.text)
+                        }
+                    }
+                }
+            }
+            .task {
+                do {
+                    let headlinesURL = URL(string: "https://hws.dev/headlines.json")!
+                    let messagesURL = URL(string: "https://hws.dev/messages.json")!
+
+                    let (headlineData, _) = try await URLSession.shared.data(from: headlinesURL)
+                    let (messageData, _) = try await URLSession.shared.data(from: messagesURL)
+
+                    headlines = try JSONDecoder().decode([News].self, from: headlineData)
+                    messages = try JSONDecoder().decode([Message].self, from: messageData)
+                } catch {
+                    print("Error handling is a smart move!")
+                }
+            }
         }
-        .padding()
     }
 }
 
