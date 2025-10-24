c28e5c0e4f87592f92b037a78b0389c0c37924e8
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-24 08:13:35 -0400

    add Feature model to project

---
+++ HackingWithSwift/WelcomeScreen/WelcomeScreen/Feature.swift
@@ -0,0 +1,15 @@
+//
+//  Feature.swift
+//  WelcomeScreen
+//
+//  Created by Thomas Cowern on 10/24/25.
+//
+
+import Foundation
+
+struct Feature: Decodable, Identifiable {
+    var id = UUID()
+    let title: String
+    let description: String
+    let image: String
+}
