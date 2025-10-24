425d098a06214e420a073e0e22b83e9abd079475
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-24 10:32:16 -0400

    update ContentView, add List, ForEach and functions to fetch data and clost WelcomeScreen

--- HackingWithSwift/WelcomeScreen/WelcomeScreen/ContentView.swift
+++ HackingWithSwift/WelcomeScreen/WelcomeScreen/ContentView.swift
@@ -8,15 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var features: [Feature] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Welcome to AwesomeApp")
+                .multilineTextAlignment(.center)
+                .font(.largeTitle.bold())
+
+            ForEach(features) { feature in
+                HStack {
+                    Image(systemName: feature.image)
+                        .frame(width: 44)
+                        .font(.title)
+                        .foregroundColor(.blue)
+                        .accessibilityHidden(true)
+
+                    VStack(alignment: .leading) {
+                        Text(feature.title)
+                            .font(.headline)
+
+                        Text(feature.description)
+                            .foregroundColor(.secondary)
+                    }
+                    .accessibilityElement(children: .combine)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }   //: End of ForEach
+            
+            Text("Any important small print here.")
+                .font(.footnote)
+                .foregroundColor(.secondary)
+
+            Button("Continue", action: close)
+                .frame(maxWidth: .infinity, minHeight: 44)
+                .background(Color.blue)
+                .foregroundColor(.white)
+                .cornerRadius(10)
+            
         }
         .padding()
+        .onAppear {
+            features = loadJSONData(filename: "features") ?? []
+        }
     }
+    
+    func loadJSONData(filename: String) -> [Feature]? {
+        guard let url = Bundle.main.url(forResource: filename, withExtension: "json") else {
+            print("JSON file not found: \(filename).json")
+            return nil
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            return try JSONDecoder().decode([Feature].self, from: data)
+        } catch {
+            print("Error decoding JSON: \(error)")
+            return nil
+        }
+    }
+    
+    func close() {
+        presentationMode.wrappedValue.dismiss()
+    }
+
 }
 
 #Preview {
