18be61450224e1b2d0f5218c74d4c4af33ac4593
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-29 13:55:08 -0400

    add DecoupledNavigation to AzamSharp folder

---
+++ AzamSharp/DecoupledNavigation/DecoupledNavigation/DecoupledNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecoupledNavigationApp.swift
+//  DecoupledNavigation
+//
+//  Created by Thomas Cowern on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct DecoupledNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
