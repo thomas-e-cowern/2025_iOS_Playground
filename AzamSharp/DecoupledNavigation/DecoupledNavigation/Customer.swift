fe0c8268ab4152bb22b92c319ac6f30d5c1a92af
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-29 14:01:34 -0400

    add Customer model to project

---
+++ AzamSharp/DecoupledNavigation/DecoupledNavigation/Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Custoner.swift
+//  DecoupledNavigation
+//
+//  Created by Thomas Cowern on 10/29/25.
+//
+
+import Foundation
+
+struct Customer {
+    let id = UUID()
+    let name: String
+}
