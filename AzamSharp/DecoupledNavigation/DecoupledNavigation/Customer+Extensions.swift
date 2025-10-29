083a7ae6f5fd309e601ad03e56bbf3400853afa9
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-29 14:22:51 -0400

    update Customer+Extensions, add data for address field

--- AzamSharp/DecoupledNavigation/DecoupledNavigation/Customer+Extensions.swift
+++ AzamSharp/DecoupledNavigation/DecoupledNavigation/Customer+Extensions.swift
@@ -10,14 +10,14 @@ import Foundation
 #if DEBUG
 extension Customer {
     static var sampleCustomer: Customer {
-        Customer(name: "Alice Smith")
+        Customer(name: "Alice Smith", address: "1939 Pine Road Phoenix, AZ 12360")
     }
 
     static var sampleCustomers: [Customer] {
         [
-            Customer(name: "Bob Elks"),
-            Customer(name: "Lisa Belt"),
-            Customer(name: "Phil Collings")
+            Customer(name: "Bob Elks", address: "2345 Pine Road Phoenix, AZ 12360"),
+            Customer(name: "Lisa Belt", address: "8828 Cedar Lane Houston, AZ 38237"),
+            Customer(name: "Phil Collings", address: "3098 Cedar Lane New York, AZ 72202")
         ]
     }
 }
