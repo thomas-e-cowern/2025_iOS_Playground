28831d3f2b290e5a0465e99f024c67e744a50793
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-29 14:07:51 -0400

    add CustomerListView to project

--- AzamSharp/DecoupledNavigation/DecoupledNavigation/CustomerListView.swift
+++ AzamSharp/DecoupledNavigation/DecoupledNavigation/CustomerListView.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct CustomerListView: View {
+    
+    let customers: [Customer]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(customers) { customer in
+                Text(customer.name)
+            }
+        }
     }
 }
 
 #Preview {
-    CustomerListView()
+    CustomerListView(customers: Customer.sampleCustomers)
 }
