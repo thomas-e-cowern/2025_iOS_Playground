2122dfdc0c8fa7b477f2bb8af49e2bbeb7df022f
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-10-30 15:13:41 -0400

    update DataHeaderView, refactor main view into seperate view

--- ToDoList/ToDoList/DataHeaderView.swift
+++ ToDoList/ToDoList/DataHeaderView.swift
@@ -8,30 +8,49 @@
 import SwiftUI
 
 struct DataHeaderView: View {
+    fileprivate func nameHeaderTextView() -> HStack<TupleView<(VStack<TupleView<(some View, some View)>>, Spacer, VStack<TupleView<(Text, Button<some View>)>>)>> {
+        return HStack {
+            VStack(alignment: .listRowSeparatorLeading, spacing: 0) {
+                Text("Hi Tom")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                    .padding(4)
+                
+                Text("Whats up today?")
+                    .font(.caption)
+                    .fontWeight(.light)
+                    .foregroundStyle(.black)
+                    .padding(4)
+            }
+            
+            Spacer()
+            
+            VStack(alignment: .trailing, spacing: 0) {
+                Text("dataManager.selectedDate.monthToString()")
+                    .font(.system(size: 10))
+                    .fontWeight(.heavy)
+                    .foregroundStyle(.black)
+                
+                Button {
+                    // More to come...
+                } label: {
+                    Text("Today")
+                        .font(.system(size: 16))
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.white)
+                        .padding(4)
+                        .background(.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 4))
+                }
+            }
+        }
+    }
+    
     var body: some View {
         ZStack {
             VStack {
-                HStack {
-                    VStack(alignment: .listRowSeparatorLeading, spacing: 0) {
-                        Text("Hi Tom")
-                            .font(.title)
-                            .fontWeight(.semibold)
-                            .foregroundStyle(.black)
-                            .padding(4)
-                        
-                        Text("Whats up today?")
-                            .font(.caption)
-                            .fontWeight(.light)
-                            .foregroundStyle(.black)
-                            .padding(4)
-                    }
-                    
-                    Spacer()
-                    
-                    VStack(alignment: .trailing, spacing: 0) {
-                        // More to come...
-                    }
-                }
+                nameHeaderTextView()
             }
         }
     }
