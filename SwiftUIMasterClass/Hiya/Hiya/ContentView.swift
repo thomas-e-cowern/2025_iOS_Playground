451e8301441061784602ff3b48ae5fab9c390a9a
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-11-20 15:10:32 -0500

    update ContentView, add AppleIntelligence and determine if phone meets specs

--- SwiftUIMasterClass/Hiya/Hiya/ContentView.swift
+++ SwiftUIMasterClass/Hiya/Hiya/ContentView.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import FoundationModels
 
 struct ContentView: View {
+    
+    private var largeLanguageModel = SystemLanguageModel.default
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            switch largeLanguageModel.availability {
+            case .available:
+                Text("Available")
+            case .unavailable(.deviceNotEligible):
+                Text("Youre device is not AppleIntelligence capable")
+            case .unavailable(.appleIntelligenceNotEnabled):
+                Text("Please enable AppleIntelligence in settings")
+            case .unavailable(.modelNotReady):
+                Text("The AI model is not ready")
+            case .unavailable(_):
+                Text("The AI feature is not available for an unknown reason")
+            }
         }
         .padding()
     }
