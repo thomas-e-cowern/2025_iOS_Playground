6e622607d4c8c4bd430b73f2a592dc823acd688f
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-11-20 13:13:05 -0500

    Hiya app initial commit in SwiftUIMasterClass folder

---
+++ SwiftUIMasterClass/Hiya/Hiya/HiyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaApp.swift
+//  Hiya
+//
+//  Created by Thomas Cowern on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct HiyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIMasterClass/Hiya/HiyaTests/HiyaTests.swift
@@ -0,0 +1,17 @@
+//
+//  HiyaTests.swift
+//  HiyaTests
+//
+//  Created by Thomas Cowern on 11/20/25.
+//
+
+import Testing
+@testable import Hiya
+
+struct HiyaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
