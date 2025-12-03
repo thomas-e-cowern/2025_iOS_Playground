bcda012f8ab718b5e03597855907723c6ad79cf4
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-12-03 14:59:53 -0500

    add confirmationDialogModifier to Modifiers folder

---
+++ SwiftfulThinking/ActionSheetPlayground/ActionSheetPlayground/Modifiers/confirmationDialogModifier.swift
@@ -0,0 +1,27 @@
+//
+//  confirmationDialogModifier.swift
+//  ActionSheetPlayground
+//
+//  Created by Thomas Cowern on 12/3/25.
+//
+
+import SwiftUI
+
+struct confirmationDialogModifier<Actions: View>: ViewModifier {
+    @Binding var isPresented: Bool
+    let title: Text
+    let message: Text?
+    @ViewBuilder let actions: () -> Actions
+    
+    func body(content: Content) -> some View {
+        content.confirmationDialog(
+            title,
+            isPresented: $isPresented,
+            titleVisibility: .automatic
+        ) {
+            actions()
+        } message: {
+            message
+        }
+    }
+}
