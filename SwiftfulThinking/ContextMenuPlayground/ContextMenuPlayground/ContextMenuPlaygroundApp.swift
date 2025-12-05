08e3656af8a58affe5136c5c67e93e7b0fc06e1f
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-12-05 11:11:24 -0500

    update ContentView add sheet button

---
+++ SwiftfulThinking/ContextMenuPlayground/ContextMenuPlayground/ContextMenuPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuPlaygroundApp.swift
+//  ContextMenuPlayground
+//
+//  Created by Thomas Cowern on 12/5/25.
+//
+
+import SwiftUI
+
+@main
+struct ContextMenuPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfulThinking/ContextMenuPlayground/ContextMenuPlaygroundTests/ContextMenuPlaygroundTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContextMenuPlaygroundTests.swift
+//  ContextMenuPlaygroundTests
+//
+//  Created by Thomas Cowern on 12/5/25.
+//
+
+import Testing
+@testable import ContextMenuPlayground
+
+struct ContextMenuPlaygroundTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
