7d4d6d4e786e5bc5be2dc7728a225b540d5728bc
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-12-15 06:47:04 -0500

    update ContentView, add Button to view

--- Rebeloper/NavigationFixed/NavigationFixed/ContentView.swift
+++ Rebeloper/NavigationFixed/NavigationFixed/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button {
+                // More to come...
+            } label: {
+                Button("Tap me!") {
+                    
+                }
+            }
+
         }
         .padding()
     }
---
+++ Rebeloper/NavigationFixed/NavigationFixed/DestinationView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationView.swift
+//  NavigationFixed
+//
+//  Created by Thomas Cowern on 12/15/25.
+//
+
+import SwiftUI
+
+struct DestinationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationView()
+}
