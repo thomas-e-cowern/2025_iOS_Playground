a2bc9563c50655c29bd81b4a52b69f854f5dfde2
Thomas Cowern <thomas.e.cowern@gmail.com>
2025-12-15 06:43:13 -0500

    initial commit NavigationFixed in Rebeloper folder

---
+++ Rebeloper/NavigationFixed/NavigationFixed/NavigationFixedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationFixedApp.swift
+//  NavigationFixed
+//
+//  Created by Thomas Cowern on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationFixedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
