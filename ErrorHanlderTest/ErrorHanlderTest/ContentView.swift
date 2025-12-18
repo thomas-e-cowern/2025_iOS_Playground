1a0451324294b138c2ce4e7a8aeefa09203264af
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-18 14:38:25 -0500

    update ContentView and App file to inject ErrorHandler

--- ErrorHanlderTest/ErrorHanlderTest/ContentView.swift
+++ ErrorHanlderTest/ErrorHanlderTest/ContentView.swift
@@ -8,17 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                NavigationLink("Go to Details") {
+                    DetailsView()
+                }
+            }
+            .navigationTitle("Home")
         }
-        .padding()
+        .globalErrorAlert()
     }
 }
 
 #Preview {
     ContentView()
+        .environment(ErrorCenter())
 }
--- ErrorHanlderTest/ErrorHanlderTest/ErrorHanlderTestApp.swift
+++ ErrorHanlderTest/ErrorHanlderTest/ErrorHanlderTestApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct ErrorHanlderTestApp: App {
+    
+    @State private var errorCenter = ErrorCenter()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(errorCenter)     // make available everywhere
         }
     }
 }
