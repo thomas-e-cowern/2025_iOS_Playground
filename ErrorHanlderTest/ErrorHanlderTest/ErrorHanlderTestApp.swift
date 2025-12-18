af34cc47145e4b62b5232097afbbb5b649408f1e
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-18 14:14:54 -0500

    ErrirHandlerTest initial commit

---
+++ ErrorHanlderTest/ErrorHanlderTest/ErrorHanlderTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErrorHanlderTestApp.swift
+//  ErrorHanlderTest
+//
+//  Created by Thomas Cowern on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct ErrorHanlderTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
