a8a2e317f1b79e8a68087ccfb29f4e009f793cbc
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-19 13:56:03 -0500

    update ItemDetail with Image and Text

--- HackingWithSwift/iDine/iDine/ItemDetail.swift
+++ HackingWithSwift/iDine/iDine/ItemDetail.swift
@@ -8,11 +8,18 @@
 import SwiftUI
 
 struct ItemDetail: View {
+    
+    var item: MenuItem
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(item.mainImage)
+            Text(item.name)
+        }
+        .navigationTitle(item.name)
     }
 }
 
 #Preview {
-    ItemDetail()
+    ItemDetail(item: MenuItem.example)
 }
