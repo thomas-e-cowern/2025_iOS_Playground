faf3ba89a526b63558422083272622f0aa815a81
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-19 13:05:17 -0500

    update ItemRow, add formatting

--- HackingWithSwift/iDine/iDine/ItemRow.swift
+++ HackingWithSwift/iDine/iDine/ItemRow.swift
@@ -14,8 +14,11 @@ struct ItemRow: View {
     var body: some View {
         HStack(spacing: 10) {
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
             VStack(alignment: .leading) {
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
             }
         }
