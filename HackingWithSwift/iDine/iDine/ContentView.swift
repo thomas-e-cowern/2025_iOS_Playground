e09ded49cefcd1b444e82e0065e316e5e523d2c9
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-19 13:17:51 -0500

    update ContentView, add NavigationLink to List

--- HackingWithSwift/iDine/iDine/ContentView.swift
+++ HackingWithSwift/iDine/iDine/ContentView.swift
@@ -16,7 +16,11 @@ struct ContentView: View {
             List(menu) { section in
                 Section(header: Text(section.name)) {
                     ForEach(section.items) { item in
-                        ItemRow(item: item)
+                        NavigationLink {
+                            Text(item.name)
+                        } label: {
+                            ItemRow(item: item)
+                        }
                     }
                 }
             }
---
+++ HackingWithSwift/iDine/iDine/ItemDetail.swift
@@ -0,0 +1,18 @@
+//
+//  ItemDetail.swift
+//  iDine
+//
+//  Created by Thomas Cowern on 12/19/25.
+//
+
+import SwiftUI
+
+struct ItemDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemDetail()
+}
