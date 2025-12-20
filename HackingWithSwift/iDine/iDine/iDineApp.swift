b32d100dfdfea96f4e6cb105e76ac716eafcfaec
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-20 09:38:42 -0500

    update App file to use MainView as starting point

--- HackingWithSwift/iDine/iDine/iDineApp.swift
+++ HackingWithSwift/iDine/iDine/iDineApp.swift
@@ -14,7 +14,7 @@ struct iDineApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environment(order)
         }
     }
