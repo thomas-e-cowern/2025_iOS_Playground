8168dcd18364e757ced1edc05617e31c1d342b0b
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-21 10:19:18 -0500

    ProductRow to project

--- Skills/MockDataTest/MockDataTest/ProductRow.swift
+++ Skills/MockDataTest/MockDataTest/ProductRow.swift
@@ -8,11 +8,27 @@
 import SwiftUI
 
 struct ProductRow: View {
+    
+    var product: Product
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            AsyncImage(url: URL(string: product.images.first ?? "")) { image in
+                        image
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } placeholder: {
+                        ProgressView()
+                    }
+                    .frame(width: 80, height: 80)
+            Text(product.title)
+                .font(.title)
+        }
     }
 }
 
 #Preview {
-    ProductRow()
+    ProductRow(product: Product(id: 11, title: "Classic Red Baseball Cap", slug: "Classic Red Baseball Cap", price: 35, description: "Elevate your casual wardrobe with this timeless red baseball cap. Crafted from durable fabric, it features a comfortable fit with an adjustable strap at the back, ensuring one size fits all. Perfect for sunny days or adding a sporty touch to your outfit.", category: Category(id: 1, name: "change", slug: "change", image: "cBuLvBi"), images: [
+        "cBuLvBi", "kKc9A5p", "N1GkCIR"
+    ]))
 }
