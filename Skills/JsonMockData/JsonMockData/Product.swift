90aa771f0b2714e74ea689625f9a6e3c0c02f1dc
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-22 12:48:46 -0500

    update Product model with example

--- Skills/JsonMockData/JsonMockData/Product.swift
+++ Skills/JsonMockData/JsonMockData/Product.swift
@@ -18,6 +18,13 @@ struct Product: Codable, Identifiable, Equatable {
     private enum CodingKeys: String, CodingKey {
         case id, title, slug, price, description, category, images
     }
+    
+    #if DEBUG
+    static let example = Product(id: 13, title: "Classic Olive Chino Shorts", slug: "classic-olive-chino-shorts", price: 84, description: "Elevate your casual wardrobe with these classic olive chino shorts. Designed for comfort and versatility, they feature a smooth waistband, practical pockets, and a tailored fit that makes them perfect for both relaxed weekends and smart-casual occasions. The durable fabric ensures they hold up throughout your daily activities while maintaining a stylish look.", category: Category(id: 1, name: "Clothes", slug: "clothes", image: "https://i.imgur.com/QkIa5tT.jpeg"), images: [
+        "https://i.imgur.com/UsFIvYs.jpeg",
+        "https://i.imgur.com/YIq57b6.jpeg"
+      ])
+    #endif
 }
 
 struct Category: Codable, Identifiable, Equatable {
@@ -26,4 +33,3 @@ struct Category: Codable, Identifiable, Equatable {
     let slug: String
     let image: String
 }
-
