4c56a00e3fe3ed22706f4705f844534a783c3d52
Thomas Cowerrn <thomas.e.cowern@gmail.com>
2025-12-22 10:44:13 -0500

    add ProductByCategory to project

---
+++ Skills/JsonMockData/JsonMockData/ProductByCategory.swift
@@ -0,0 +1,41 @@
+//
+//  ProductByCategory.swift
+//  JsonMockData
+//
+//  Created by Thomas Cowern on 12/22/25.
+//
+
+import SwiftUI
+
+struct ProductByCategory: View {
+    
+    let category: Category
+    
+    @State var products: [Product] = load("products.json")
+    
+    var body: some View {
+        VStack {
+            List {
+                ForEach(products) { product in
+                    HStack {
+                        AsyncImage(url: URL(string: product.images.first ?? "")) { image in
+                            image
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 80, height: 80)
+                        Text(product.title)
+                            .font(.title)
+                    }
+                }
+            }
+            .navigationTitle(category.name)
+        }
+    }
+}
+
+#Preview {
+    ProductByCategory(category: Category(id: 1, name: "Rocks", slug: "rocks", image: "https://picsum.photos/200"))
+}
