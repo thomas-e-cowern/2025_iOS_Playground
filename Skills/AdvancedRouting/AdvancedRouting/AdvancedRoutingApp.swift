4dbde35babbdc47181682e449fedae732650b7d7
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-09 07:38:59 -0500

    AdvancedRouting initial commit in Skills folder

---
+++ Skills/AdvancedRouting/AdvancedRouting/AdvancedRoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedRoutingApp.swift
+//  AdvancedRouting
+//
+//  Created by Thomas Cowern on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedRoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Skills/AdvancedRouting/AdvancedRouting/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdvancedRouting
+//
+//  Created by Thomas Cowern on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
