48417eedf02a645efa5725b546a794972af234b4
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-09 07:48:43 -0500

    add Enums file

--- Skills/AdvancedRouting/AdvancedRouting/Enums.swift
+++ Skills/AdvancedRouting/AdvancedRouting/Enums.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+
+enum Destination: Hashable {
+    case tab(TabDestination)
+    case push(PushDestination)
+    case sheet(SheetDestination)
+}
+
+enum TabDestination: String, Hashable {
+    case home
+    case calendar
+    case profile
+}
+
+enum PushDestination: Hashable {
+    case favoriteList
+    case settingsDetail(section: String)
+}
+
+enum SheetDestination: Identifiable, Hashable {
+    case favoriteList
+    case settingsDetail(section: String)
+
+    var id: String {
+        switch self {
+        case .favoriteList:
+            return "Favorites"
+        case .settingsDetail(section: "Section"):
+            return "Section"
+        default:
+            return "Home"
+        }
+    }
+}
