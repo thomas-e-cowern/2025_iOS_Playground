53afd9a7d31be31450a35e3c19e24838c80f875f
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-09 09:11:59 -0500

    update Enums with new enums

--- Skills/AdvancedRouting/AdvancedRouting/Enums.swift
+++ Skills/AdvancedRouting/AdvancedRouting/Enums.swift
@@ -7,55 +7,21 @@
 
 import SwiftUI
 
-enum Destination: Hashable {
-    case tab(TabDestination)
-    case push(PushDestination)
-    case sheet(SheetDestination)
-}
-
-enum TabDestination: String, Hashable {
+enum Route: Hashable {
     case home
     case favorites
     case settings
 }
 
-enum PushDestination: Hashable {
-    case favoriteList
-    case settingsDetail(section: String)
-}
-
-enum SheetDestination: Identifiable, Hashable {
-    case favoriteList
-    case settingsDetail(section: String)
-
-    var id: String {
+extension Route: View {
+    var body: some View {
         switch self {
-        case .favoriteList:
-            return "Favorites"
-        case .settingsDetail(section: "Section"):
-            return "Section"
-        default:
-            return "Home"
+        case .home:
+            Home()
+        case .favorites:
+            Favorites()
+        case .settings:
+            Settings()
         }
-    }
-}
-
-@ViewBuilder
-func view(for destination: PushDestination) -> some View {
-    switch destination {
-    case .favoriteList:
-        Favorites()
-    case .settingsDetail(section: let section):
-        Settings()
-    }
-}
-
-@ViewBuilder
-func view(for destination: SheetDestination) -> some View {
-    switch destination {
-    case .favoriteList:
-        Favorites()
-    case .settingsDetail(section: let section):
-        Settings()
     }
 }
