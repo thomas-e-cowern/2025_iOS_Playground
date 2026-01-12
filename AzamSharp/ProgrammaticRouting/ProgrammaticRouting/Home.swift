10a0dd6330c77c82844095d8e17e1702602611db
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-12 11:19:14 -0500

    add Home screen to project

---
+++ AzamSharp/ProgrammaticRouting/ProgrammaticRouting/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  ProgrammaticRouting
+//
+//  Created by Thomas Cowern on 1/12/26.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
