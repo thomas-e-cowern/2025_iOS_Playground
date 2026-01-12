6743d6f3938823ad75211a3724e1ddb81abfc7f4
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-12 10:44:54 -0500

    add AppTabView to project

--- AzamSharp/ProgrammaticRouting/ProgrammaticRouting/AppTabView.swift
+++ AzamSharp/ProgrammaticRouting/ProgrammaticRouting/AppTabView.swift
@@ -5,4 +5,18 @@
 //  Created by Thomas Cowern on 1/12/26.
 //
 
-import Foundation
+import SwiftUI
+
+struct AppTabView: View {
+    var body: some View {
+        TabView(selection: $selection) {
+            ForEach(AppScreen.allCases) { screen in
+                screen.destination
+                    .tag(screen as AppScreen?)
+                    .tabItem {
+                        screen.label
+                    }
+            }
+        }
+    }
+}
