7fcff4af756141e5ebc2ddc3cfcd9d53a95aa343
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-12 11:25:30 -0500

    add Settings to project

--- AzamSharp/ProgrammaticRouting/ProgrammaticRouting/Settings.swift
+++ AzamSharp/ProgrammaticRouting/ProgrammaticRouting/Settings.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct Settings: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            List(1...5, id: \.self) { index in
+                NavigationLink("Settings \(index)", destination: Text("Settings Page number \(index)"))
+            }
+            .navigationTitle("Settings")
+        }
     }
 }
 
