81b3208ae8c02e6cd3698f4247e24f4f31ae0ee2
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-12 16:21:50 -0500

    update Favorites to add route

--- AzamSharp/ProgrammaticRouting/ProgrammaticRouting/Favorites.swift
+++ AzamSharp/ProgrammaticRouting/ProgrammaticRouting/Favorites.swift
@@ -8,12 +8,31 @@
 import SwiftUI
 
 struct Favorites: View {
+    
+    @State private var routes: [BirdRoute] = []
+    
     var body: some View {
-        NavigationStack {
-            List(1...10, id: \.self) { index in
-                NavigationLink("Favorites \(index)", destination: Text("Favorites Page number \(index)"))
+        NavigationStack(path: $routes) {
+            VStack {
+                List(1...10, id: \.self) { index in
+                    NavigationLink("Favorites \(index)", destination: Text("Favorites Page number \(index)"))
+                }
+                .navigationTitle("Favorites")
+                
+                Button("Go to details") {
+                    routes.append(.details(id: 2))
+                }
+                .navigationDestination(for: BirdRoute.self) { route in
+                    switch route {
+                    case .home:
+                        Home()
+                    case .details(id: 2):
+                        BirdView(bird: Birds.example[2])
+                    default:
+                        Home()
+                    }
+                }
             }
-            .navigationTitle("Favorites")
         }
     }
 }
