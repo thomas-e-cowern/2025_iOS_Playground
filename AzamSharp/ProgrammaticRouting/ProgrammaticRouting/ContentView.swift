7c95f55511353bc53521ed52aa81301e54383e0a
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-12 10:52:14 -0500

    update ContentView, add AppTabView

--- AzamSharp/ProgrammaticRouting/ProgrammaticRouting/ContentView.swift
+++ AzamSharp/ProgrammaticRouting/ProgrammaticRouting/ContentView.swift
@@ -8,14 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var selection: AppScreen? = .home
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        AppTabView(selection: $selection)
     }
 }
 
