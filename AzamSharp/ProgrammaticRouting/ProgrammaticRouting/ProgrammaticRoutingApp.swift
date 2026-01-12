af2e1294b1dc4059f3d7b706d1e356c63d45551d
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-12 10:36:01 -0500

    ProgrommaticRouting initial commit in AzamSharp folder

---
+++ AzamSharp/ProgrammaticRouting/ProgrammaticRouting/ProgrammaticRoutingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticRoutingApp.swift
+//  ProgrammaticRouting
+//
+//  Created by Thomas Cowern on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammaticRoutingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
