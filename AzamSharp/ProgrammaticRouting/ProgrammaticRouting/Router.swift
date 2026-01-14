4d9c342fd7d3568670803a0e1631ccc9ef0895cd
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-14 07:46:08 -0500

    add Router to project

---
+++ AzamSharp/ProgrammaticRouting/ProgrammaticRouting/Router.swift
@@ -0,0 +1,16 @@
+//
+//  Router.swift
+//  ProgrammaticRouting
+//
+//  Created by Thomas Cowern on 1/12/26.
+//
+
+import SwiftUI
+import Observation
+
+@Observable
+class Router {
+    var birdRoutes: [BirdRoute] = []
+    
+    
+}
