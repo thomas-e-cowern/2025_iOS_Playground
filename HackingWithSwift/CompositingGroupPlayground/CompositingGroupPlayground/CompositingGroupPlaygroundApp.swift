52f0524804809888e652bb0f52035b2a5f5afe01
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-15 08:09:12 -0500

    CompositingGroupPlayground initial commit in HackingWithSwift

---
+++ HackingWithSwift/CompositingGroupPlayground/CompositingGroupPlayground/CompositingGroupPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompositingGroupPlaygroundApp.swift
+//  CompositingGroupPlayground
+//
+//  Created by Thomas Cowern on 1/15/26.
+//
+
+import SwiftUI
+
+@main
+struct CompositingGroupPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HackingWithSwift/CompositingGroupPlayground/CompositingGroupPlayground/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CompositingGroupPlayground
+//
+//  Created by Thomas Cowern on 1/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
