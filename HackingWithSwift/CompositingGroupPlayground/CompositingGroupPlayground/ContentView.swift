98f3b43f3d34c3b8c8e2c59549d04ad8640c028b
Thomas Cowern <thomas.e.cowern@gmail.com>
2026-01-15 09:22:14 -0500

    update ContentView with Circles()

--- HackingWithSwift/CompositingGroupPlayground/CompositingGroupPlayground/ContentView.swift
+++ HackingWithSwift/CompositingGroupPlayground/CompositingGroupPlayground/ContentView.swift
@@ -9,16 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Hello, world!")
-                .font(.largeTitle)
-                .foregroundStyle(.white)
-                .padding()
-                .background(.blue)
-                .compositingGroup()
-                .shadow(color: .black, radius: 5)
+        ZStack {
+            Circle()
+                .fill((Color(red: 1, green: 0, blue: 0)))
+                .frame(width: 300, height: 200)
+                .offset(x: 0, y: 50)
+            
+            Circle()
+                .fill((Color(red: 0, green: 1, blue: 0)))
+                .frame(width: 200, height: 200)
+                .offset(x: 50, y: -50)
+            
+            Circle()
+                .fill((Color(red: 0, green: 0, blue: 1)))
+                .frame(width: 200, height: 200)
+                .offset(x: -50, y: -50)
         }
-        .padding()
     }
 }
 
